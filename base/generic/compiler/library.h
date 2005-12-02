22718b17d5f7cde75f1fe758fa7c3c9ca54d12db
jds <jds@f15cd148-d149-4907-926e-73cf92b3ee03>
2005-12-02 23:33:32 +0000

    Doxygen comment update for a few headers.
    git-svn-id: https://svn.ascend4.org/trunk@74 f15cd148-d149-4907-926e-73cf92b3ee03

--- base/generic/compiler/library.h
+++ base/generic/compiler/library.h
@@ -29,6 +29,9 @@
 /** @file
  *  Type Definition Library.
  *
+ *  Implements a type definition library for use by the ASCEND compiler.
+ *  Each type encountered during parsing is converted to a TypeDescription
+ *  and stored in the main type library maintained by this module.
  *  The type library will only maintain one definition for a given type
  *  name.  The library will complain if you try to add two types with the
  *  same name unless it happens when reloading a module.
@@ -36,11 +39,14 @@
  *  When #including library.h, make sure these files are #included first:
  *         #include "utilities/ascConfig.h"
  *         #include "compiler/compiler.h"
+ *         #include "compiler/type_desc.h"
+ *         #include "compiler/module.h"
+ *         #include "general/list.h"
  *  </pre>
  */
 
-#ifndef __LIBRARY_H_SEEN__
-#define __LIBRARY_H_SEEN__
+#ifndef library_h_seen__
+#define library_h_seen__
 
 /** For use in constructing hierarchies. */
 struct HierarchyNode {
@@ -49,160 +55,188 @@ struct HierarchyNode {
 };
 
 extern void InitializeLibrary(void);
-/**< Initialize the library. */
+/**<
+ *  Initializes the library.
+ *  This should be called before using any other functions
+ *  or globals in this module.  Call DestroyLibrary() when
+ *  finished with the library.
+ */
+
+extern void DestroyLibrary(void);
+/**<  
+ *  Cleans up after library use. 
+ *  Do not use any other functions or globals in this module after
+ *  using this function until InitializeLibrary() is called.  
+ */
 
 extern struct TypeDescription *FindType(symchar *name);
-/**< 
- *  <!--  struct TypeDescription *FindType(name)                       -->
- *  <!--  const char *name;                                            -->
- *  Find the type description associated with name.  Returns NULL if unable
- *  to locate the type. Handles NULL input gracefully.
+/**<
+ *  Finds the type description associated with name.  
+ *  Returns NULL if unable to locate the type. 
+ *  Handles NULL input gracefully.
+ *
+ *  @param name The type name to look up.
+ *  @return The type description having the specified name,
+ *          or NULL if none have that name.
  */
 
 extern struct TypeDescription *FindRelationType(void);
 /**< 
- *  <!--  struct TypeDescription *FindRelationType()                   -->
- *  Find the type description associated with real relations.
+ *  Finds the type description associated with real relations.
  *  Returns NULL if never defined, which means someone forgot to
  *  load a system.lib equivalent.
  */
 
 extern struct TypeDescription *FindLogRelType(void);
-/**< 
- *  <!--  struct TypeDescription *FindLogRelnType()                    -->
- *  Find the type description associated with logical relations.
+/**<
+ *  Finds the type description associated with logical relations.
  *  Returns NULL if never defined, which means someone forgot to
  *  load a system.lib equivalent.
  */
 
 extern struct TypeDescription *FindSetType(void);
-/**< 
- *  <!--  struct TypeDescription *FindWhenType()                       -->
- *  Find the type description associated with set statements.
+/**<
+ *  Finds the type description associated with set statements.
  *  Returns NULL if never defined, which is an extreme error.
  */
 
 extern struct TypeDescription *FindDummyType(void);
 /**< 
- *  <!--  struct TypeDescription *FindDummyType()                      -->
- *  Find the type description associated with unselected statements.
+ *  Finds the type description associated with unselected statements.
  *  Returns NULL if never defined, which is an extreme error.
  */
 
 extern struct TypeDescription *FindWhenType(void);
 /**< 
- *  <!--  struct TypeDescription *FindWhenType()                       -->
- *  Find the type description associated with WHEN statements.
+ *  Finds the type description associated with WHEN statements.
  *  Returns NULL if never defined, which is an extreme error.
  */
 
 extern struct TypeDescription *FindExternalType(void);
 /**< 
- *  <!--  struct TypeDescription *FindExternalType()                   -->
- *  Find the type description associated with external statements.
+ *  Finds the type description associated with external statements.
  *  Returns NULL if never defined, which is an extreme error.
  */
 
 extern int AddType(struct TypeDescription *desc);
 /**< 
- *  <!--  void AddType(desc)                                           -->
- *  <!--  struct TypeDescription *desc;                                -->
- *  Add a type to the library.
- *  Returns 1 if kept the new type, 0 if already had it and kept old.
+ *  Adds a type to the library.
+ *  The type is not added if it is already present in the
+ *  type library.  Otherwise, the library takes ownership of
+ *  desc and adds it to the library.
+ *  
+ *  @param desc The type to add to the library.
+ *  @return Returns 1 if the new type is added and kept, 
+ *          0 if it was already present.
  */
-
-extern void DestroyLibrary(void);
-/**<  Clean up after library use. */
 
 extern struct gl_list_t *FindFundamentalTypes(void);
 /**< 
- *  <!--  struct gl_list_t *FindFundamentalTypes                       -->
- *  Return a gl_list containing pointers to the fundamental types
+ *  Creates a gl_list_t containing pointers to the fundamental 
+ *  types.  Destruction of the returned list (but not it's 
+ *  contents) is the responsibility of the caller.
+ *
+ *  @return A gl_list_t of (struct TypeDescription *) to the
+ *          fundamental types.
  */
 
 extern struct gl_list_t *Asc_TypeByModule(CONST struct module_t *module);
 /**< 
- *  <!--  struct gl_list_t *Asc_TypeByModule(module);                  -->
- *  <!--  const struct module_t *module;                               -->
+ *  Builds a list of type names defined in module that are already
+ *  present in the main type library.  Destruction of the returned
+ *  list (but not its contents) is the responsibility of the caller.
  *
- *  <!--  Comments:                                                    -->
- *  Accepts a module and searches the main type library defined in
- *  library.c for the all types that were found in that module.
- *  Builds a list of the types found and returns it.
+ *  @param module The module to parse for defined type names.
+ *  @return A gl_list_t of (symchar *) to the names of types found
+ *          both in module and in the main type library.
  */
 
 extern struct gl_list_t *TypesThatRefineMe (symchar *name);
 /**< 
- *  <!--  struct gl_list_t *TypesThatRefineMe(name);                   -->
- *  <!--  symchar *name;                                               -->
- *  <!--  Comments :                                                   -->
- *  Accepts the name of a type and searches the main library defined in
- *  library.c for all the types that refine the given type. Builds a list
- *  of all the types found and returns it. This should be an expensive
- *  function and could be made more efficient for certain atomic types.
- *  The list return is the list of immediate refinements only, not
- *  all of the refinements in a chain such as a<-b<-c<-d. Given a, only
- *  b is returned.
+ *  Builds a list of type names in the main type library which refine
+ *  the type having the specified name.  The returned list includes 
+ *  immediate refinements only, not all of the refinements in a chain 
+ *  such as a<-b<-c<-d.  Given a, only b is included in the list.
+ *  This should be an expensive function and could be made more 
+ *  efficient for certain atomic types.  Destruction of the returned
+ *  list (but not its contents) is the responsibility of the caller.
+ *
+ *  @param name The type name to check for registered refinements.
+ *  @return A gl_list_t of (symchar *) to the names of types in the main
+ *          type library which refine the specified type.
  */
 
 extern struct gl_list_t *AllTypesThatRefineMe_Flat (symchar *name);
-/**< 
- *  <!--  struct gl_list_t *AllTypesThatRefineMe_Flat(name);           -->
- *  <!--  symchar *name;                                               -->
- *  <!--  Comments :                                                   -->
- *  Accepts the name of a type and searches the main library defined in
- *  library.c for all the types that refine the given type. Builds a list
- *  of all the types found and returns it. This is only slightly more
- *  expensive than TypesThatRefineMe.
- *  For efficiency atoms and models are handled differently.
- *  The list returned is the list of all refinements in a chain
- *  such as a<-b<-c<-d. if a is input, b,c,d are returned.
+/**<
+ *  Builds a list of all type names in the main type library which
+ *  refine the type having the specified name.  The returned list
+ *  includes all refinements in a chain such as a<-b<-c<-d.  Given a,
+ *  b, c, and d are all included in the list.  This is only slightly
+ *  more expensive than TypesThatRefineMe().  For efficiency atoms 
+ *  and models are handled differently.  Destruction of the returned
+ *  list (but not its contents) is the responsibility of the caller.
+ *
+ *  @param name The type name to check for registered refinements.
+ *  @return A gl_list_t of (symchar *) to the names of types in the main
+ *          type library which refine the specified type.
  */
 
 extern struct HierarchyNode *AllTypesThatRefineMe_Tree (symchar *name);
 /**< 
- *  <!--  struct HierarchyNode *AllTypesThatRefineMe_Tree(name);       -->
- *  <!--  const char *name;                                            -->
- *  <!--  Comments :                                                   -->
- *  Accepts the name of a type and searches the main library defined in
- *  library.c for all the types that refine the given type. Builds a tree
- *  of HierarchyNodes of all the types found and returns it. This is somewhat
- *  pricey in terms of the number of pointer comparisons.
- *  For efficiency atoms and models are handled separately.
- *  The first node returned is the node of the type given. If it is null,
- *  the type given was not found.
+ *  Builds a tree of HierarchyNodes all types in the main type library 
+ *  which refine the type having the specified name.  This is similar to
+ *  AllTypesThatRefineMe_Flat() except that the results are returned in
+ *  a HierarchyNodes tree.  This is somewhat pricey in terms of the number 
+ *  of pointer comparisons.   For efficiency atoms and models are handled 
+ *  separately.  The first node returned is the node of the type given. 
+ *  If it is null, the type given was not found.  Destruction of the 
+ *  returned tree is the responsibility of the caller.  Use 
+ *  DestroyHierarchyNode() for this purpose.
+ *
+ *  @param name The type name to check for registered refinements.
+ *  @return A tree of HierarchyNodes containing the types in the main
+ *          type library which refine the specified type.
  */
 
 extern void DestroyHierarchyNode(struct HierarchyNode *heir);
 /**< 
- *  <!--  struct HierarchyNode *hier;                                  -->
- *  <!--  Comments:                                                    -->
- *  Deallocates (recursively) all the memory associated with a HierarchyNode.
+ *  Deallocates (recursively) all the memory associated with a 
+ *  HierarchyNode.  Use this function to destroy the tree returned
+ *  by AllTypesThatRefineMe_Tree().
+ *
+ *  @param heir Head of the HeirarchyNode tree to destroy.
  */
 
 extern int IsTypeRefined(CONST struct TypeDescription *desc);
-/**< 
- *  <!--  int IsTypeRefined(desc);                                     -->
- *  <!--  CONST struct TypeDescription *desc;                          -->
- *  <!--  Comments :                                                   -->
- *  Searches the main library defined in library.c to see if the type desc,
- *  is refined by any other type, i.e., if the type is the root of a
- *  hierarchy. Returns 1 if TRUE, 0 otherwise.
+/**<
+ *  Check whether the specified type is refined by any other type in 
+ *  the main type library.  
+ *
+ *  @param desc The type to check for refinements.
+ *  @return Returns 1 if desc is refined, 0 otherwise.
  */
 
 extern struct gl_list_t *DefinitionList(void);
-/**< 
- *  Make a sorted list of all the definitions.  In the case of there being
- *  two versions of a given type, the latest version is used.  This doesn't
- *  include array type definitions.
- *  The user is responsible for destroying the list (but not its content).
+/**<
+ *  Makes a sorted list of all registered definitions.
+ *  In the case of there being two versions of a given type,
+ *  the latest version is used.  This doesn't include array type
+ *  definitions.  The user is responsible for destroying the
+ *  returned list (but not its content).
+ *
+ *  @return A gl_list_t of (struct TypeDescription *) containing
+ *          the sorted types.
  */
 
 extern unsigned int CheckFundamental(symchar *f);
-/**< 
- *  Return true if string is a fundamental type name.
- *  f must be from the symbol table.
+/**<
+ *  Checks whether string f is a fundamental type name.
+ *  f must be from the symbol table (i.e. not an externally-defined
+ *  character string).
+ *
+ *  @param f The string to check.
+ *  @return Returns 1 if f is a fundamental type name, 0 if not.
  */
 
-#endif /* __LIBRARY_H_SEEN__ */
+#endif /* library_h_seen__ */
 
--- base/generic/compiler/syntax.h
+++ base/generic/compiler/syntax.h
@@ -1,4 +1,4 @@
-/* 
+/*
  *  Syntax Routines
  *  by Tom Epperly
  *  Created: 3/22/1990
@@ -25,7 +25,7 @@
  *  with the program; if not, write to the Free Software Foundation, Inc., 675
  *  Mass Ave, Cambridge, MA 02139 USA.  Check the file named COPYING.
  */
-
+                                     
 /** @file
  *  Syntax Routines.
  *  These routines support the yacc parsing file ascParse.y.  This module
@@ -33,12 +33,12 @@
  *  <pre>
  *  When #including syntax.h, make sure these files are #included first:
  *         #include "utilities/ascConfig.h"
- *         #include "fractions.h"
- *         #include "compiler.h"
- *         #include "dimen.h"
- *         #include "types.h"
- *         #include "exprs.h"
- *         #include "stattypes.h"
+ *         #include "compiler/fractions.h"
+ *         #include "compiler/compiler.h"
+ *         #include "compiler/dimen.h"
+ *         #include "compiler/types.h"
+ *         #include "compiler/exprs.h"
+ *         #include "compiler/stattypes.h"
  *  </pre>
  */
 
@@ -47,31 +47,40 @@
 
 extern unsigned NumberOfRelOps(struct Expr *ex);
 /**< 
- *  <!--  unsigned NumberOfRelOps(ex)                                  -->
- *  <!--  struct Expr *ex;                                             -->
- *  Return the number of relation operators in the given Expr.
+ *  Counts the number of relation operators in the given Expr.
+ *
+ *  @param ex The expresion to evaluate.
+ *  @return The number of relation operators in ex.
  */
 
 extern int IsRelation(struct Expr *ex);
-/**< 
- *  <!--  unsigned IsRelation(ex)                                      -->
- *  <!--  struct Expr *ex;                                             -->
- *  Return 1 in case of a real relation. 0 if logical relation.
+/**<
+ *  Tests whether an expression is a real or logical relation.
+ *
+ *  @param ex The expresion to test.
+ *  @return Returns 1 if ex is a real relation, 0 if a
+ *          logical relation.
+ *  @todo compiler/syntax:IsRelation() notated as broken in source file.
+ *        Fix or remove comment.
  */
 
 extern int GetParseRelnsFlag(void);
 /**<
- *  <!--  int GetParseRelnsFlag(void);                                 -->
- *  Returns the value of the parse relations flag. If this flag is set to
- *  TRUE, which it is by default, normal ASCEND relations will be parsed;
- *  otherwise they will not be. This does not affect external relations.
+ *  Retrieves the value of the parse relations flag. If this flag is 
+ *  non-zero (the default), normal ASCEND relations will be parsed.  
+ *  If it is 0, they will not be.  This flag does not affect 
+ *  external relations.
+ *
+ *  @return The current value of the parse relations flag.
  */
 
 extern void SetParseRelnsFlag(int flag);
-/**< 
- *  <!--  void SetParseRelnsFlag(flag);                                -->
- *  This function sets the parse relations flag. Any nonzero integer will
- *  turn the flag on. Later it may be given more meaning.
+/**<
+ *  Sets the parse relations flag. Any nonzero integer will
+ *  cause relations to be parsed (see GetParseRelnsFlag()).
+ *  Later it may be given more meaning.
+ *
+ *  @param flag The new value for the parse relations flag.
  */
 
 #endif  /* __SYNTAX_H_SEEN__ */
