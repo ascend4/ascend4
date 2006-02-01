56d8a06dc28ef70cdc3c0a7a3d09b3bfb6e5f40a
John Pye <john.pye@anu.edu.au>
2006-02-01 03:22:11 +0000

    Working (read-only) observer thing. Observer instances are updated in the table and if you click 'add' on the observer tab, the current values are kept and a new row is added.
    git-svn-id: https://svn.ascend4.org/trunk@249 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/interface/name.cpp
+++ pygtk/interface/name.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+	#include <iostream>
 #include <stdexcept>
 #include <sstream>
 using namespace std;
--- pygtk/interface/observer.py
+++ pygtk/interface/observer.py
@@ -10,39 +10,79 @@ class ObserverTab:
 
 		self.view = xml.get_widget('observerview')
 
-		self.activeimg = self.view.render_icon(gtk.STOCK_YES,gtk.ICON_SIZE_MENU)
+		#self.activeimg = gtk.Image()
+		#self.activeimg.set_from_file("icons/active.png")
+		self.activeimg = None
+		self.keptimg = None
+		
+		# no instances yet in the observer:
+		self.columninstances = []
 
 		self.columns = [gtk.gdk.Pixbuf]
 		self.titles = ["T"]
-		self.store = gtk.TreeStore(*self.columns)
+		_store = gtk.TreeStore(*self.columns)
 		self.rows = []
-		self.rows.append([self.activeimg])
 
+		# add an empty first row
+		self.rows.append([])
+
+		# work towards having multiple observers for multiple simulations
 		self.name = name
 		self.browser = browser
 
-
 		# create the 'active' pixvuf columns	
+		_renderer = gtk.CellRendererPixbuf()
 		_col = gtk.TreeViewColumn()
 		_col.set_title(self.titles[0])
-		self.view.append_column(_col);
-		_renderer = gtk.CellRendererPixbuf()
 		_col.pack_start(_renderer,False)
-		_col.add_attribute(_renderer, 'pixbuf', int(0))
+		_col.add_attribute(_renderer, 'pixbuf', 0)
+		self.view.append_column(_col);
 
 		# create the first row
 		print "Adding row",self.rows[0],"to store"
-		self.store.append(None, self.rows[0])
+		_store.append(None, self.make_row(self.activeimg, self.rows[0]) )
+
+		self.view.set_model(_store)
 
 		self.browser.reporter.reportNote("Created OBSERVER")
 
-	def on_add_click(self,*args):
-		print "ADD"
-		pass
+	def on_add_clicked(self,*args):
+		_rownum = len(self.rows)
 
-	def on_clear_click(self,*args):
+		# add a copy of the last row
+		self.rows.append(self.rows[_rownum-1])
+		self.activerow = _rownum
+
+		self.view.get_model().set(self.activeiter,0,self.keptimg)
+		self.activeiter = self.view.get_model().append(None,self.make_row(self.activeimg,self.rows[_rownum]))
+
+	def on_clear_clicked(self,*args):
 		print "CLEAR"
 		pass
+
+	def sync(self):
+		print "SYNCHING ROW",self.activerow
+		
+		#new row data
+		_r = [_i.getRealValue() for _i in self.columninstances]
+
+		# stick the row data into the TreeStore
+		_m = self.view.get_model()
+		_i = 1
+		for _c in _r:
+			_m.set(self.activeiter, _i, _c)
+			_i = _i + 1
+		print "REPLACEMENT DATA IS",_r
+
+		# keep the data in self.rows as well
+		self.rows[self.activerow] = _r;
+
+	def make_row(self,img,row):
+		_r = [img]
+		for _c in row:
+			_r.append(_c)
+		print "MADE KEPT ROW:",_r
+		return _r		
 
 	def add_instance(self, inst):
 		if not inst.getType().isRefinedSolverVar():
@@ -54,6 +94,12 @@ class ObserverTab:
 		_value = float(inst.getRealValue())
 		_rownum = len(self.rows)-1
 
+		# store the instances in self.columninstances for sync purposes
+		self.columninstances.append(inst)
+	
+		print "ROWS IN self.rows",len(self.rows)
+		print "LAST ROW: ",repr(self.rows[_rownum])
+
 		print "COLNUM =",_colnum
 
 		print "ADDING TO OBSERVER '"+self.name+"' COLUMN '"+_colname+"', CURRENT VALUE = ",_value
@@ -62,46 +108,35 @@ class ObserverTab:
 		self.columns.append(float)
 		self.titles.append(_colname);
 		print "NEW COLUMNS WILL BE",str(self.columns)
+
 		_store = gtk.TreeStore(*(self.columns))
-		_currentrow = None
+
+		_iter = None
+		_i = 0
 		for _r in self.rows:
-			print "OLD ROW DATA:",repr(_r)
-			_newrowdata = _r.append(None)
-			print "NEW ROW DATA:",repr(_r)
-			_currentrow = _store.append(None,_newrowdata)
+			print "ROW",_i
+			_r.append(0)
+			_iter = _store.append(None, self.make_row(self.keptimg,_r) )
+			_i = _i + 1
+	
+		self.activeiter = _iter
 
 		# add newest data point in bottom-right
-		self.rows[_rownum][_colnum] = _value
-		_store.set_value(_currentrow, _colnum, _value)
-
-		# kill old self.store
-		del(self.store)
-		self.store = _store
+		self.rows[_rownum][_colnum-1] = _value
+		_store.set_value(self.activeiter, _colnum, _value)
 
 		# re-assign store to TreeView
-		self.view.set_model(self.store)		#self.store.set(_currentrow, _colnum, _value)
+		self.view.set_model(_store)		#self.store.set(_currentrow, _colnum, _value)
 
 		# add value to current column
 		#self.store.set(_currentrow, _colnum, _value)
 
 		_renderer = gtk.CellRendererText()
 		_col = gtk.TreeViewColumn(_colname, _renderer)
-		_col.set_alignment(1.0)
-		_col.set_reorderable(True)
-		_col.set_sort_column_id(_colnum)
+		_col.add_attribute(_renderer, 'text', _colnum)
+		_col.set_alignment(0.0)
+		#_col.set_reorderable(True)
+		#_col.set_sort_column_id(_colnum)
 
 		self.view.append_column(_col);
-
-		print "SELF.STORE: COLUMNS =",self.store.get_n_columns()," ROWS =",self.store.iter_n_children(None)
-
-
-		print "LAST ROW DATA:",repr(self.rows[_rownum])	
-
-		_rn = 0
-		_r = self.store.iter_children(None)
-		while _r != None:
-			for _i in range(0, self.store.get_n_columns()):
-				print "ROW",_rn,"COL",_i,":",self.store.get(_r,_i)
-			_r = self.store.iter_next(_r)
-			_rn = _rn + 1
 
