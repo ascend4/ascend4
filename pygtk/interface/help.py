78b4b619bc8855fcdea8fcf952552c5567419403
johnpye <johnpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2006-03-01 00:51:23 +0000

    Something is starting to run on MinGW now. GUI loads and ascend DLL is opened. This revision still breaks the linux and Tcl/Tk builds, I expect. Also added some smarts to the 'help...contents' menu for loading offline/online help files.
    git-svn-id: https://svn.ascend4.org/trunk@337 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/interface/help.py
+++ pygtk/interface/help.py
@@ -1,18 +1,49 @@
 import webbrowser
 import os.path
+import platform
+import config
 
 class Help:
 	def __init__(self,helproot=None):
+		print "HELPROOT =",config.HELPROOT
+		self.goonline=False
+		self.webhelproot = config.WEBHELPROOT
+		
 		if helproot==None:
-			self.helproot = os.path.expanduser("~/src/ascend-doc/trunk/html")
+			self.helproot = os.path.expanduser(config.HELPROOT)
 		else:
 			self.helproot = helproot
+		
+		if os.path.exists(self.helproot):
+			print "HELP PATH FOUND"
+		else:
+			print "LOCAL HELP FILES NOT FOUND, WILL USE ONLINE COPY"
+			self.goonline = True
 
 	def run(self,topic=None):
-		#_b = webbrowser.GenericBrowser('dillo %s &');
-		_b = webbrowser.Konqueror();
-		_p = os.path.join(self.helproot,"book.html");
-		_b.open("file:"+_p);
+			
+		if platform.system()=='Windows':
+			try:
+				import win32api
+				_b = webbrowser.get('windows-default')
+			except:
+				print "FAILED TO IMPORT 'win32', hoping that firefox is installed..."
+				_b = webbrowser.GenericBrowser('\\Progra~1\\Mozill~1\\firefox %s &');
+			
+			print _b
+		elif platform.system()=='Linux':
+			_b = webbrowser.get('mozilla')
+		else:
+			print "PLATFORM IS ".platform.platform(True,True)
+
+		if self.goonline:
+			_u = self.webhelproot
+		else:
+			_p = os.path.join(self.helproot,"book.html")
+			_u = "file:"+_p
+		
+		print "OPENING WEB PAGE:",_u
+		_b.open(_u);
 
 if __name__ == "__main__":
 	_h = Help()
