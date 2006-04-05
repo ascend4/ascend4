6fa64dd578b3c8b9c072ae3077057c684d655f31
Wang YM <wangym@ascend4.org>
2006-04-05 13:35:15 +0000

    move redifinition macros from HAVE_C99 TO MSVC
    git-svn-id: https://svn.ascend4.org/trunk@456 f15cd148-d149-4907-926e-73cf92b3ee03

--- base/generic/utilities/error.h
+++ base/generic/utilities/error.h
@@ -117,15 +117,14 @@ typedef enum error_severity_enum{
                              fprintf(stderr, ##__VA_ARGS__) + \
                              fprintf(stderr, ERR_NORM "\n"))
 
-#define ERROR_REPORTER_START_HERE(SEV) error_reporter_start(SEV,__FILE__,__LINE__,__FUNCTION__);
-# define ERROR_REPORTER_DEBUG(...) error_reporter(ASC_PROG_NOTE,__FILE__,__LINE__,__FUNCTION__,## __VA_ARGS__)
-# define ERROR_REPORTER_HERE(SEV,...) error_reporter(SEV,__FILE__,__LINE__,__FUNCTION__, ## __VA_ARGS__)
-# define ERROR_REPORTER_NOLINE(SEV,...) error_reporter(SEV,NULL,0,NULL, ## __VA_ARGS__)
-# define CONSOLE_DEBUG(...) (fprintf(stderr, ERR_BOLD "%s:%d (%s): ", __FILE__,__LINE__,__FUNCTION__) + \
-                             fprintf(stderr, ##__VA_ARGS__) + \
-                             fprintf(stderr, ERR_NORM "\n"))
-
 #elif defined(_MSC_VER) && _MSC_VER >= 1310 /* Microsoft Visual C++ 2003 or newer */
+#  define ERROR_REPORTER_START_HERE(SEV) error_reporter_start(SEV,__FILE__,__LINE__,__FUNCTION__);
+#  define ERROR_REPORTER_DEBUG(...)     error_reporter(ASC_PROG_NOTE,__FILE__,__LINE__,__FUNCTION__,## __VA_ARGS__)
+#  define ERROR_REPORTER_HERE(SEV,...)  error_reporter(SEV,__FILE__,__LINE__,__FUNCTION__, ## __VA_ARGS__)
+#  define ERROR_REPORTER_NOLINE(SEV,...) error_reporter(SEV,NULL,0,NULL, ## __VA_ARGS__)
+#  define CONSOLE_DEBUG(...)   (fprintf(stderr, ERR_BOLD "%s:%d (%s): ", __FILE__,__LINE__,__FUNCTION__) + \
+                                fprintf(stderr, ##__VA_ARGS__) + \
+                                fprintf(stderr, ERR_NORM "\n"))
 
 #else /* workaround for compilers without variadic macros: last resort */
 # define NO_VARIADIC_MACROS
