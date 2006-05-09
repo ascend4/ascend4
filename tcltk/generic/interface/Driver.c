175d22c0c260e75232832001fa68227580b2c563
John Pye <john.pye@anu.edu.au>
2006-05-09 01:07:41 +0000

    Commenting changes, GPL header.
    git-svn-id: https://svn.ascend4.org/trunk@575 f15cd148-d149-4907-926e-73cf92b3ee03

--- tcltk/generic/interface/Driver.c
+++ tcltk/generic/interface/Driver.c
@@ -1,31 +1,30 @@
-/*
- *  Driver.c
- *  by Kirk Abbott and Ben Allan
- *  Created: 1/94
- *  Version: $Revision: 1.48 $
- *  Version control file: $RCSfile: Driver.c,v $
- *  Date last modified: $Date: 2003/08/23 18:43:06 $
- *  Last modified by: $Author: ballan $
- *
- *  This file is part of the ASCEND Tcl/Tk interface
- *
- *  Copyright 1997, Carnegie Mellon University
- *
- *  The ASCEND Tcl/Tk interface is free software; you can redistribute
- *  it and/or modify it under the terms of the GNU General Public License as
- *  published by the Free Software Foundation; either version 2 of the
- *  License, or (at your option) any later version.
- *
- *  The ASCEND Tcl/Tk interface is distributed in hope that it will be
- *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with the program; if not, write to the Free Software Foundation,
- *  Inc., 675 Mass Ave, Cambridge, MA 02139 USA.  Check the file named
- *  COPYING.  COPYING is found in ../compiler.
- */
+/*	ASCEND modelling environment
+	Copyright 1997, Carnegie Mellon University
+	Copyright (C) 2006 Carnegie Mellon University
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330,
+	Boston, MA 02111-1307, USA.
+*//*
+	Driver.c
+	by Kirk Abbott and Ben Allan
+	Created: 1/94
+	Version: $Revision: 1.48 $
+	Version control file: $RCSfile: Driver.c,v $
+	Date last modified: $Date: 2003/08/23 18:43:06 $
+	Last modified by: $Author: ballan $
+*/
 
 #include <stdarg.h>
 #include <ctype.h>
@@ -40,7 +39,7 @@
 #else
 
 /* jds20041229 - windows.h now included in ascConfig.h. */
-/* jp - no, i took it out of ascConfig.h - Apr 2005 */
+/* jp - i took it back out of ascConfig.h - Apr 2005 */
 # define WIN32_LEAN_AND_MEAN
 # include <windows.h>
 
@@ -521,7 +520,7 @@ static int AscCheckEnvironVars(Tcl_Interp *interp)
 
 	/home/john/ascroot/bin/ascend4
 	/home/john/ascroot/share/ascend/
-	
+
    *  to "."
    */
   if( Asc_ImportPathList(DIST_ENVIRONVAR) == 0 ) {
