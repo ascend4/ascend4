c07ab70dc7959e3737992c17824022891f0bca2b
wangym <wangym@f15cd148-d149-4907-926e-73cf92b3ee03>
2006-05-29 10:23:30 +0000

    fix SIZE_T name conflit under win32
    git-svn-id: https://svn.ascend4.org/trunk@645 f15cd148-d149-4907-926e-73cf92b3ee03

--- base/winVC8/prebuild.bat
+++ base/winVC8/prebuild.bat
@@ -4,6 +4,6 @@ del ascParse.h
 del ascParse.c
 flex -Pzz_ -t scanner.l >scanner.c
 bison -d -o ascParse.c ascParse.y
-sed -i -e "s/yy/zz_/g" scanner.c
-sed -i -e "/#ifndef YYSTYPE/,/#endif/d" -e "s/yy/zz_/g" -e "s/YY/ZZ_/g"  ascParse.c
-sed -i -e "s/yy/zz_/g" -e "s/YY/ZZ_/g" ascParse.h
+sed -i -e "s/yy/zz_/g" -e "s/SIZE_T/TSIZE_T/g" scanner.c
+sed -i -e "/#ifndef YYSTYPE/,/#endif/d" -e "s/yy/zz_/g" -e "s/YY/ZZ_/g" -e "s/SIZE_T/TSIZE_T/g" ascParse.c
+sed -i -e "s/yy/zz_/g" -e "s/YY/ZZ_/g" -e "s/SIZE_T/TSIZE_T/g" ascParse.h
