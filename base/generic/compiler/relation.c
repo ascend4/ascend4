2d9a106b7e9e89612bce7c45697df9a756a5a8bc
John Pye <john.pye@anu.edu.au>
2006-06-27 05:49:11 +0000

    Lots of tidying up in instantiate.c. Fixed up some GPL headers. Changed from 'solver_var' to 'factor' in extfntest.a4c. Added test for AtomAssigned in extfunc.c.
    git-svn-id: https://svn.ascend4.org/trunk@707 f15cd148-d149-4907-926e-73cf92b3ee03

--- base/generic/compiler/relation.c
+++ base/generic/compiler/relation.c
@@ -1,34 +1,31 @@
-/*
- *  Relation  construction  routines
- *  by Tom Epperly
- *  Created: 1/30/90
- *  Version: $Revision: 1.32 $
- *  Version control file: $RCSfile: relation.c,v $
- *  Date last modified: $Date: 1998/03/17 22:09:24 $
- *  Last modified by: $Author: ballan $
- *
- *  This file is part of the Ascend Language Interpreter.
- *
- *  Copyright (C) 1990, 1993, 1994 Thomas Guthrie Epperly
- *  Copyright (C) 1993, 1994, 1995  Kirk Andre' Abbott
- *  Copyright (C) 1996 Benjamin Andrew Allan
- *
- *  The Ascend Language Interpreter is free software; you can redistribute
- *  it and/or modify it under the terms of the GNU General Public License as
- *  published by the Free Software Foundation; either version 2 of the
- *  License, or (at your option) any later version.
- *
- *  The Ascend Language Interpreter is distributed in hope that it will be
- *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with the program; if not, write to the Free Software Foundation,
- *  Inc., 675 Mass Ave, Cambridge, MA 02139 USA.  Check the file named
- *  COPYING.
- *
- */
+/*	ASCEND modelling environment
+	Copyright (C) 2006 Carnegie Mellon University
+	Copyright (C) 1990, 1993, 1994 Thomas Guthrie Epperly
+	Copyright (C) 1993, 1994, 1995  Kirk Andre' Abbott
+	Copyright (C) 1996 Benjamin Andrew Allan
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330,
+	Boston, MA 02111-1307, USA.
+*//*
+	@file
+	Relation construction routines
+*//*
+	by Tom Epperly
+	Created: 1/30/90
+	Last in CVS $Revision: 1.32 $ $Date: 1998/03/17 22:09:24 $ $Author: ballan $
+*/
 
 #include <math.h>
 #include <stdarg.h>
