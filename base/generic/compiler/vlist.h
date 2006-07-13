94f11f9c417c9a50991fc118ca724723869f4ffb
John Pye <john.pye@anu.edu.au>
2006-07-13 08:19:11 +0000

    Tidying up
    git-svn-id: https://svn.ascend4.org/trunk@769 f15cd148-d149-4907-926e-73cf92b3ee03

--- base/generic/compiler/vlist.h
+++ base/generic/compiler/vlist.h
@@ -1,43 +1,36 @@
-/* 
+/*	ASCEND modelling environment
+	Copyright (C) 2006 Carnegie Mellon University
+	Copyright (C) 1990, 1993, 1994 Thomas Guthrie Epperly
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330,
+	Boston, MA 02111-1307, USA.
+*//** @file
  *  Variable name list
- *  by Tom Epperly
- *  August 8, 1989
- *  Version: $Revision: 1.7 $
- *  Version control file: $RCSfile: vlist.h,v $
- *  Date last modified: $Date: 1997/07/18 12:36:38 $
- *  Last modified by: $Author: mthomas $
- *
- *  This file is part of the Ascend Language Interpreter.
- *
- *  Copyright (C) 1990, 1993, 1994 Thomas Guthrie Epperly
- *
- *  The Ascend Language Interpreter is free software; you can redistribute
- *  it and/or modify it under the terms of the GNU General Public License as
- *  published by the Free Software Foundation; either version 2 of the
- *  License, or (at your option) any later version.
- *
- *  The Ascend Language Interpreter is distributed in hope that it will be
- *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with the program; if not, write to the Free Software Foundation, Inc., 675
- *  Mass Ave, Cambridge, MA 02139 USA.  Check the file named COPYING.
- */
 
-/** @file
- *  Variable name list.
- *  <pre>
- *  When #including vlist.h, make sure these files are #included first:
- *         #include "utilities/ascConfig.h"
- *         #include "compiler.h"
- *         #include "expr_types.h"
- *  </pre>
- */
+	Requires:
+	#include "utilities/ascConfig.h"
+	#include "compiler.h"
+	#include "expr_types.h"
+*//* 
+	by Tom Epperly
+	August 8, 1989
+	Last in CVS: $Revision: 1.7 $ $Date: 1997/07/18 12:36:38 $ $Author: mthomas $
+*/
 
-#ifndef __VLIST_H_SEEN__
-#define __VLIST_H_SEEN__
+#ifndef ASC_VLIST_H
+#define ASC_VLIST_H
 
 /**<
  *  Don't ever access these things yourself.  Use the functions and/or
@@ -50,45 +43,41 @@ struct VariableList {
 
 extern struct VariableList *CreateVariableNode(struct Name *name);
 /**<
- *  <!--  struct VariableList *CreateVariableNode(name)                -->
- *  <!--  struct Name *name;                                           -->
  *  Create a node with Name pointer name.  The next attribute is initialized to
  *  NULL.
  */
 
 extern void LinkVariableNodes(struct VariableList *cur, struct VariableList *next);
 /**< 
- *  <!--  void LinkVariableListNodes(cur,next)                         -->
- *  <!--  struct VariableList *cur,*next;                              -->
  *  Set the next attribute of cur to next.  Next can be NULL, but cur cannot.
  */
 
 #ifdef NDEBUG
-#define NextVariableNode(n) ((n)->next)
+# define NextVariableNode(n) ((n)->next)
 #else
-#define NextVariableNode(n) NextVariableNodeF(n)
+extern CONST struct VariableList *NextVariableNodeF(CONST struct VariableList *vl);
+/**<
+	Implementation function for NextVariableNode() (debug mode).
+	Do not call this function directly - use NextVariableNode() instead.
+*/
+# define NextVariableNode(n) NextVariableNodeF(n)
 #endif
 /**<
- *  Return the name list node linked to vl.
- *  @param n CONST struct VariableList*, the list to query.
- *  @return The node as a CONST struct VariableList*.
- *  @see NextVariableNodeF()
- */
-extern CONST struct VariableList
-*NextVariableNodeF(CONST struct VariableList *vl);
-/**<
- *  <!--  macro NextVariableNode(nvl)                                  -->
- *  <!--  extern CONST struct VariableList *NextVariableNodeF(vl)      -->
- *  <!--  struct VariableList *vl;                                     -->
- *  <!--  Return the name list node linked to vl.                      -->
- *  Implementation function for NextVariableNode() (debug mode).
- *  Do not call this function directly - use NextVariableNode() instead.
- */
+	Return the name list node linked to vl.
+	@param n CONST struct VariableList*, the list to query.
+	@return The node as a CONST struct VariableList*.
+	@see NextVariableNodeF()
+*/
 
 #ifdef NDEBUG
-#define NamePointer(n) ((n)->nptr)
+# define NamePointer(n) ((n)->nptr)
 #else
-#define NamePointer(n) NamePointerF(n)
+extern CONST struct Name *NamePointerF(CONST struct VariableList *vl);
+/**<
+ *  Implementation function for NamePointer() (debug mode).
+ *  Do not call this function directly - use NamePointer() instead.
+ */
+# define NamePointer(n) NamePointerF(n)
 #endif
 /**<
  *  Return the name pointer stored in node vl.
@@ -96,56 +85,36 @@ extern CONST struct VariableList
  *  @return The name as a CONST struct Name*.
  *  @see NamePointerF()
  */
-extern CONST struct Name *NamePointerF(CONST struct VariableList *vl);
-/**<
- *  <!--  struct Name *NamePointer(vl)                                 -->
- *  <!--  struct VariableList *vl;                                     -->
- *  <!--  Return the name pointer stored in node vl.                   -->
- *  Implementation function for NamePointer() (debug mode).
- *  Do not call this function directly - use NamePointer() instead.
- */
 
 extern struct VariableList *CopyVariableList(struct VariableList *vl);
 /**< 
- *  <!--  struct VariableList *CopyVariableList(vl)                    -->
- *  <!--  struct VariableList *vl;                                     -->
  *  Make and return a copy of the whole name list.
  */
 
 extern void DestroyVariableList(struct VariableList *vl);
 /**< 
- *  <!--  void DestroyVariableList(vl)                                 -->
- *  <!--  struct VariableList *vl;                                     -->
  *  Deallocate the memory associated with the list of names.
  */
 
 extern void DestroyVariableListNode(struct VariableList *vl);
 /**< 
- *  <!--  void DestroyVariableListNode(vl)                             -->
- *  <!--  struct VariableList *vl;                                     -->
  *  Deallocate this name node, and don't change the next one.
  */
 
 extern struct VariableList
 *JoinVariableLists(struct VariableList *vl1, struct VariableList *vl2);
 /**< 
- *  <!--  struct VariableList *JoinVariableLists(vl1,vl2)              -->
- *  <!--  struct VariableList *vl1,*vl2;                               -->
  *  Appends vl2 to the end of vl1.  This will return vl1, unless vl1 is NULL
  *  in which case it will return vl2.
  */
 
 extern struct VariableList *ReverseVariableList(struct VariableList *vl);
 /**< 
- *  <!--  struct VariableList *ReverseVariableList(vl)                 -->
- *  <!--  struct VariableList *vl;                                     -->
  *  Returns the reverse of vl.
  */
 
 extern unsigned long VariableListLength(CONST struct VariableList *vl);
 /**< 
- *  <!--  unsigned long VariableListLength(vl);                        -->
- *  <!--  CONST struct VariableList *vl;                               -->
  *  Returns the length of the variable list. Does not attempt to expand arrays
  *  of variables or anything like that.
  */
@@ -153,10 +122,8 @@ extern unsigned long VariableListLength(CONST struct VariableList *vl);
 extern int CompareVariableLists(CONST struct VariableList *vl1,
                                 CONST struct VariableList *vl2);
 /**<
- *  <!--  int CompareVariableLists(vl1,vl2);                           -->
  *  Returns -1,0,1 as vl1 is < == > vl2 in content.
  *  The NULL list is > all lists.
  */
 
-#endif /* __VLIST_H_SEEN__ */
-
+#endif /* ASC_VLIST_H */
