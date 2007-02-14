282cfa03a38f86f7c887138f1c2428905cfa5aa9
John Pye <john.pye@anu.edu.au>
2007-02-14 14:22:31 +0000

    Some GPL headers cleaned up. #if 0'd some (stated-to-be) futuristic code. Removed some <!-- comments. Added defaultall.c, which will be a new package METHOD similar to ClearAll.
    git-svn-id: https://svn.ascend4.org/trunk@1282 f15cd148-d149-4907-926e-73cf92b3ee03

--- base/generic/packages/ascFreeAllVars.c
+++ base/generic/packages/ascFreeAllVars.c
@@ -1,58 +1,41 @@
-/*
- *  ascFreeAllVars.c
- *  by Ben Allan
- *  February 24, 1998
- *  Part of ASCEND
- *  Version: $Revision: 1.4 $
- *  Version control file: $RCSfile: ascFreeAllVars.c,v $
- *  Date last modified: $Date: 1998/06/16 16:42:09 $
- *  Last modified by: $Author: mthomas $
- *
- *  This file is part of the Ascend Language Interpreter.
- *
- *  Copyright (C) 1998 Carnegie Mellon University
- *
- *  The Ascend Language Interpreter is free software; you can
- *  redistribute it and/or modify it under the terms of the GNU
- *  General Public License as published by the Free Software
- *  Foundation; either version 2 of the License, or (at your option)
- *  any later version.
- *
- *  The Ascend Language Interpreter is distributed in hope that it
- *  will be useful, but WITHOUT ANY WARRANTY; without even the implied
- *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with the program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139 USA.  Check
- *  the file named COPYING.
- */
+/*	ASCEND modelling environment
+	Copyright (C) 1998 Carnegie Mellon University
+	Copyright (C) 2007 Carnegie Mellon University
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330,
+	Boston, MA 02111-1307, USA.
+*//** @file
+	The following functions give an alternative to the recursive clear
+	procedure found older ASCEND models.
+*//*
+	by Ben Allan, February 24, 1998
+	Last in CVS: $Revision: 1.4 $ $Date: 1998/06/16 16:42:09 $ $Author: mthomas $
+*/
 
 #include <utilities/ascConfig.h>
 #include <utilities/ascPrint.h>
-#include <general/list.h>
-
-
 
 #include <compiler/child.h>
 #include <compiler/type_desc.h>
 #include <compiler/symtab.h>
-#include <compiler/instance_enum.h>
 #include <compiler/instquery.h>
 #include <compiler/atomvalue.h>
 #include <compiler/visitinst.h>
-#include <compiler/extfunc.h>
 #include <compiler/parentchild.h>
 #include <compiler/library.h>
 #include <packages/ascFreeAllVars.h>
-
-
-/*
- * The following functions give an alternative
- * to the recursive clear procedure found in most
- * models.
- */
 
 struct cvpacket {
   struct TypeDescription *g_solver_var_type;
