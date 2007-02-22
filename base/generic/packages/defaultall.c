8b7aeb90318558593f54aef0b81952f8425202c0
johnpye <johnpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2007-02-22 14:08:25 +0000

    Added asc_default_self to packages
    git-svn-id: https://svn.ascend4.org/trunk@1289 f15cd148-d149-4907-926e-73cf92b3ee03

--- base/generic/packages/defaultall.c
+++ base/generic/packages/defaultall.c
@@ -16,14 +16,16 @@
 	Foundation, Inc., 59 Temple Place - Suite 330,
 	Boston, MA 02111-1307, USA.
 *//** @file
-	The following functions give an alternative to the recursive default_self
-	procedures found in standard ASCEND models.  EXPERIMENTAL.
+	The following functions give an automatic, default form for the
+	'default_all' and 'default_self' methods usually written as explicit
+	METHODs in a model. EXPERIMENTAL.
 *//*
 	by John Pye, 15 Feb 2007.
 */
 
 #include <utilities/ascConfig.h>
 #include <utilities/ascPrint.h>
+#include <utilities/ascPanic.h>
 
 #include <compiler/child.h>
 #include <compiler/type_desc.h>
@@ -36,17 +38,46 @@
 #include <packages/ascFreeAllVars.h>
 
 /**
-	Recursive default 'default' routine.
-
-	If the current instance is a MODEL and it has a 'default' method, we
-	run it and return.
-
-	Otherwise, loop through child atoms in the current model. For the children,
-		* for atoms, set to default values, if defaults are known.
-		* for compound types, recurse.
+	Find atom children in the present model and set them to their ATOM DEFAULT
+	values.
 */
 static
-void Asc_RecursiveDefault(struct Instance *inst){
+int Asc_DefaultSelf1(struct Instance *inst){
+	int i,n;
+	struct Instance *c;
+	struct TypeDescription *type;	
+
+	/* default any child atoms' values */
+	n = NumberChildren(inst);
+	for(i = 1; i <= n; ++i){
+		CONSOLE_DEBUG("Child %d...", i);
+		c = InstanceChild(inst,i);
+		type = InstanceTypeDesc(c);
+		if(BaseTypeIsAtomic(type)){
+			if(!AtomDefaulted(type))continue;
+			switch(GetBaseType(type)){
+				case real_type: SetRealAtomValue(c, GetRealDefault(type), 0); break;
+				case integer_type: SetIntegerAtomValue(c, GetIntDefault(type), 0); break;
+				case boolean_type: SetBooleanAtomValue(c, GetBoolDefault(type), 0); break;
+				case symbol_type: SetSymbolAtomValue(c, GetSymDefault(type)); break;
+				case set_type: /* what is the mechanism for defaulting of sets? */
+				default: ASC_PANIC("invalid type");
+			}
+			CONSOLE_DEBUG("Reset atom to default value");
+		}else if(GetBaseType(type)==array_type){
+			/* descend into arrays */
+			Asc_DefaultSelf1(c);
+		}
+	}
+	return 0;
+}
+
+#if 0
+/**
+	Find child models in the present model and run their 'default_all' methods
+*/
+static
+void Asc_DefaultAll(struct Instance *inst){
 	int i, n;
 
 	struct Instance *c;
@@ -98,4 +129,19 @@ int Asc_DefaultAllVars(struct Instance *root, struct gl_list_t *arglist, void *u
 			(struct Instance *)gl_fetch((struct gl_list_t *)gl_fetch(arglist,1),1)
 		);
 	}
+}
+#endif
+
+
+
+int Asc_DefaultSelf(struct Instance *root, struct gl_list_t *arglist, void *userdata){
+  /* arglist is a list of gllist of instances */
+  if (arglist == NULL ||
+      gl_length(arglist) == 0L ||
+      gl_length((struct gl_list_t *)gl_fetch(arglist,1)) != 1 ||
+      gl_fetch((struct gl_list_t *)gl_fetch(arglist,1),1) == NULL) {
+    return Asc_DefaultSelf1(root);
+  }else{
+    return Asc_DefaultSelf1((struct Instance *)gl_fetch( (struct gl_list_t *)gl_fetch(arglist,1),1 ));
+  }
 }
---
+++ base/generic/packages/defaultall.h
@@ -0,0 +1,34 @@
+/*	ASCEND modelling environment
+	Copyright (C) 1998 Carnegie Mellon University
+	Copyright (C) 2006, 2007 Carnegie Mellon University
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330,
+	Boston, MA 02111-1307, USA.
+*//**
+	@file
+	Variable default-value routines, for implementation of METHOD default_all.
+*//*
+	by John Pye, Feb 2007
+*/
+
+#ifndef ASC_DEFAULTALL_H
+#define ASC_DEFAULTALL_H
+
+#include <compiler/compiler.h>
+#include <general/list.h>
+
+int Asc_DefaultSelf(struct Instance *root, struct gl_list_t *arglist, void *userdata);
+
+#endif /* ASC_DEFAULTALL_H */
