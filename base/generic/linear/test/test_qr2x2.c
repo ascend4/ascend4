45e9ebe688d25ce4702e5187f42635ce4d0c6903
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2007-03-06 13:40:44 +0000

    Test case to assure that linsol_rank works correctly for 1x1, 2x2 and 3x3 mtxs.
    git-svn-id: https://svn.ascend4.org/trunk@1323 f15cd148-d149-4907-926e-73cf92b3ee03

--- base/generic/linear/test/test_qr2x2.c
+++ base/generic/linear/test/test_qr2x2.c
@@ -27,6 +27,49 @@
 
 #include <assertimpl.h>
 
+/*
+	Test the rank of matrix 
+
+	[ 1 ]
+*/
+static void test_qr1x1(void){
+	linsolqr_system_t L;
+	mtx_matrix_t M;
+	mtx_coord_t C;
+	mtx_range_t R;
+	mtx_region_t G;
+	int r;
+
+	/* build the matrix [[1,0],[1,1]] */
+	M = mtx_create();
+	mtx_set_order(M,1);
+	mtx_set_value(M,mtx_coord(&C,0,0), 1.0);
+
+	/* construct a 'whole matrix' region (can't use mtx_ENTIRE_MATRIX with linsolqr_set_region) */
+	R.low = 0;
+	R.high = mtx_order(M) - 1;
+	G.row = R;
+	G.col = R;
+
+	mtx_write_region_mmio(stderr,M,&G);
+
+	L = linsolqr_create_default();
+	linsolqr_set_matrix(L,M);
+	linsolqr_set_region(L,G);
+	linsolqr_prep(L,linsolqr_fmethod_to_fclass(linsolqr_fmethod(L)));
+	linsolqr_reorder(L, &G, linsolqr_rmethod(L));
+	linsolqr_factor(L,linsolqr_fmethod(L));
+	r = linsolqr_rank(L);
+
+	CU_ASSERT(r==1);
+}
+
+/*
+	Test the rank of matrix 
+
+	[ 1 0
+	  1 1 ]
+*/
 static void test_qr2x2(void){
 	linsolqr_system_t L;
 	mtx_matrix_t M;
@@ -54,23 +97,70 @@ static void test_qr2x2(void){
 	linsolqr_set_matrix(L,M);
 	linsolqr_set_region(L,G);
 	linsolqr_prep(L,linsolqr_fmethod_to_fclass(linsolqr_fmethod(L)));
-	linsolqr_reorder(L, &R, linsolqr_rmethod(L));
+	linsolqr_reorder(L, &G, linsolqr_rmethod(L));
 	linsolqr_factor(L,linsolqr_fmethod(L));
 	r = linsolqr_rank(L);
 
 	CU_ASSERT(r==2);
 }
 
+/*
+	Test the rank of matrix 
+
+	[ 2 0 1
+	  1 1 1 
+	  0 1 1 ]
+*/
+static void test_qr3x3(void){
+	linsolqr_system_t L;
+	mtx_matrix_t M;
+	mtx_coord_t C;
+	mtx_range_t R;
+	mtx_region_t G;
+	int r;
+
+	/* build the matrix [[1,0],[1,1]] */
+	M = mtx_create();
+	mtx_set_order(M,3);
+	mtx_set_value(M,mtx_coord(&C,0,0), 2.0);
+	mtx_set_value(M,mtx_coord(&C,1,1), 1.0);
+	mtx_set_value(M,mtx_coord(&C,2,2), 1.0);
+	mtx_set_value(M,mtx_coord(&C,0,2), 1.0);
+	mtx_set_value(M,mtx_coord(&C,1,0), 1.0);
+	mtx_set_value(M,mtx_coord(&C,1,2), 1.0);
+	mtx_set_value(M,mtx_coord(&C,2,1), 1.0);
+
+	/* construct a 'whole matrix' region (can't use mtx_ENTIRE_MATRIX with linsolqr_set_region) */
+	R.low = 0;
+	R.high = mtx_order(M) - 1;
+	G.row = R;
+	G.col = R;
+
+	mtx_write_region_mmio(stderr,M,&G);
+
+	L = linsolqr_create_default();
+	linsolqr_set_matrix(L,M);
+	linsolqr_set_region(L,G);
+	linsolqr_prep(L,linsolqr_fmethod_to_fclass(linsolqr_fmethod(L)));
+	linsolqr_reorder(L, &G, linsolqr_rmethod(L));
+	linsolqr_factor(L,linsolqr_fmethod(L));
+	r = linsolqr_rank(L);
+
+	CU_ASSERT(r==3);
+}
+
 /*===========================================================================*/
 /* Registration information */
 
-static CU_TestInfo qr2x2_test_list[] = {
+static CU_TestInfo qr_test_list[] = {
+	{"test_qr1x1", test_qr1x1},
 	{"test_qr2x2", test_qr2x2},
+	{"test_qr3x3", test_qr3x3},
 	CU_TEST_INFO_NULL
 };
 
 static CU_SuiteInfo suites[] = {
-	{"test_linear_qr2x2", NULL, NULL, qr2x2_test_list},
+	{"test_linear_qr", NULL, NULL, qr_test_list},
 	CU_SUITE_INFO_NULL
 };
 
