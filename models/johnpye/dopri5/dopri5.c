1c3e278d9af10791e9949244d264cf31a96f1227
John Pye <john.pye@anu.edu.au>
2007-05-28 10:20:09 +0000

    Some more work on the DOPRI5 integrator.
    git-svn-id: https://svn.ascend4.org/trunk@1451 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/johnpye/dopri5/dopri5.c
+++ models/johnpye/dopri5/dopri5.c
@@ -102,7 +102,9 @@ static double max_d (double a, double b){
 
 static double hinit (unsigned n, FcnEqDiff *fcn, double x, double* y,
                      double posneg, double* f0, double* f1, double* yy1, int iord,
-                     double hmax, double* atoler, double* rtoler, int itoler){
+                     double hmax, double* atoler, double* rtoler, int itoler
+		,void *user_data
+){
     double   dnf, dny, atoli, rtoli, sk, h, h1, der2, der12, sqr;
     unsigned i;
 
@@ -141,7 +143,7 @@ static double hinit (unsigned n, FcnEqDiff *fcn, double x, double* y,
     /* perform an explicit Euler step */
     for (i = 0; i < n; i++)
         yy1[i] = y[i] + h * f0[i];
-    fcn (n, x+h, yy1, f1);
+    fcn (n, x+h, yy1, f1, user_data);
 
     /* estimate the second derivative of the solution */
     der2 = 0.0;
@@ -176,10 +178,12 @@ static double hinit (unsigned n, FcnEqDiff *fcn, double x, double* y,
 
 /* core integrator */
 static int dopcor (unsigned n, FcnEqDiff *fcn, double x, double* y, double xend,
-                   double hmax, double h, double* rtoler, double* atoler,
-                   int itoler, FILE* fileout, SolTrait *solout, int iout,
-                   long nmax, double uround, int meth, long nstiff, double safe,
-                   double beta, double fac1, double fac2, unsigned* icont){
+		double hmax, double h, double* rtoler, double* atoler,
+		int itoler, FILE* fileout, SolTrait *solout, int iout,
+		long nmax, double uround, int meth, long nstiff, double safe,
+		double beta, double fac1, double fac2, unsigned* icont
+		, void *user_data	
+){
     double   facold, expo1, fac, facc1, facc2, fac11, posneg, xph;
     double   atoli, rtoli, hlamb, err, sk, hnew, yd0, ydiff, bspl;
     double   stnum, stden, sqr;
@@ -224,11 +228,11 @@ static int dopcor (unsigned n, FcnEqDiff *fcn, double x, double* y, double xend,
     last  = 0;
     hlamb = 0.0;
     iasti = 0;
-    fcn(n, x, y, k1);
+    fcn(n, x, y, k1, user_data);
     hmax = fabs (hmax);
     iord = 5;
     if (h == 0.0)
-        h = hinit (n, fcn, x, y, posneg, k1, k2, k3, iord, hmax, atoler, rtoler, itoler);
+        h = hinit (n, fcn, x, y, posneg, k1, k2, k3, iord, hmax, atoler, rtoler, itoler, user_data);
     nfcn += 2;
     reject = 0;
     xold = x;
@@ -237,7 +241,7 @@ static int dopcor (unsigned n, FcnEqDiff *fcn, double x, double* y, double xend,
         irtrn = 1;
         hout = h;
         xout = x;
-        solout (naccpt+1, xold, x, y, n, &irtrn);
+        solout (naccpt+1, xold, x, y, n, &irtrn, user_data);
         if (irtrn < 0)
         {
             if (fileout)
@@ -278,23 +282,23 @@ static int dopcor (unsigned n, FcnEqDiff *fcn, double x, double* y, double xend,
         /* the first 6 stages */
         for (i = 0; i < n; i++)
             yy1[i] = y[i] + h * a21 * k1[i];
-        fcn (n, x+c2*h, yy1, k2);
+        fcn (n, x+c2*h, yy1, k2, user_data);
         for (i = 0; i < n; i++)
             yy1[i] = y[i] + h * (a31*k1[i] + a32*k2[i]);
-        fcn (n, x+c3*h, yy1, k3);
+        fcn (n, x+c3*h, yy1, k3, user_data);
         for (i = 0; i < n; i++)
             yy1[i] = y[i] + h * (a41*k1[i] + a42*k2[i] + a43*k3[i]);
-        fcn (n, x+c4*h, yy1, k4);
+        fcn (n, x+c4*h, yy1, k4, user_data);
         for (i = 0; i <n; i++)
             yy1[i] = y[i] + h * (a51*k1[i] + a52*k2[i] + a53*k3[i] + a54*k4[i]);
-        fcn (n, x+c5*h, yy1, k5);
+        fcn (n, x+c5*h, yy1, k5, user_data);
         for (i = 0; i < n; i++)
             ysti[i] = y[i] + h * (a61*k1[i] + a62*k2[i] + a63*k3[i] + a64*k4[i] + a65*k5[i]);
         xph = x + h;
-        fcn (n, xph, ysti, k6);
+        fcn (n, xph, ysti, k6, user_data);
         for (i = 0; i < n; i++)
             yy1[i] = y[i] + h * (a71*k1[i] + a73*k3[i] + a74*k4[i] + a75*k5[i] + a76*k6[i]);
-        fcn (n, xph, yy1, k2);
+        fcn (n, xph, yy1, k2, user_data);
         if (iout == 2){
             if (nrds == n){
                 for (i = 0; i < n; i++)
@@ -418,7 +422,7 @@ static int dopcor (unsigned n, FcnEqDiff *fcn, double x, double* y, double xend,
             {
                 hout = h;
                 xout = x;
-                solout (naccpt+1, xold, x, y, n, &irtrn);
+                solout (naccpt+1, xold, x, y, n, &irtrn, user_data);
                 if (irtrn < 0)
                 {
                     if (fileout)
@@ -464,6 +468,7 @@ int dopri5(
 	double* atoler, int itoler, SolTrait *solout, int iout, FILE* fileout, double uround,
 	double safe, double fac1, double fac2, double beta, double hmax, double h,
 	long nmax, int meth, long nstiff, unsigned nrdens, unsigned* icont, unsigned licont
+	, void *user_data
 ){
     int       arret, idid;
     unsigned  i;
@@ -660,8 +665,10 @@ int dopri5(
     }
     else
     {
-        idid = dopcor (n, fcn, x, y, xend, hmax, h, rtoler, atoler, itoler, fileout,
-                       solout, iout, nmax, uround, meth, nstiff, safe, beta, fac1, fac2, icont);
+        idid = dopcor (n, fcn, x, y, xend, hmax, h, rtoler, atoler, itoler, fileout
+            , solout, iout, nmax, uround, meth, nstiff, safe, beta, fac1, fac2, icont
+			, user_data
+		);
         free (ysti);
         free (k6);
         free (k5);    /* reverse order freeing too increase chances */
