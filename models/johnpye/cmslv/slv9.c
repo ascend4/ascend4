1e4e7351e4839ef80ca8862d5abe45403b4dc16c
John Pye <john.pye@anu.edu.au>
2007-06-10 13:53:10 +0000

    Fixing <solver/conopt.h>
    git-svn-id: https://svn.ascend4.org/trunk@1492 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/johnpye/cmslv/slv9.c
+++ models/johnpye/cmslv/slv9.c
@@ -55,7 +55,7 @@ typedef struct slv9_system_structure *slv9_system_t;
 ASC_DLLSPEC SolverRegisterFn cmslv_register;
 
 #ifdef ASC_WITH_CONOPT
-# include "../conopt/conopt.h"
+# include <solver/conopt.h>
 #else
 # define MAX_INT MAXINT
 # define MAX_REAL MAXDOUBLE
