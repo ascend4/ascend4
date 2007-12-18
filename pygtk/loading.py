61cac78a5663bacb7cac44d729474a73ca4d40ee
John Pye <john.pye@anu.edu.au>
2007-12-18 02:33:50 +0000

    Fixed loading for case of missing ascend-loading.svg
    git-svn-id: https://svn.ascend4.org/trunk@1672 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/loading.py
+++ pygtk/loading.py
@@ -2,40 +2,22 @@ import sys
 import config
 import os.path
 
+global have_gtk
+have_gtk = False
 try:
 	import pygtk 
 	pygtk.require('2.0') 
 	import gtk
-	is_loading = True
+	have_gtk = True
 except:
 	print "PyGTK COULD NOT BE LOADED (is it installed? do you have X-Windows running?)"
-	is_loading = False
+	sys.exit(1)
 
+global _messages
 _messages = []
 
 def get_messages():
 	return _messages
-
-def print_status(status,msg=None):
-	if is_loading:
-		print status
-		loadinglabel.set_text(status)
-		if msg is not None:
-			sys.stderr.write(msg+"\n")
-			_messages.append(msg)
-		while gtk.events_pending():
-			gtk.main_iteration(False)
-	else:
-		sys.stderr.write("\r                                                 \r")
-		if msg!=None:
-			sys.stderr.write(msg+"\n")
-			_messages.append(msg)
-		sys.stderr.write(status+"...\r")
-		sys.stderr.flush()
-
-def complete():
-	loadingwindow.destroy()
-	_is_loading = False
 
 def load_matplotlib(throw=False,alert=False):
 	print_status("Loading python matplotlib")
@@ -76,29 +58,86 @@ def load_matplotlib(throw=False,alert=False):
 		if throw:
 			raise RuntimeError("Failed to load plotting library 'matplotlib'. (%s)" % str(e))
 
-def create_window():
-	_w = gtk.Window(gtk.WINDOW_TOPLEVEL)
-	_w.set_decorated(False)
-	_w.set_position(gtk.WIN_POS_CENTER)
-	_a = gtk.Alignment()
-	_a.set_padding(4,4,4,4)
-	_w.add(_a)
-	_a.show()
-	_v = gtk.VBox()
-	_a.add(_v)
-	_v.show()
-	_i = gtk.Image()
-	_i.set_pixel_size(3)
-	_i.set_from_file(os.path.join(config.PYGTK_ASSETS,'ascend-loading.svg'))
-	_v.add(_i)
-	_i.show()
-	_l = gtk.Label("Loading ASCEND...")
-	_l.set_justify(gtk.JUSTIFY_CENTER)
-	_v.add(_l)
-	_l.show()
-	_w.show()
-	return _w, _l
 
-loadingwindow, loadinglabel = create_window()
-is_loading = True
+class LoadingWindow:
+	def __init__(self):
+		self.is_loading = False
+		self.set_assets_dir(config.PYGTK_ASSETS)
 
+	def set_assets_dir(self, d):
+		self.assetsdir = d
+		self.splashfile = os.path.join(self.assetsdir,'ascend-loading.svg')
+
+	def create_window(self):
+		if have_gtk:
+			if os.path.exists(self.splashfile):
+				_w = gtk.Window(gtk.WINDOW_TOPLEVEL)
+				_w.set_decorated(False)
+				_w.set_position(gtk.WIN_POS_CENTER)
+				_a = gtk.Alignment()
+				_a.set_padding(4,4,4,4)
+				_w.add(_a)
+				_a.show()
+				_v = gtk.VBox()
+				_a.add(_v)
+				_v.show()
+				_i = gtk.Image()
+				self.image = _i
+				_i.set_pixel_size(3)
+				_i.set_from_file(self.splashfile)
+				_v.add(_i)
+				_i.show()
+				_l = gtk.Label("Loading ASCEND...")
+				_l.set_justify(gtk.JUSTIFY_CENTER)
+				_v.add(_l)
+				_l.show()
+				_w.show()
+				self.window = _w
+				self.label = _l
+				self.is_loading = True
+				while gtk.events_pending():
+					gtk.main_iteration(False)
+			else:
+				pass
+				#do nothing, don't know where splash file is yet
+		else:
+			print "DON'T HAVE GTK!"
+			sys.exit(1)
+	
+	def print_status(self,status,msg=None):
+		print "STATUS = ",status
+		if self.is_loading:
+			print status
+			self.label.set_text(status)
+			if msg is not None:
+				sys.stderr.write(msg+"\n")
+				_messages.append(msg)
+			while gtk.events_pending():
+				gtk.main_iteration(False)
+		else:
+			sys.stderr.write("\r                                                 \r")
+			if msg!=None:
+				sys.stderr.write(msg+"\n")
+				_messages.append(msg)
+			sys.stderr.write(status+"...\r")
+			sys.stderr.flush()
+
+	def complete(self):
+		if self.is_loading:
+			self.window.destroy()
+		self.is_loading = False
+
+global w
+
+def print_status(status,msg=None):
+	w.print_status(status,msg)
+
+def complete():
+	w.complete()
+
+def create_window(assetsdir=config.PYGTK_ASSETS):
+	w.set_assets_dir(assetsdir)
+	w.create_window()
+
+w = LoadingWindow()
+create_window()
