20ec09db03b203238f5ae42ff7f60a9414f794de
John Pye <john.pye@anu.edu.au>
2007-12-18 03:47:55 +0000

    Fixed incidence matrix coloring for case where overspecified rows are presuent.
    git-svn-id: https://svn.ascend4.org/trunk@1674 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/incidencematrix.py
+++ pygtk/incidencematrix.py
@@ -22,28 +22,46 @@ class IncidenceMatrixWindow:
 		# convert incidence map to pylab numarray type:
 		_id = self.im.getIncidenceData();
 
-		self.data = pylab.zeros((self.im.getNumRows(), self.im.getNumCols(), ))*0.
+		self.data = pylab.zeros((self.im.getNumRows(), self.im.getNumCols(), ))
 		for i in _id:
 			self.data[i.row, i.col] = int(i.type)
 
 		del(_id)
 
-		# prepare colour map
-		cmapdata = {
-			          # type = 0     type = 1       type = 2
-			          # norelation   active fixed   active free
-			'red'  :  ((0., 1., 1.), (0.5, 0., 0.), (1., 0., 0.)),
-			'green':  ((0., 1., 1.), (0.5, 1., 1.), (1., 0., 0.)),
-			'blue' :  ((0., 1., 1.), (0.5, 0., 0.), (1., 0.3, 0.3))
+		colors = {
+			0: (1.,1.,1.)   # IM_NULL
+			,1: (0.,1.,0.)  # IM_ACTIVE_FIXED
+			,2: (0.,0.,0.3) # IM_ACTIVE_FREE
+			,3: (1.,0.5,0.)  # IM_DORMANT_FIXED
+			,4: (1.,0.,0.)  # IM_DORMANT_FREE
 		}
 
-		_im_cmap =  LinearSegmentedColormap('im_cmap',  cmapdata, 4)
+		red = []
+		green = []
+		blue = []	
+		n = 4
+		for k,v in colors.iteritems():
+			red.append((float(k)/n, v[0], v[0]))
+			green.append((float(k)/n, v[1], v[1]))
+			blue.append((float(k)/n, v[2], v[2]))
+
+		cmapdata = {'red':tuple(red), 'green':tuple(green), 'blue':tuple(blue)}
+
+		# prepare colour map
+#		cmapdata = {
+#			          # IM_NULL      IM_ACTIVE_FIXED IM_DORMANT_FREE IM_DORMANT_FIXED IM_ACTIVE_FREE
+#			'red'  :  ((0., 1., 1.), (1., 0., 0.), (xx,                            (0.5, 0., 0.)),
+#			'green':  ((0., 1., 1.), (1., 0., 0.)                                   , (0.5, 1., 1.)),
+#			'blue' :  ((0., 1., 1.), (1., 0.3, 0.3)                                 , (0.5, 0., 0.))
+#		}
+
+		_im_cmap =  LinearSegmentedColormap('im_cmap',  cmapdata, n+1)
 
 		pylab.ioff()
 		pylab.figure()
 		ax = pylab.subplot(111)
 		ax.axis('equal') # aspect ratio = 1.0
-		ax.imshow(self.data, cmap=_im_cmap, interpolation='nearest') 
+		ax.imshow(self.data, cmap=_im_cmap, interpolation='nearest',vmin=0, vmax=n) 
 			# integer 'type' values become reals 0..1, which are then coloured
 			# according to cmapdata
 		pylab.title("Incidence Matrix")
