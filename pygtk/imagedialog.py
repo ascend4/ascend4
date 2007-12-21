686445d26f7b3eac8ff79f79fed6b13b02ccfb2a
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2007-12-21 05:33:43 +0000

    Added ImageWindow for viewing Incidence Graph, added zoom and fit-to-window features.
    git-svn-id: https://svn.ascend4.org/trunk@1685 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/imagedialog.py
+++ pygtk/imagedialog.py
@@ -7,6 +7,7 @@ from varentry import *
 class ImageDialog:
 	def __init__(self,browser,parent,imagefilename,title):
 		self.browser = browser;
+		self.imagefilename = imagefilename
 
 		# GUI config
 		_xml = gtk.glade.XML(browser.glade_file,"imagedialog")
@@ -32,6 +33,13 @@ class ImageDialog:
 		self.vbox.add(s)
 
 		_xml.signal_autoconnect(self)
+
+	def on_savebutton_clicked(self,*args):
+		self.browser.reporter.reportNote("SAVE %s" % self.imagefilename)
+		self.window.response(gtk.RESPONSE_NONE)
+
+	def on_zoom_change_value(self,*args):
+		self.browser.reporter.reportNote("ZOOM");
 
 	def on_infodialog_close(self,*args):
 		self.window.response(gtk.RESPONSE_CLOSE);
