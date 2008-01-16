7eb2a0abd7d3c13a65af167c71e0bfc344f4f3e2
kchittur <kchittur@f15cd148-d149-4907-926e-73cf92b3ee03>
2008-01-16 01:01:07 +0000

    Minor changes to eos-vanderwaals.a4c and zplot.py
    git-svn-id: https://svn.ascend4.org/trunk@1728 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/kchittur/zplot.py
+++ models/kchittur/zplot.py
@@ -1,11 +1,50 @@
 import extpy
-
-try:
-	from pylab import *
-except:
-	pass
-
+from pylab import *
 from solverreporter import *
+
+def pvplot(self):
+
+	browser = extpy.getbrowser()
+	ioff()
+	figure()
+#
+# I have chosen several temperatures 
+#
+	for T in [190,210,230,250,270,290,310,330,350,370,400, 420]:
+		self.T.setRealValue(T)
+#
+# collect the data for plotting in two sets of arrays (one for X, one for Y)
+# I have one set here - for P versus V at different T's  
+#
+		XX1 = []
+		YY1 = []
+#
+# there has to be a space between "in" and "["
+#
+
+		for P in [8,9,10,15,20,25,30,40,50,60,70,80,100,120,140,150,170,190]:
+		    self.P.setRealValueWithUnits(P,"bar")
+#
+# send the pair of values P, T to the solver
+# and append the Volume and Pressure from the solver to the arrays 
+		    try:
+				browser.sim.solve(browser.solver,SimpleSolverReporter(browser,message="T = %f, P = %f" % (T,P)))
+				XX1.append(float(self.V))
+				YY1.append(float(self.P))
+		    except:
+		        browser.reporter.reportError('Failed to solve for P = %f' % P)
+		        continue
+## plot the data 
+
+		plot(XX1,YY1)
+
+		hold(1)
+	
+##	legend()
+	ion()
+	show()
+
+extpy.registermethod(pvplot)
 
 def zplot(self):
 
