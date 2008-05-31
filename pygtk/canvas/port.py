d87e10cccdb956630d4630e908e1f30d3fc15de9
John Pye <john.pye@anu.edu.au>
2008-05-31 08:28:39 +0000

    Removed unused bits of code. Trying to debug constraints.
    git-svn-id: https://svn.ascend4.org/trunk@1783 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/canvas/port.py
+++ pygtk/canvas/port.py
@@ -6,7 +6,7 @@ import gtk
 import cairo
 
 from gaphas.item import Item
-from gaphas.state import observed, reversible_property
+from gaphas.state import observed, reversible_property, disable_dispatching
 from gaphas.tool import HandleTool
 
 from gaphas import Canvas, GtkView, View
@@ -60,12 +60,28 @@ class Port(object):
     
     """
 
-    x = solvable()
-    y = solvable()
+    _x = solvable()
+    _y = solvable()
 
-    def __init__(self, block):
+    def __init__(self, block, strength = STRONG):
         self.block = block
+        self._x.strength = strength
+        self._y.strength = strength
         self._connectable = True
+
+    @observed
+    def _set_x(self, x):
+        self._x = x
+
+    x = reversible_property(lambda s: s._x, _set_x, bind={'x': lambda self: float(self.x) })
+    disable_dispatching(_set_x)
+
+    @observed
+    def _set_y(self, y):
+        self._y = y
+
+    y = reversible_property(lambda s: s._y, _set_y, bind={'y': lambda self: float(self.y) })
+    disable_dispatching(_set_y)
 
     @observed
     def _set_connectable(self, connectable):
@@ -140,7 +156,7 @@ class PointConstraint(Constraint):
         for k in match:
             if var is self._variables[k]:
                 print "Updating variable %d to equal value of variable %d" % (k,match[k])
-                _update(self._variables[k], self._variables[match[k]].value)
+                _update(self._variables[match[k]], self._variables[k].value)
                 return
         raise AssertionError("wrong variable in solve_for")
 
@@ -176,7 +192,10 @@ class Block(Element):
         phalfsize = 3
         for p in self.ports:
             c.rectangle(p.x - phalfsize, p.y - phalfsize, 2*phalfsize, 2*phalfsize)
-            c.set_source_rgba(0.8,0.8,1, 0.8)
+            if p.connectable:
+                c.set_source_rgba(0.8,0.8,1, 0.8)
+            else:
+                c.set_source_rgba(1,0,0,1)
             c.fill_preserve()
             c.set_source_rgb(0.8,0.8,0)
             c.stroke()
@@ -200,7 +219,7 @@ class DefaultBlock(Block):
     box with the input ports on the left and the output ports on the right.
     """
 
-    def __init__(self, label="unnamed", width=10, height=10, inputs=2, outputs=2):
+    def __init__(self, label="unnamed", width=10, height=10, inputs=2, outputs=3):
 
         super(DefaultBlock, self).__init__(label, width, height)
 
@@ -265,8 +284,12 @@ class PortConnectingHandleTool(HandleTool):
         glue_distance = 10
         glue_port = None
         glue_point = None
+        print "Gluing..."
         for i in view.canvas.get_all_items():
+            if not hasattr(i,'ports'):
+                continue
             if not i is item:
+                print "Trying glue to",i
                 v2i = view.get_matrix_v2i(i).transform_point
                 ix, iy = v2i(wx, wy)
                 try:
@@ -299,49 +322,35 @@ class PortConnectingHandleTool(HandleTool):
         # create a special local handle_disconnect function 
         def handle_disconnect():
             try:
-                view.canvas.solver.remove_constraint(handle._connect_constraint)
+                view.canvas.solver.remove_constraint(handle.connection_data)
             except KeyError:
                 print 'constraint was already removed for', item, handle
                 pass # constraint was alreasy removed
             else:
                 print 'constraint removed for', item, handle
-            handle._connect_constraint = None
+            handle.connected_to.connectable = True
+            handle.connection_data = None
             handle.connected_to = None
+            
             # Remove disconnect handler:
             handle.disconnect = lambda: 0
 
         #print 'Handle.connect', view, item, handle, wx, wy
         glue_port = self.glue(view, item, handle, wx, wy)
 
+        if glue_port:
+            if glue_port is handle.connected_to:    
+                hand
+
         if glue_port and glue_port is handle.connected_to:
             try:
-                view.canvas.solver.remove_constraint(handle._connect_constraint)
+                view.canvas.solver.remove_constraint(handle.connection_data)
             except KeyError:
-                pass # constraint was already removed
-
-                print "handle.pos =",handle.pos
-                print "glue_port =",glue_port
-                print "glue_port.pos = ",glue_port.pos
-                print "glue_port.block =",glue_port.block
-                p2 = CanvasProjection(glue_port.pos, glue_port.block)     
-                print "connect: p2 =",p2
-                print "connect: p2[0] =",p2[0].variable()
-                print "connect: p2[0].strength =",p2[0].variable().strength
-
-                handle._connect_constraint = PointConstraint(
-                    p1=CanvasProjection(handle.pos,item)
-                    ,p2=p2
-                )                
-                view.canvas.solver.add_constraint(handle._connect_constraint)
-                handle.connected_to = glue_port
-                handle.disconnect = handle_disconnect
-
-            handle.disconnect = handle_disconnect
-            return
-
-        # drop old connection
-        if handle.connected_to:
-            handle.disconnect()
+                pass
+        else:
+            # ie no glue_port found, or the handle connected to something else
+            if handle.connected_to:
+                handle.disconnect()
 
         if glue_port:
             if isinstance(glue_port, Port):
@@ -356,18 +365,19 @@ class PortConnectingHandleTool(HandleTool):
                 print "connect: p2[0] =",p2[0].variable()
                 print "connect: p2[0].strength =",p2[0].variable().strength
 
-                handle._connect_constraint = PointConstraint(
+                handle.connection_data = PointConstraint(
                     p1=CanvasProjection(handle.pos,item)
                     ,p2=p2
                 )
-                view.canvas.solver.add_constraint(handle._connect_constraint)
+                view.canvas.solver.add_constraint(handle.connection_data)
                 handle.connected_to = glue_port
                 handle.disconnect = handle_disconnect
+                glue_port.connectable = False
 
     def disconnect(self, view, item, handle):
         if handle.connected_to:
-            #print 'Handle.disconnect', view, item, handle
-            view.canvas.solver.remove_constraint(handle._connect_constraint)
+            print 'Handle.disconnect', view, item, handle
+            view.canvas.solver.remove_constraint(handle.connection_data)
 
 
 def DefaultExampleTool():
@@ -380,127 +390,5 @@ def DefaultExampleTool():
     chain.append(ItemTool())
     chain.append(RubberbandTool())
     return chain
-
-
-
-
-from gaphas.view import View
-import pygtk
-pygtk.require('2.0') 
-
-import math
-import gtk
-
-
-def create_window(canvas, zoom=1.0):
-    view = View()
-    
-    w = gtk.Window()
-    h = gtk.HBox()
-    w.add(h)
-
-    # VBox contains buttons that can be used to manipulate the canvas:
-    v = gtk.VBox()
-    v.set_property('border-width', 3)
-    v.set_property('spacing', 2)
-    f = gtk.Frame()
-    f.set_property('border-width', 1)
-    f.add(v)
-    h.pack_start(f, expand=False)
-
-    v.add(gtk.Label('Item placement:'))
-    
-    b = gtk.Button('Add box')
-
-    def on_clicked(button, view):
-        #view.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.CROSSHAIR))
-        view.tool.grab(PlacementTool(MyBox, HandleTool(), 2))
-
-    b.connect('clicked', on_clicked, view)
-    v.add(b)
-
-    b = gtk.Button('Add line')
-
-    def on_clicked(button):
-        view.tool.grab(PlacementTool(MyLine, HandleTool(), 1))
-
-    b.connect('clicked', on_clicked)
-    v.add(b)
-
-    v.add(gtk.Label('Zooming:'))
-   
-    b = gtk.Button('Zoom in')
-
-    def on_clicked(button):
-        view.zoom(1.2)
-
-    b.connect('clicked', on_clicked)
-    v.add(b)
-
-    b = gtk.Button('Zoom out')
-
-    def on_clicked(button):
-        view.zoom(1/1.2)
-
-    b.connect('clicked', on_clicked)
-    v.add(b)
-
-    v.add(gtk.Label('Misc:'))
-
-    b = gtk.Button('Split line')
-
-    def on_clicked(button):
-        if isinstance(view.focused_item, Line):
-            view.focused_item.split_segment(0)
-            view.queue_draw_item(view.focused_item, handles=True)
-
-    b.connect('clicked', on_clicked)
-    v.add(b)
-
-#    b = gtk.Button('Cursor')
-#
-#    def on_clicked(button, li):
-#        c = li[0]
-#        li[0] = (c+2) % 154
-#        button.set_label('Cursor %d' % c)
-#        button.window.set_cursor(gtk.gdk.Cursor(c))
-#
-#    b.connect('clicked', on_clicked, [0])
-#    v.add(b)
-
-    # Add the actual View:
-
-    t = gtk.Table(2,2)
-    h.add(t)
-
-    w.connect('destroy', gtk.main_quit)
-
-    view.canvas = canvas
-    view.zoom(zoom)
-    view.set_size_request(150, 120)
-    hs = gtk.HScrollbar(view.hadjustment)
-    vs = gtk.VScrollbar(view.vadjustment)
-    t.attach(view, 0, 1, 0, 1)
-    t.attach(hs, 0, 1, 1, 2, xoptions=gtk.FILL, yoptions=gtk.FILL)
-    t.attach(vs, 1, 2, 0, 1, xoptions=gtk.FILL, yoptions=gtk.FILL)
-
-    w.show_all()
-    
-    def handle_changed(view, item, what):
-        print what, 'changed: ', item
-
-    view.connect('focus-changed', handle_changed, 'focus')
-    view.connect('hover-changed', handle_changed, 'hover')
-    view.connect('selection-changed', handle_changed, 'selection')
-
-
-
-if __name__=="__main__":
-    from gaphas.canvas import Canvas
-    canvas = Canvas()
-
-    create_window(canvas)
-    
-
 
 # vim: sw=4:et:ai
