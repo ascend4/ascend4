222e667c1c28e731ae4606df28ec982ce73dfd17
John Pye <john.pye@anu.edu.au>
2008-06-11 09:45:49 +0000

    Fixed to port placement problem from Arjan.
    git-svn-id: https://svn.ascend4.org/trunk@1789 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/canvas/port.py
+++ pygtk/canvas/port.py
@@ -400,6 +400,29 @@ class BlockCanvas(Canvas):
 
         super(BlockCanvas,self).update_constraints(items)
 
+    def _normalize(self, items):
+        """
+        Correct offset of ports due to movement of left-side handles.
+        """
+        dirty_matrix_items = set()
+        for item in items:
+            if not hasattr(item, 'ports'):
+                continue
+            handles = item.handles()
+            ports = item.ports
+            if not handles or not ports:
+                continue
+            x, y = map(float, handles[0].pos)
+            # Dirty marking is done by the superclass' method
+            if x:
+                for p in ports:
+                    p.x._value -= x
+            if y:
+                for p in ports:
+                    p.y._value -= y
+        dirty_matrix_items.update(super(BlockCanvas, self)._normalize(items))
+        return dirty_matrix_items
+
 
     
 
