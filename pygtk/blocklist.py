a353526a1d34dfdf54b5fc0e468d9a7f8ce91ad9
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2008-08-14 14:03:41 +0000

    The getNoteForVariable seems to work now, picks up more types.
    git-svn-id: https://svn.ascend4.org/trunk@1813 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/blocklist.py
+++ pygtk/blocklist.py
@@ -31,3 +31,20 @@ print "-----"
 
 T = L.findType("pump_simple")
 print D.getNoteForVariable(T,ascpy.SymChar("inlet"),ascpy.SymChar("inline"));
+
+print "-----"
+blocktypes = set()
+
+for m in M:
+	T = L.getModuleTypes(m)
+	for t in T:
+		x = str(D.getNoteForVariable(t,ascpy.SymChar("inlet"),ascpy.SymChar("inline")))
+		if x[0:max(3,len(x))] == "in:" or x[0:max(4,len(x))] == "out:":
+			blocktypes.add(t)
+
+print "block types:"
+if not blocktypes:
+	print "NONE FOUND"
+for t in blocktypes:
+	print t.getName()
+
