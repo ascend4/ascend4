d4c620e23bbe814db1a320381c5bf760bd6f3ca5
John Pye <john.pye@anu.edu.au>
2008-09-18 04:27:16 +0000

    Added some GPL headers, fixing water helm_ideal coefficients.
    git-svn-id: https://svn.ascend4.org/trunk@1874 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/johnpye/fprops/water.c
+++ models/johnpye/fprops/water.c
@@ -1,3 +1,22 @@
+/*	ASCEND modelling environment
+	Copyright (C) 2008 Carnegie Mellon University
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330,
+	Boston, MA 02111-1307, USA.
+*/
+
 #include "water.h"
 
 #define WATER_R 461.51805 /* J/kg·K */
@@ -14,15 +33,16 @@ const IdealData ideal_data_water = {
 	, WATER_R /* cpstar J/kgK */
 	, 1 /* power terms */	
 	, (const IdealPowTerm[]){
-		{3.00632,  1}
+		{1. + 3.00632,  1}
 	}
 	, 5 /* exponential terms */
 	, (const IdealExpTerm []){
-		{0.012436, 1.28728967}
-		,{0.97315,  3.53734222}
-		,{1.27950, 7.74073708}
-		,{0.96956, 9.24437796}
-		,{0.24873, 27.5075105}
+		/* b, beta */
+		{0.012436, 1.28728967*WATER_TSTAR}
+		,{0.97315,  3.53734222*WATER_TSTAR}
+		,{1.27950, 7.74073708*WATER_TSTAR}
+		,{0.96956, 9.24437796*WATER_TSTAR}
+		,{0.24873, 27.5075105*WATER_TSTAR}
 	}
 };
 
@@ -112,9 +132,9 @@ const HelmholtzData helmholtz_data_water = {
 */
 
 const double n0[] = {
-    0.0,
-    -8.32044648201, 6.6832105268, 3.00632, 0.012436,
-    0.97315, 1.27950, 0.96956, 0.24873
+    0.0 /* placeholder */,
+    -8.32044648201, 6.6832105268, 3.00632 /* const, linear, ln(tau) coeffs */
+	, 0.012436, 0.97315, 1.27950, 0.96956, 0.24873 /* exponential coeffs */
 };
 
 const double gamma0[] = {
@@ -129,11 +149,6 @@ const double gamma0[] = {
 enum Limits{
     eGamma1 = 4,
     eGamma2 = 9,
-    ePhir1 = 1,
-    ePhir2 = 8,
-    ePhir3 = 52,
-    ePhir4 = 55,
-    ePhir5 = 57
 };
 
 #include <assert.h>
