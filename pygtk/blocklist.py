1415458ed28eeae9892d8c15559f873265604aa5
John Pye <john.pye@anu.edu.au>
2008-10-08 04:23:52 +0000

    Still working on the inputs, outputs list. Problem with the notequery functions, seems that assumption about ordering of notes returned form GetNotesList was wrong.
    git-svn-id: https://svn.ascend4.org/trunk@1926 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/blocklist.py
+++ pygtk/blocklist.py
@@ -40,11 +40,28 @@ if not blocktypes:
 	print "NONE FOUND"
 for t in blocktypes:
 	print t.getName()
+	if str(t.getName())!="condenser_simple":
+		continue
 
 	nn = D.getNotes(t,ascpy.SymChar("block"),ascpy.SymChar("SELF"))
 	for n in nn: 
 		print "\t%s" % n.getText()
 	nn = D.getTypeRefinedNotesLang(t,ascpy.SymChar("inline"))
+
+	inputs = []
+	outputs = []
 	for n in nn:
-		print "\t\t%s: %s" % (n.getId(), n.getText())	
+		t = n.getText()
+		if t[0:min(len(t),3)]=="in:":
+			inputs += [n]
+		elif t[0:min(len(t),4)]=="out:":
+			outputs += [n]
+
+	print "\t\tinputs:",[t.getId() for t in inputs]
+	for t in inputs:
+		print "\t\t\t%s: %s" % (t.getId(),t.getText())
+	print "\t\toutputs:",[t.getId() for t in outputs]
+	for t in outputs:
+		print "\t\t\t%s: %s" % (t.getId(),t.getText())
+
 
