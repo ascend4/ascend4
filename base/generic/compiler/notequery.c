359b565183b8e019ff9c54cb08dd9e382486b222
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2008-10-08 04:23:52 +0000

    Still working on the inputs, outputs list. Problem with the notequery functions, seems that assumption about ordering of notes returned form GetNotesList was wrong.
    git-svn-id: https://svn.ascend4.org/trunk@1926 f15cd148-d149-4907-926e-73cf92b3ee03

--- base/generic/compiler/notequery.c
+++ base/generic/compiler/notequery.c
@@ -22,6 +22,7 @@
 #include "notequery.h"
 #include "notate.h"
 #include "symtab.h"
+#include "cmpfunc.h"
 #include <utilities/error.h>
 
 const char *notes_get_for_variable(symchar *dbid
@@ -73,11 +74,11 @@ struct gl_list_t *notes_get_vars_with_notetype(
 ){
 	int i;
 	struct gl_list_t *noteslist;
-	struct gl_list_t *refinednoteslist = gl_create(1);
+	struct gl_list_t *refinednoteslist;
 
 	struct gl_list_t *types = GetAncestorNames(t);
 	struct gl_list_t *langs = gl_create(1);
-	struct gl_list_t *ids = gl_create(1);
+
 #if 0
 	CONSOLE_DEBUG("type '%s' has %ld ancestor types",SCP(GetName(t)),gl_length(types));
 	for(i=1; i<=gl_length(types); ++i){
@@ -89,37 +90,46 @@ struct gl_list_t *notes_get_vars_with_notetype(
 	gl_append_ptr(langs,(VOIDPTR)notetype);
 
 	/* create a new list with our top-level type at the start */
-	struct gl_list_t *typesall = gl_create(1);
-	gl_append_ptr(typesall,(VOIDPTR)t);
+	struct gl_list_t *typesall = gl_create(1 + gl_length(types));
 	for(i=1;i<=gl_length(types);++i){
 		//CONSOLE_DEBUG("Appending '%s' to typesall",SCP(GetName((struct TypeDescription *)gl_fetch(types,i))));
 		gl_append_ptr(typesall,gl_fetch(types,i));
 	}
-	/* CONSOLE_DEBUG("length of types = %ld, typesall = %ld",gl_length(types), gl_length(typesall));*/
+	gl_append_ptr(typesall,(VOIDPTR)GetName(t));
+	/* CONSOLE_DEBUG("length of types = %ld, typesall = %ld",gl_length(types), gl_length(typesall)); */
 
 	gl_destroy(types);
+
+#if 0
+	for(i=1;i<=gl_length(typesall);++i){
+		struct TypeDescription *t;
+		CONSOLE_DEBUG("typesall[%d] = '%s'",i,SCP((symchar *)gl_fetch(typesall,i)));
+	}
+#endif
 
 	noteslist = GetNotesList(dbid,typesall,langs,NOTESWILDLIST,NOTESWILDLIST,NOTESWILDLIST);
 
 	gl_destroy(typesall);
 	gl_destroy(langs);
 
-	/*CONSOLE_DEBUG("noteslist = %ld items",gl_length(noteslist));*/
+	/* CONSOLE_DEBUG("noteslist = %ld items",gl_length(noteslist)); */
+
+	refinednoteslist = gl_create(gl_length(noteslist));
 
 	const symchar *lastid = NULL;
+	/* CONSOLE_DEBUG("refinednotes list has %ld elements",gl_length(refinednoteslist)); */
 	for(i=1; i<=gl_length(noteslist); ++i){
 		struct Note *n = (struct Note *)gl_fetch(noteslist,i);
-		/* CONSOLE_DEBUG("note ID = %s, lastid = %s",GetNoteId(n),lastid); */
-		if(!lastid || lastid != GetNoteId(n)){
-			gl_append_ptr(refinednoteslist,(VOIDPTR)n);
-			lastid = GetNoteId(n);
-		}
+		if(GetNoteId(n)==NULL)continue;
+
+		gl_append_ptr(refinednoteslist,(VOIDPTR)n);
+		lastid = GetNoteId(n);
 	}
 	gl_destroy(noteslist);
 
 	if(gl_length(refinednoteslist)==0){
 		gl_destroy(refinednoteslist);
-		/* CONSOLE_DEBUG("empty notes list returned"); */
+		CONSOLE_DEBUG("empty notes list returned");
 		return NULL;
 	}
 
