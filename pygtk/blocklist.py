92fbe3081a2a53bb916e7823e830206f7913e45e
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2008-10-27 01:59:52 +0000

    Simple block browser started using gtk IconView.
    git-svn-id: https://svn.ascend4.org/trunk@1935 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/blocklist.py
+++ pygtk/blocklist.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 import sys
 
 if sys.platform.startswith("win"):
@@ -35,6 +36,25 @@ M = L.getModules()
 
 blocktypes = set()
 
+class Block:
+	def __init__(self, typedesc, notesdb):
+		self.type = typedesc
+		self.notesdb = notesdb
+
+		nn = notesdb.getTypeRefinedNotesLang(self.type,ascpy.SymChar("inline"))
+
+		self.inputs = []
+		self.outputs = []
+		for n in nn:
+			t = n.getText()
+			if t[0:min(len(t),3)]=="in:":
+				self.inputs += [n]
+			elif t[0:min(len(t),4)]=="out:":
+				self.outputs += [n]
+
+	def get_icon(self, width, height):
+		return gtk.gdk.pixbuf_new_from_file_at_size("canvas/defaultblock.svg",width,height)
+
 for m in M:
 	T = L.getModuleTypes(m)
 	for t in T:
@@ -47,30 +67,74 @@ for m in M:
 		if x:
 			blocktypes.add(t)
 
+blocks = []
+
 print "block types:"
 if not blocktypes:
 	print "NONE FOUND"
 for t in blocktypes:
-	print t.getName()
 
-	nn = D.getNotes(t,ascpy.SymChar("block"),ascpy.SymChar("SELF"))
-	for n in nn: 
-		print "\t%s" % n.getText()
-	nn = D.getTypeRefinedNotesLang(t,ascpy.SymChar("inline"))
+	b = Block(t,D)
 
-	inputs = []
-	outputs = []
-	for n in nn:
-		t = n.getText()
-		if t[0:min(len(t),3)]=="in:":
-			inputs += [n]
-		elif t[0:min(len(t),4)]=="out:":
-			outputs += [n]
+	blocks += [b]
 
-	print "\t\tinputs:",[n.getId() for n in inputs]
-	for n in inputs:
+	print b.type.getName()
+
+	print "\t\tinputs:",[n.getId() for n in b.inputs]
+	for n in b.inputs:
 		print "\t\t\t%s: %s (type = %s)" % (n.getId(),n.getText(),n.getType())
-	print "\t\toutputs:",[n.getId() for n in outputs]
-	for n in outputs:
+	print "\t\toutputs:",[n.getId() for n in b.outputs]
+	for n in b.outputs:
 		print "\t\t\t%s: %s" % (n.getId(),n.getText())
+
+
+# render icon table
+import threading
+import gtk
+import os, os.path, re
+gtk.gdk.threads_init()
+
+class app(gtk.Window):
+	def __init__(self):
+		gtk.Window.__init__(self)
+		self.set_title("ASCEND Blocks")
+		self.set_default_size(400, 500)
+		self.connect("destroy", gtk.main_quit)
+
+		scroll = gtk.ScrolledWindow()
+		scroll.set_border_width(2)
+		scroll.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+		scroll.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
+
+		thumb_view = gtk.IconView()
+		model = gtk.ListStore(str, gtk.gdk.Pixbuf)
+		thumb_view.set_model(model)
+		thumb_view.set_text_column(0)
+		thumb_view.set_pixbuf_column(1)
+		thumb_view.set_columns(-1)
+
+		vbox = gtk.VBox()
+		status = gtk.Statusbar()
+
+		scroll.add(thumb_view)
+		vbox.pack_start(scroll, True, True)
+		vbox.pack_start(status, False, False)
+		self.add(vbox)
+		self.show_all()
+
+		dirn = "/home/john/ENGN2222/Pictures"
+		files = os.listdir(dirn)
+		thread = threading.RLock()
+		r = re.compile("^.*\\.(png|jpg)")
+		n = 0
+		with thread:
+			for b in blocks:
+				n += 1
+				pixbuf = b.get_icon(96,96)
+				model.append([b.type.getName(), pixbuf])
+			   
+		status.push(0, "Found %d images amongst %d files." % (n,len(files)))
+	   
+a = app()
+gtk.main() 
 
