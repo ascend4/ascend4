561787187be119ffcad9ee24a1a08af7b2be953a
John Pye <john.pye@anu.edu.au>
2008-10-27 09:58:54 +0000

    Blocklist now creates blocks on the canvas with the correct number of inputs and outputs based on the results of parsing a model file.
    git-svn-id: https://svn.ascend4.org/trunk@1937 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/canvas/blocklist.py
+++ pygtk/canvas/blocklist.py
@@ -1,5 +1,11 @@
 from __future__ import with_statement
-import sys
+import os, sys
+
+os.chdir(os.path.dirname(sys.argv[0]))
+os.environ['ASCENDLIBRARY'] = "../../models"
+os.environ['LD_LIBRARY_PATH'] = "../.."
+sys.path.append("..")
+
 
 if sys.platform.startswith("win"):
     # Fetchs gtk2 path from registry
@@ -55,6 +61,7 @@ class Block:
 	def get_icon(self, width, height):
 		return gtk.gdk.pixbuf_new_from_file_at_size("defaultblock.svg",width,height)
 
+
 for m in M:
 	T = L.getModuleTypes(m)
 	for t in T:
@@ -99,66 +106,118 @@ from gaphas.tool import HoverTool, PlacementTool, HandleTool, ToolChain
 from gaphas.tool import ItemTool, RubberbandTool
 from port import *
 
+def factory(view, cls):
+    """
+    Simple canvas item factory.
+    """
+    def wrapper():
+        item = cls()
+        view.canvas.add(item)
+        return item
+    return wrapper
+
 gtk.gdk.threads_init()
 
-class app(gtk.Window):
-	def __init__(self):
-
-		canvas = BlockCanvas()
-
-		gtk.Window.__init__(self)
-		self.set_title("ASCEND Blocks")
-		self.set_default_size(400, 500)
-		self.connect("destroy", gtk.main_quit)
-
-		scroll = gtk.ScrolledWindow()
-		scroll.set_border_width(2)
-		scroll.set_shadow_type(gtk.SHADOW_ETCHED_IN)
-		scroll.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
-
-		thumb_view = gtk.IconView()
-		model = gtk.ListStore(str, gtk.gdk.Pixbuf)
-		thumb_view.set_model(model)
-		thumb_view.set_text_column(0)
-		thumb_view.set_pixbuf_column(1)
-		thumb_view.set_columns(-1)
-
-		vbox = gtk.VBox()
-		status = gtk.Statusbar()
-
-		hbox = gtk.HBox()
-
-		view = GtkView()
-		view.tool =  DefaultExampleTool()
-
-		t = gtk.Table(2,2)
-		view.canvas = canvas
-		view.zoom(1)
-		view.set_size_request(300, 500)
-		hs = gtk.HScrollbar(view.hadjustment)
-		vs = gtk.VScrollbar(view.vadjustment)
-		t.attach(view, 0, 1, 0, 1)
-		t.attach(hs, 0, 1, 1, 2, xoptions=gtk.FILL, yoptions=gtk.FILL)
-		t.attach(vs, 1, 2, 0, 1, xoptions=gtk.FILL, yoptions=gtk.FILL)
-
-		scroll.add(thumb_view)
-		hbox.pack_start(scroll, True, True)
-		hbox.pack_start(t, True, True)
-
-		vbox.pack_start(hbox, True, True)
-		vbox.pack_start(status, False, False)
-		self.add(vbox)
-		self.show_all()
-
+class BlockIconView(gtk.IconView):
+	def __init__(self,blocks,app):
+		# the mode containing the icons themselves...
+		self.model = gtk.ListStore(str, gtk.gdk.Pixbuf)
+		self.app = app
+		self.otank = {}
 		thread = threading.RLock()
 		n = 0
 		with thread:
 			for b in blocks:
 				n += 1
 				pixbuf = b.get_icon(64,64)
-				model.append([b.type.getName(), pixbuf])
-			   
-		status.push(0, "Found %d block types." % (n))
+				iter = self.model.append([b.type.getName(), pixbuf])
+				path = self.model.get_path(iter)
+				self.otank[path] = b
+
+		gtk.IconView.__init__(self)
+		self.set_model(self.model)
+		self.set_text_column(0)
+		self.set_pixbuf_column(1)
+		self.set_columns(-1)
+		self.set_size_request(180,100)
+		self.connect("item-activated", self.item_activated)
+		self.connect("selection-changed", self.selection_changed)
+	def item_activated(self,iconview,path):
+		b = self.otank[path]
+		self.app.status.push(0, "Activated '%s'..." % b.type.getName())
+        #view.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.CROSSHAIR))
+		self.app.set_placement_tool(b)
+
+	def selection_changed(self,iconview):
+		s = self.get_selected_items()
+		if len(s)==1:
+			b = self.otank[s[0]]
+			self.app.set_placement_tool(b)
+			self.app.status.push(0,"Selected '%s'..." % b.type.getName())
+
+class app(gtk.Window):
+	def __init__(self):
+		self.status = gtk.Statusbar()
+
+		# the Gaphas canvas
+		canvas = BlockCanvas()
+
+		# the main window
+		gtk.Window.__init__(self)
+		self.set_title("ASCEND Blocks")
+		self.set_default_size(400, 500)
+		self.connect("destroy", gtk.main_quit)
+
+		# vbox containing the main view and the status bar at the bottom
+		vbox = gtk.VBox()
+
+		# hbox occupies top part of vbox, with icons on left & canvas on right.
+		hbox = gtk.HBox()
+
+		# the 'view' widget implemented by Gaphas
+		self.view = GtkView()
+		self.view.tool =  DefaultExampleTool()
+
+		# table containing scrollbars and main canvas 
+		t = gtk.Table(2,2)
+		self.view.canvas = canvas
+		self.view.zoom(1)
+		self.view.set_size_request(600, 500)
+		hs = gtk.HScrollbar(self.view.hadjustment)
+		vs = gtk.VScrollbar(self.view.vadjustment)
+		t.attach(self.view, 0, 1, 0, 1)
+		t.attach(hs, 0, 1, 1, 2, xoptions=gtk.FILL, yoptions=gtk.FILL)
+		t.attach(vs, 1, 2, 0, 1, xoptions=gtk.FILL, yoptions=gtk.FILL)
+
+		# a scrolling window to contain the icon palette
+		scroll = gtk.ScrolledWindow()
+		scroll.set_border_width(2)
+		scroll.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+		scroll.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
+
+		# icon palette
+		self.blockiconview = BlockIconView(blocks, self)
+		scroll.add(self.blockiconview)
+
+		hbox.pack_start(scroll, True, True)
+		hbox.pack_start(t, True, True)
+		vbox.pack_start(hbox, True, True)
+		vbox.pack_start(self.status, False, False)
+		self.add(vbox)
+		self.show_all()
+
+		# a message about the found blocks
+		self.status.push(0, "Found %d block types." % (len(blocks)))
+				
+	def set_placement_tool(self,block):
+		label = block.type.getName()
+		def my_block_factory():
+			def wrapper():
+				b = DefaultBlock(label,inputs=len(block.inputs),outputs=len(block.outputs))
+				self.view.canvas.add(b)
+				return b
+			return wrapper
+		self.view.tool.grab(PlacementTool(my_block_factory(), HandleTool(), 2))
 	   
 a = app()
 gtk.main() 
