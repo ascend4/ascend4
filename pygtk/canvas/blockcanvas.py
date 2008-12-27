8c58968bd247b86633154da60e95a799a0f3ba10
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2008-12-27 07:19:35 +0000

    Some steps towards support of latest release of Gaphas.
    git-svn-id: https://svn.ascend4.org/trunk@1970 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/canvas/blockcanvas.py
+++ pygtk/canvas/blockcanvas.py
@@ -10,10 +10,11 @@ class BlockCanvas(Canvas):
 		# request solving of external constraints associated with dirty items
 		request_resolve = self._solver.request_resolve
 		for item in items:
-		    if hasattr(item,'ports'):
-		        for p in item.ports:
-		            request_resolve(p.x)
-		            request_resolve(p.y)
+			if hasattr(item,'ports'):
+				for p in item._ports:
+					if hasattr(p,"point"):
+						request_resolve(p.point.x)
+						request_resolve(p.point.y)
 
 		super(BlockCanvas,self).update_constraints(items)
 
@@ -23,20 +24,22 @@ class BlockCanvas(Canvas):
 		"""
 		dirty_matrix_items = set()
 		for item in items:
-		    if not hasattr(item, 'ports'):
-		        continue
-		    handles = item.handles()
-		    ports = item.ports
-		    if not handles or not ports:
-		        continue
-		    x, y = map(float, handles[0].pos)
-		    # Dirty marking is done by the superclass' method
-		    if x:
-		        for p in ports:
-		            p.x._value -= x
-		    if y:
-		        for p in ports:
-		            p.y._value -= y
+			if not hasattr(item, 'ports'):
+				continue
+			handles = item.handles()
+			ports = item._ports
+			if not handles or not ports:
+				continue
+			x, y = map(float, handles[0].pos)
+			# Dirty marking is done by the superclass' method
+			if x:
+				for p in ports:
+					if hasattr(p,"point"):
+						p.point.x._value -= x
+			if y:
+				for p in ports:
+					if hasattr(p,"point"):
+						p.point.y._value -= y
 		dirty_matrix_items.update(super(BlockCanvas, self)._normalize(items))
 		return dirty_matrix_items
 
