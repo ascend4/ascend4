15604bbf99238a03e20fcaae539a7cff7a3a150a
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2009-01-12 07:07:02 +0000

    Fixed for partly connected lines when exporting MODEL. Removed bespoke _normalize code, since changes in Gaphas rev 2598.
    git-svn-id: https://svn.ascend4.org/trunk@1980 f15cd148-d149-4907-926e-73cf92b3ee03

--- pygtk/canvas/blockcanvas.py
+++ pygtk/canvas/blockcanvas.py
@@ -18,31 +18,6 @@ class BlockCanvas(Canvas):
 
 		super(BlockCanvas,self).update_constraints(items)
 
-	def _normalize(self, items):
-		"""
-		Correct offset of ports due to movement of left-side handles.
-		"""
-		dirty_matrix_items = set()
-		for item in items:
-			if not hasattr(item, 'ports'):
-				continue
-			handles = item.handles()
-			ports = item._ports
-			if not handles or not ports:
-				continue
-			x, y = map(float, handles[0].pos)
-			# Dirty marking is done by the superclass' method
-			if x:
-				for p in ports:
-					if hasattr(p,"point"):
-						p.point.x._value -= x
-			if y:
-				for p in ports:
-					if hasattr(p,"point"):
-						p.point.y._value -= y
-		dirty_matrix_items.update(super(BlockCanvas, self)._normalize(items))
-		return dirty_matrix_items
-
 	def reattach_ascend(self, library, notesdb):
 		"""
 		After unpickling a canvas, this method gives a way of reattaching
