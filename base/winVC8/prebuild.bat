251662216ba5efc66a07215662e2e2d4bb954261
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2009-04-28 09:07:13 +0000

    Cleaning up disused stuff from #/base.
    git-svn-id: https://svn.ascend4.org/trunk@2013 f15cd148-d149-4907-926e-73cf92b3ee03

--- base/winVC8/prebuild.bat
+++
@@ -1,9 +0,0 @@
-cd ..\generic\compiler
-del scanner.c
-del ascParse.h
-del ascParse.c
-flex -Pzz_ -t scanner.l >scanner.c
-bison -d -o ascParse.c ascParse.y
-sed -i -e "s/yy/zz_/g" -e "s/SIZE_T/TSIZE_T/g" scanner.c
-sed -i -e "/#ifndef YYSTYPE/,/#endif/d" -e "s/yy/zz_/g" -e "s/YY/ZZ_/g" -e "s/SIZE_T/TSIZE_T/g" ascParse.c
-sed -i -e "s/yy/zz_/g" -e "s/YY/ZZ_/g" -e "s/SIZE_T/TSIZE_T/g" ascParse.h
