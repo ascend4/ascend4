e8d9219f810ddb1b92cc61695b6bde617ffccd4d
John Pye <john.pye@anu.edu.au>
2009-07-07 00:17:03 +0000

    Cleaned up SimsProc.h (removing old <!-- comments -->). Fixed up some svn:ignore flags.
    git-svn-id: https://svn.ascend4.org/trunk@2065 f15cd148-d149-4907-926e-73cf92b3ee03

--- tcltk/interface/SimsProc.c
+++ tcltk/interface/SimsProc.c
@@ -1,4 +1,21 @@
-/*
+/*	ASCEND modelling environment
+	Copyright (C) 1997, 2009 Carnegie Mellon University
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330,
+	Boston, MA 02111-1307, USA.
+*//*
  *  SimsProc.c
  *  by Kirk Abbott and Ben Allan
  *  Created: 1/94
@@ -6,26 +23,7 @@
  *  Version control file: $RCSfile: SimsProc.c,v $
  *  Date last modified: $Date: 2003/08/23 18:43:08 $
  *  Last modified by: $Author: ballan $
- *
- *  This file is part of the ASCEND Tcl/Tk interface
- *
- *  Copyright 1997, Carnegie Mellon University
- *
- *  The ASCEND Tcl/Tk interface is free software; you can redistribute
- *  it and/or modify it under the terms of the GNU General Public License as
- *  published by the Free Software Foundation; either version 2 of the
- *  License, or (at your option) any later version.
- *
- *  The ASCEND Tcl/Tk interface is distributed in hope that it will be
- *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with the program; if not, write to the Free Software Foundation,
- *  Inc., 675 Mass Ave, Cambridge, MA 02139 USA.  Check the file named
- *  COPYING.  COPYING is found in ../compiler.
- */
+*/
 
 #define ASC_BUILDING_INTERFACE
 
