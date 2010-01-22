4322d294943d3d39029bc980c5293a8b58e062db
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2010-01-22 13:49:18 +0000

    Updated SCons tool for case where PyWin32 not present.
    git-svn-id: https://svn.ascend4.org/trunk@2148 f15cd148-d149-4907-926e-73cf92b3ee03

--- tools/scons/ascend.py
+++ tools/scons/ascend.py
@@ -6,12 +6,31 @@
 import os, platform,sys,subprocess
 from SCons.Script import *
 
+def my_parse_config_win(env,cmd):
+	env1 = env.Clone()
+	proc = subprocess.Popen(cmd,stdout=subprocess.PIPE)
+	out = proc.communicate()[0].strip()	
+	
+	env1.MergeFlags(out)
+
+	env.Append(
+		ASCEND_CPPPATH = env1.get('CPPPATH') or []
+		,ASCEND_CPPDEFINES = env1.get('CPPDEFINES') or []
+		,ASCEND_LIBPATH = env1.get('LIBPATH') or []
+		,ASCEND_LIBS = env1.get('LIBS') or []
+	)
+
 def generate(env):
 	"""
 	Detect ASCEND settings and add them to the environment.
 	"""
 	try:
 		if platform.system()=="Windows":
+			try:
+				import win32api
+			except:
+				raise RuntimeError("PyWin32 is not installed. Install it first.")
+
 			import _winreg
 			x=_winreg.ConnectRegistry(None,_winreg.HKEY_LOCAL_MACHINE)
 			y= _winreg.OpenKey(x,r"SOFTWARE\ASCEND")
@@ -20,15 +39,9 @@ def generate(env):
 			if not os.path.exists(Path):
 				raise RuntimeError("Could not find 'ascend-config' in your PATH")
 				
-			cmd = [sys.executable,"\"%s\""%Path,"--cppflags","--libs"]
-			env1 = env.Copy()
-			env1.ParseConfig(cmd)
-			env.Append(
-				ASCEND_CPPPATH = env1.get('CPPPATH') or []
-				,ASCEND_CPPDEFINES = env1.get('CPPDEFINES') or []
-				,ASCEND_LIBPATH = env1.get('LIBPATH') or []
-				,ASCEND_LIBS = env1.get('LIBS') or []
-			)
+			cmd = [sys.executable,Path,"--cppflags","--libs"]
+			my_parse_config_win(env,cmd)
+
 			cmd = [sys.executable,Path]
 			libext = ".dll"
 			libpref = ""
@@ -73,17 +86,21 @@ def generate(env):
 
 		env.Append(HAVE_ASCEND=True)
 
-		print "ASCEND_LIBS =",env.get('ASCEND_LIBS')
-		print "ASCEND_LIBPATH =",env.get('ASCEND_LIBPATH')
-		print "ASCEND_CPPPATH =",env.get('ASCEND_CPPPATH')
-		print "ASCEND_EXTLIB_SUFFIX =",env.get('ASCEND_EXTLIB_SUFFIX')
-		print "ASCEND_EXTLIB_PREFIX =",env.get('ASCEND_EXTLIB_PREFIX')
-		print "ASCEND_MODELS =",env.get('ASCEND_MODELS')
+		#print "ASCEND_LIBS =",env.get('ASCEND_LIBS')
+		#print "ASCEND_LIBPATH =",env.get('ASCEND_LIBPATH')
+		#print "ASCEND_CPPPATH =",env.get('ASCEND_CPPPATH')
+		#print "ASCEND_EXTLIB_SUFFIX =",env.get('ASCEND_EXTLIB_SUFFIX')
+		#print "ASCEND_EXTLIB_PREFIX =",env.get('ASCEND_EXTLIB_PREFIX')
+		#print "ASCEND_MODELS =",env.get('ASCEND_MODELS')
 
 	except Exception,e:
-		print "FAILED TO SET UP ASCEND (%s)" % str(e)
-		pass
-
+		print "Checking for ASCEND... not found! (%s)" % str(e)
+		env.Append(HAVE_ASCEND=False)
+		return False
+		
+	print "Checking for ASCEND... found (ASCEND_CPPPATH = %s)" % env.get('ASCEND_CPPPATH')
+	return True
+	
 def exists(env):
 	"""
 	Make sure this tool exists.
