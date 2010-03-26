20ff1c1b5f50729d8182a425d0d4235bdbaf76d4
John Pye <john.pye@anu.edu.au>
2010-03-26 03:02:43 +0000

    Fix for detecting sundials on Windows
    git-svn-id: https://svn.ascend4.org/trunk@2170 f15cd148-d149-4907-926e-73cf92b3ee03

--- scons/sundials.py
+++ scons/sundials.py
@@ -25,17 +25,33 @@ def generate(env):
 	"""
 	try:
 		if platform.system()=="Windows":
-			import _winreg
-			x=_winreg.ConnectRegistry(None,_winreg.HKEY_LOCAL_MACHINE)
-			y= _winreg.OpenKey(x,r"SOFTWARE\SUNDIALS")
-			PATH,t = _winreg.QueryValueEx(y,"InstallPath")
-			LIB = os.path.join(PATH,"lib")
-			BIN = os.path.join(PATH,"bin")
-			INCLUDE = os.path.join(PATH,"include")
+			try:
+				# one day, we'll provide a SUNDIALS installer so that people don't have to 
+				# build their own SUNDIALS. In that case, look for the settings in the registry
+				import _winreg
+				x=_winreg.ConnectRegistry(None,_winreg.HKEY_LOCAL_MACHINE)
+				y= _winreg.OpenKey(x,r"SOFTWARE\SUNDIALS")
+				PATH,t = _winreg.QueryValueEx(y,"InstallPath")
+				LIB = os.path.join(PATH,"lib")
+				BIN = os.path.join(PATH,"bin")
+				INCLUDE = os.path.join(PATH,"include")
+				env['SUNDIALS_CPPPATH'] = [munge(INCLUDE)]
+				env['SUNDIALS_LIBPATH'] = [munge(BIN)]
+				env['SUNDIALS_LIBS'] = ['sundials_ida','sundials_nvecserial','m']
+			except WindowsError:
+				# if someone has installed sundials with ./configure --prefix=/MinGW using MSYS, then
+				# this should work, but we would like to make this a lot more robust!
+				cmd = ['c:\\MSYS\\1.0\\bin\\sh.exe','/MinGW/bin/sundials-config','-mida','-ts','-lc']
+				env1 = env.Clone()
+				env1['CPPPATH'] = None
+				env1['LIBPATH'] = None
+				env1['LIBS'] = None
+				env1.ParseConfig(cmd)
+				env['SUNDIALS_CPPPATH'] = env1.get('CPPPATH')
+				env['SUNDIALS_LIBPATH'] = env1.get('LIBPATH')
+				env['SUNDIALS_LIBS'] = env1.get('LIBS')
+				env['HAVE_SUNDIALS'] = True		
 
-			env['SUNDIALS_CPPPATH'] = [munge(INCLUDE)]
-			env['SUNDIALS_LIBPATH'] = [munge(BIN)]
-			env['SUNDIALS_LIBS'] = ['sundials_ida','sundials_nvecserial','m']
 			env['HAVE_SUNDIALS'] = True
 									
 		else:
@@ -54,7 +70,8 @@ def generate(env):
 		print "SUNDIALS_LIBPATH =",env.get('SUNDIALS_LIBPATH')
 		print "SUNDIALS_CPPPATH =",env.get('SUNDIALS_CPPPATH')
 
-	except:
+	except Exception, e:
+		print "FAILED SUNDIALS DETECTION:",e.__class__,str(e)
 		env['HAVE_SUNDIALS'] = False
 
 def exists(env):
