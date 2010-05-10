e4e4e1cd0e1228e50f52cdcff7097681f77e883e
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2010-05-10 11:11:54 +0000

    Some progress with parsing the web pages.
    git-svn-id: https://svn.ascend4.org/trunk@2186 f15cd148-d149-4907-926e-73cf92b3ee03

--- tools/mediawiki/gcache2wiki/striphf.py
+++ tools/mediawiki/gcache2wiki/striphf.py
@@ -33,39 +33,38 @@ def strip_highlight(soup):
 	for a1 in soup.findAll('p'):
 		if a1.find('style',{'type':"text/css"}):
 			n1 = a1.nextSibling
-			a1.extract()
+			if str(n1.string).strip() != "/* Highlighting theme definition: */":
+				print "NO MATCH"
+				sys.exit(1)
 			n2 = n1.nextSibling
-			if n1.find('p'):
-				n1.extract()
+			#print "REMOVING",str(a1)
+			a1.extract()
+			#print "REMOVING N1",str(n1)
+			n1.extract()
 			n3 = n2.nextSibling
+			#print "REMOVING N2",str(n2)
 			n2.extract()
 			n4= n3.nextSibling
+			#print "REMOVING N3",str(n3)
 			n3.extract()
 			pre = n4.nextSibling
+			#print "REMOVING N4",str(n4)
 			n4.extract()
-			print "CODE LISTING"
+
 			if pre.name != 'pre':
 				print "ERROR parsing syntax-highlighting:",pre
 				sys.exit(1)
 			for x in pre.findAll('b',{'style':True}):
-				print "BOLD:",x,"-->",str(x.string)
 				x1 = NavigableString(str(x.string))
-				print "X1:",x1
 				x.replaceWith(x1)
-
-			print "\n\nPRE:",pre,"\n\n"
 	
 			for x in pre.findAll('span',{'class':True}):
-				x1 = NavigableString(str(x.string))
+				x1 = NavigableString(str(x.renderContents()))
 				x.replaceWith(x1)
 
-			print "\n\nPRE2:",pre,"\n\n"
-
 			t = Tag(soup,"src",[("lang",'a4c')])
-			t.insert(0, NavigableString(str(pre.string)))
+			t.insert(0, NavigableString(str(pre.renderContents()).strip()))
 			pre.replaceWith(t)
-			print str(pre)
-			sys.exit(1)
 
 def strip_anchors(soup):
 	for a1 in soup.findAll('a',{'name':True}):
@@ -85,18 +84,75 @@ def wikify_headings(soup):
 
 def wikify_paragraphs(soup):
 	for p in soup.findAll('p'):
-		if p.string is None:
+		#print "PARA",str(p)
+		if p.renderContents() is None:
 			p.replaceWith(NavigableString("\n"))
 		else:
-			p.replaceWith(NavigableString("\n" + p.string + "\n"))
+			p.replaceWith(NavigableString("\n" + p.renderContents() + "\n"))
 
+def strip_printfooter(soup):
+	soup.find('div',{'class':'printfooter'}).extract()
+
+def wikify_categories(soup):
+	cats = soup.find("div",{"id":"catlinks"})
+	if not cats:
+		return
+	r2 = re.compile("/[A-Z][a-z_0-9-]*")
+	cc = []
+	for a in cats.findAll("a"):
+		if str(a['href']) == "/Special:Categories":
+			#print "CATEGORIES LINK ignored"
+			a.extract()
+		elif r2.match(a['href']):
+			t = NavigableString("[[" + a['href'][1:] + "]]\n")
+			print "  categ:",t.strip()
+			cc.append(t)
+	#print "CATS:",cc
+	#cats.replace(cc)
+	for c in cc:
+		cats.parent.append(c)
+	cats.extract()
+
+def wikify_links(soup):
+	rr1 = re.compile(" ")
+	def linkified(s):
+		s = rr1.sub("_",s)
+		s = s[0:1].upper() + s[1:]
+		return s
+
+	r = re.compile("^http://")
+	r2 = re.compile("/[A-Z][a-z_0-9-]*")
+	for a in soup.findAll('a',{'href':True}):
+		#print "LINK:",a.parent
+		if r.match(a['href']):
+			t = NavigableString("[" + a['href'] + " " + a.renderContents() + "]")
+			a.replaceWith(t)
+			print " --> ",t
+
+		elif r2.match(a['href']):
+			if linkified(a.renderContents()) == a['href'][1:]:
+				t = NavigableString("[[" + a.renderContents() + "]]")
+			else:
+				t = NavigableString("[[" + a['href'][1:] + "|" + a.renderContents() + "]]")
+			a.replaceWith(t)
+			print " --> ",t
+
+def wikify_bold(soup):
+	for b in soup.findAll("b",{"style":True}):
+		#print "BOLD:",b
+		b2 = NavigableString("'''" + b.renderContents() + "'''")
+		b.replaceWith(b2)
+
+strip_printfooter(s1)
 strip_highlight(s1)
-
-print str(s1)
-sys.exit(1)
-
 strip_anchors(s1)
 wikify_headings(s1)
 wikify_paragraphs(s1)
+s1 = BeautifulSoup(str(s1))
+wikify_categories(s1)
+wikify_bold(s1)
+wikify_links(s1)
+#sys.exit(1)
+print str(s1)
 
 
