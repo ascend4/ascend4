ff99356fc8863c37d232865b4ed8c8cae2b8998a
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2010-05-13 06:43:22 +0000

    Working up uploading the necessary files.
    git-svn-id: https://svn.ascend4.org/trunk@2192 f15cd148-d149-4907-926e-73cf92b3ee03

--- tools/mediawiki/gcache2wiki/loop.py
+++ tools/mediawiki/gcache2wiki/loop.py
@@ -13,13 +13,19 @@ if not os.path.exists(targetdir):
 	print "Target directory %s does not exist"%targetdir
 	sys.exit(1)
 
+res = {}
 for f in os.listdir(sourcedir):
 	sys.stderr.write("Processing %s...\n"%f)
 	if f in excl:
 		continue
 	c = open(os.path.join(sourcedir,f)).read()
-	s = html2wiki(c,wikiname)
+	s,pagename = html2wiki(c,wikiname)
 	t = os.path.join(targetdir,f + ".txt")
 	sys.stderr.write("Writing to %s\n"%t)
+	res[pagename] = (t,"NEW")
 	open(t,"w").write(s)
-	
+
+import pickle
+pickle.dump(res,open("reslist.pickle","w"))
+
+
