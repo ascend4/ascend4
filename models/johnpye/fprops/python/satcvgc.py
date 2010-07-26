e823ca5197c2129521991a4708342c90561eb15d
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2010-07-26 13:29:14 +0000

    Add error count.
    git-svn-id: https://svn.ascend4.org/trunk@2229 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/johnpye/fprops/python/satcvgc.py
+++ models/johnpye/fprops/python/satcvgc.py
@@ -14,11 +14,22 @@ DD = [
 #	, helmholtz_data_methane
 ]
 
+toterrors = 0
+totchecks = 0
 for D in DD:
 	TT = linspace(D.T_t, D.T_c, 300);
+	firsterror = True
+	errcount = 0
 	for T in TT:
 		res, p, rhof, rhog = fprops_sat_T(T,D)
 		if res:
-			print "%s: Error %d at T = %f" % (D.name,res,T)
+			if firsterror:
+				print "%s: Error %d at T = %f" % (D.name,res,T)
+				firsterror = False
+			errcount += 1				
+	print "%s: %d errors from %d checks" % (D.name, errcount, len(TT))
+	totchecks += len(TT)
+	toterrors += errcount
 
+print "Total: %d errors across all tested substances (%0.1f%% correct)" % (toterrors,100.*(1 - float(toterrors)/totchecks))
 	
