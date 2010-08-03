e9facb221ca5579dbfa73c97cc81de0ddf4a66d6
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2010-08-03 14:12:37 +0000

    Added binding for solve_ph, but code is not yet completed.
    git-svn-id: https://svn.ascend4.org/trunk@2251 f15cd148-d149-4907-926e-73cf92b3ee03

---
+++ models/johnpye/fprops/python/solve_ph.py
@@ -0,0 +1,19 @@
+from fprops import *
+from pylab import *
+
+D = helmholtz_data_water;
+
+p = 1e5;
+h = 200;
+print "p = %f bar, h = %f kJ/kg" % (p/1e5, h/1e3)
+
+res, T, rho = fprops_solve_ph(p,h,0,D);
+
+print "res = %d" % res
+
+print "T = %f" % T
+print "rho = %f" % rho
+
+print "p(T,rho) =", helmholtz_p(T,rho,D)
+print "h(T,rho) =", helmholtz_h(T,rho,D)
+
