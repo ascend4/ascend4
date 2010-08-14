99c5f28374367f4405fe305f71e58c97aac2e4a4
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2010-08-14 08:47:16 +0000

    Fixing ignore list, adding couple of short test routines.
    git-svn-id: https://svn.ascend4.org/trunk@2281 f15cd148-d149-4907-926e-73cf92b3ee03

---
+++ models/johnpye/fprops/python/sat_T.py
@@ -0,0 +1,15 @@
+from fprops import *
+from pylab import *
+
+D = helmholtz_data_water;
+T = D.T_t
+
+res, p1, rf1, rg1 = fprops_sat_T(T,D)
+
+if res:
+	print "ERROR in calculation of psat(T)"
+	print "res = %d" % res
+
+print "psat(T = %f) = %.12e bar" % (T, p1/1e5)
+
+print "rhof = %f, rhog = %f" % (rf1, rg1);
