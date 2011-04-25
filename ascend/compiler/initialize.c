764746f42dd9e23036d7e7b14f6c9d76358fefc3
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2011-04-25 09:09:57 +0000

    Working on P&W Ex 9.7. Some minor cleanup of other files.
    git-svn-id: https://svn.ascend4.org/trunk@2461 f15cd148-d149-4907-926e-73cf92b3ee03

--- ascend/compiler/initialize.c
+++ ascend/compiler/initialize.c
@@ -1,33 +1,32 @@
-/* ex:set ts=4: */
 /*
- *  Initialization Routines
+	ASCEND modelling environment
+	Copyright (C) 1990, 1993, 1994 Thomas Guthrie Epperly
+	Copyright (C) 2011 Carnegie Mellon University
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330,
+	Boston, MA 02111-1307, USA.
+*//** @file
+	Initialization Routines (Support for running METHODs in ASCEND).
+*//*
  *  by Tom Epperly
  *  Created: 3/24/1990
  *  Version: $Revision: 1.36 $
  *  Version control file: $RCSfile: initialize.c,v $
  *  Date last modified: $Date: 1998/06/11 15:28:30 $
  *  Last modified by: $Author: ballan $
- *
- *  This file is part of the Ascend Language Interpreter.
- *
- *  Copyright (C) 1990, 1993, 1994 Thomas Guthrie Epperly
- *
- *  The Ascend Language Interpreter is free software; you can redistribute
- *  it and/or modify it under the terms of the GNU General Public License as
- *  published by the Free Software Foundation; either version 2 of the
- *  License, or (at your option) any later version.
- *
- *  The Ascend Language Interpreter is distributed in hope that it will be
- *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with the program; if not, write to the Free Software Foundation,
- *  Inc., 675 Mass Ave, Cambridge, MA 02139 USA.  Check the file named
- *  COPYING.
- *
- */
+*/
 
 #include <ascend/general/platform.h>
 #include <ascend/general/ascMalloc.h>
@@ -2176,3 +2175,5 @@ enum Proc_enum ClassAccessInitialize(struct Instance *context,
                                       err,options,watchpoints,log,&fm);
   }
 }
+
+/* vim: set ts=4: */
