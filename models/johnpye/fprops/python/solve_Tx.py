261993b769e0212af8d8e1a3b6dd4dfc0eef0b59
John Pye <john.pye@anu.edu.au>
2011-05-27 06:35:20 +0000

    Fix for current function names
    git-svn-id: https://svn.ascend4.org/trunk@2468 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/johnpye/fprops/python/solve_Tx.py
+++ models/johnpye/fprops/python/solve_Tx.py
@@ -1,11 +1,12 @@
-import fprops,sys
+import sys
+from fprops import *
 
-D = fprops.fluid("toluene");
+D = fprops_fluid("toluene");
 
 T = 300;
 x = 0.5;
 
-res, rho = fprops.solve_Tx(T,x, D);
+res, rho = fprops_solve_Tx(T,x, D);
 
 if res:
 	print "failed to solve state, exiting."
@@ -15,15 +16,15 @@ print "solved state"
 
 print "T = %f, x = %f" % (T,x)
 
-h = fprops.helmholtz_h(T,rho,D);
-u = fprops.helmholtz_u(T,rho,D);
-s = fprops.helmholtz_s(T,rho,D);
+h = helmholtz_h(T,rho,D);
+u = helmholtz_u(T,rho,D);
+s = helmholtz_s(T,rho,D);
 
 print "--> rho = %f, h = %f, s = %f, u = %f" % (rho,h,u,s)
 
 print "\nchecking reverse solve:"
 
-res, psat, rhof, rhog = fprops.sat_T(T,D)
+res, psat, rhof, rhog = fprops_sat_T(T,D)
 
 x1 = (1./rho - 1./rhof) / (1./rhog - 1./rhof);
 
