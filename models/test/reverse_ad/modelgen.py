1a2ee550441b76ba586ac9b33e3a7e7123240534
John Pye <john.pye@anu.edu.au>
2012-12-13 07:29:48 +0000

    Fixing GPL header, removing postal address (rpmlint incorrect-fsf-address)
    git-svn-id: https://svn.ascend4.org/trunk@2651 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/test/reverse_ad/modelgen.py
+++ models/test/reverse_ad/modelgen.py
@@ -13,9 +13,8 @@
 #	GNU General Public License for more details.
 #
 #	You should have received a copy of the GNU General Public License
-#	along with this program; if not, write to the Free Software
-#	Foundation, Inc., 59 Temple Place - Suite 330,
-#	Boston, MA 02111-1307, USA.
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 # This script generates a model file that includes all the models under
 # models/test/ipopt which is later used for second derivative calculations
