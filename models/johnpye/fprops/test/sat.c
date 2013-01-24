326fa724bb8b60fd120ed2ec783c5c51f4a838f0
John Pye <john.pye@anu.edu.au>
2013-01-24 00:35:44 +0000

    Added testing of saturation curve for all fluids in their 'best' correlation. Six fluids are showing occasional failures, all of them PR EOS.
    git-svn-id: https://svn.ascend4.org/trunk@2671 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/johnpye/fprops/test/sat.c
+++ models/johnpye/fprops/test/sat.c
@@ -4,7 +4,7 @@
 #include "../sat.h"
 #include <assert.h>
 #include <math.h>
-
+#include "../fluids/fluids_list.h"
 #include "../color.h"
 
 #define MSG(FMT, ...) \
@@ -29,6 +29,72 @@ int main(void){
 	const PureFluid *P;
 	FpropsError err;
 	FluidState S;
+
+#define FNAME(F) #F
+#define COMMA ,
+	const char *helmfluids[] = { FLUIDS(FNAME,COMMA) COMMA RPPFLUIDS(FNAME,COMMA) };
+#undef FNAME
+#undef COMMA
+	const int n = sizeof(helmfluids)/sizeof(char *);
+	int i,j;
+	int nerrfluids = 0;
+	const char *errfluids[n];
+	MSG("Testing convergence of saturation curves for all helmholtz fluids...");
+	for(i=0; i<n; ++i){
+		int nerr = 0;
+		P = fprops_fluid(helmfluids[i],NULL,NULL);
+		if(!P){
+			MSG("Error initialising fluid '%s'",helmfluids[i]);
+			nerr = 1;
+			color_on(stdout,ASC_FG_BRIGHTRED);
+			fprintf(stdout,"I");
+			color_off(stdout);
+		}else{
+			double Tt = P->data->T_t;
+			double Tc = P->data->T_c;
+			if(Tt == 0){
+				color_on(stdout,ASC_FG_YELLOW);
+				fprintf(stdout,"T");
+				color_off(stdout);
+				Tt = 273.15 - 20;
+				if(Tt > Tc){
+					Tt = 0.5 * Tc;
+				}
+			}else{
+				color_on(stdout,ASC_FG_BRIGHTGREEN);
+				fprintf(stdout,"I");
+				color_off(stdout);
+			}
+			double nT = 170;
+			double rT = 1/Tt;
+			double drT = (1/Tc - 1/Tt) / nT;
+			for(j=0; j<nT; ++j){
+				double T = 1/rT;
+				double psat,rhof,rhog;
+				err = FPROPS_NO_ERROR;
+				fprops_sat_T(T, &psat, &rhof, &rhog, P, &err);
+				if(err){
+					nerr++;
+					color_on(stdout,ASC_FG_BRIGHTRED);
+					fprintf(stdout,"C");
+					color_off(stdout);
+				}else{
+					fprintf(stdout,".");
+				}
+				rT += drT;
+			}
+		}
+		fprintf(stdout,":%s\n",helmfluids[i]);
+		if(nerr)errfluids[nerrfluids++] = helmfluids[i];
+	}
+
+	MSG("There were %d fluids with saturation curve errors:",nerrfluids);
+	for(i=0; i<nerrfluids; ++i){
+		fprintf(stderr,"  %s",errfluids[i]);
+	}
+	fprintf(stderr,"\n");
+	if(nerrfluids)return nerrfluids;
+
 	double T0, rho, rhof, rhog, psat1, psat2;
 
 #define TEST_SAT(T1) \
@@ -52,7 +118,7 @@ int main(void){
 
 	const char *fluids[] = {"water","toluene","ethanol","isohexane", NULL};
 //	const char *fluids[] = {"toluene",NULL};
-	char **fi = fluids;
+	const char **fi = fluids;
 	while(*fi){
 		MSG("TESTING %s",*fi);
 		P = fprops_fluid(*fi,"pengrob",NULL);
