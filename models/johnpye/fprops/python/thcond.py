f39b6b47d69b3548cc86c599a44d43e50eac016a
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2014-03-27 12:32:40 +0000

    plot of co2 thcond, problems with saturation region.
    git-svn-id: https://svn.ascend4.org/trunk@2775 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/johnpye/fprops/python/thcond.py
+++ models/johnpye/fprops/python/thcond.py
@@ -1,3 +1,4 @@
+from pylab import *
 from fprops import *
 
 D = fluid("carbondioxide")
@@ -12,3 +13,22 @@ print S.mu
 print "CONDUCTIVITY"
 print S.lam
 
+hh = linspace(100e3, 2000e3,100)
+pp = [1.5e6,2.5e6, 5e6, 6e6,7e6,8e6,9e6,10e6, 20e6,50e6, 100e6]
+figure()
+hold(1)
+for p in pp:
+	print p
+	ll = []
+	TT = []
+	for h in hh:
+		S = D.set_ph(p,h)
+		lam = S.lam
+		T = S.T
+		rho = S.rho
+		ll.append(lam)
+		TT.append(T)
+		print "p=%f, h=%f -> T=%f, rho=%f, lam=%f"%(p,h,T,rho,lam)
+	plot(TT,ll)
+
+show()
