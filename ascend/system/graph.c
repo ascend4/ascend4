8553c667007009e0337a7555254da09296bcae71
jpye <jpye@f15cd148-d149-4907-926e-73cf92b3ee03>
2014-07-10 05:23:14 +0000

    added combined agraph/cgraph support
    git-svn-id: https://svn.ascend4.org/trunk@2795 f15cd148-d149-4907-926e-73cf92b3ee03

--- ascend/system/graph.c
+++ ascend/system/graph.c
@@ -29,12 +29,22 @@
 #include <ascend/general/platform.h>
 
 #ifdef WITH_GRAPHVIZ
-#define WITH_CGRAPH
-# include <cgraph.h>
 # include <gvc.h>
+#ifdef WITH_CGRAPH
+# include <cgraph.h>
+#endif
 #endif
 
 #define ASC_GV_LIBNAME "libgvc.so"
+#ifdef WITH_CGRAPH
+# define ASC_CGRAPH_LIBNAME "libcgraph.so"
+#else
+# define ASC_CGRAPH_LIBNAME ASC_GV_LIBNAME
+#endif
+
+#ifdef WITH_CGRAPH
+# error "Not ready for this yet"
+#endif
 
 #include "./graph.h"
 #include "slv_client.h"
@@ -72,53 +82,91 @@ int system_write_graph(slv_system_t sys
 
 	char temp[200];
 
-	/* function pointers */
-	GVC_t *(*gvConte)();
+	/* dynamic loading of stuff from the graphviz libraries... */
+#ifdef WITH_CGRAPH
+	/* gvc */
+	GVC_t *(*gvConte)(void);
+	void (*gvLayo)(GVC_t* , graph_t*, const char*engine);
+	void (*gvRend)(GVC_t* , graph_t*, const char*format, FILE*out);
+	/* cgraph */
 	Agdesc_t Agdirected = { 1, 0, 0, 1};
-	Agnode_t *(*agno)(Agraph_t* , char*, int);
-	Agraph_t *(*agop)(char* , Agdesc_t type, int);
-	Agsym_t *(*agatt)(Agraph_t * g, int kind, char *name, char *value);
-	Agedge_t *(*aged)(Agraph_t* , Agnode_t*, Agnode_t*, void*,int);
+	Agraph_t *(*agop)(char* , Agdesc_t type, Agdisc_t *disc);
+	Agsym_t *(*agatt)(Agraph_t *g, int kind, char *name, char *value);
+	Agnode_t *(*agno)(Agraph_t *g, char *name, int cflag);
+	Agedge_t *(*aged)(Agraph_t *g, Agnode_t *t, Agnode_t *h, char *name,int cflag);
+# define ASC_AGNODE(GRAPH,NAME) (*agno)(GRAPH,NAME,1)
+# define ASC_AGEDGE(GRAPH,N1,N2) (*aged)(GRAPH,N1,N2,NULL,1)
+#else // agraph
+	GVC_t *(*gvConte)();
+	Agnode_t *(*agno)(Agraph_t* , char*);
+	Agraph_t *(*agop)(char* , int);
+	void (*agatt) (Agraph_t* , char* , char*);
+	Agedge_t *(*aged)(Agraph_t* , Agnode_t*, Agnode_t*);
 	void (*gvLayo)(GVC_t* , Agraph_t*, char*);
 	void (*gvRend)(GVC_t* , Agraph_t*, char*, FILE*);
+# define ASC_AGNODE(GRAPH,NAME) (*agno)(GRAPH,NAME)
+# define ASC_AGEDGE(GRAPH,N1,N2) (*aged)(GRAPH,N1,N2)
+#endif
 
 	res = Asc_DynamicLoad(ASC_GV_LIBNAME,NULL);
 	if(res){
-		ERROR_REPORTER_NOLINE(ASC_USER_ERROR,"Unable to access GraphViz 'gvc' on your system. Is it installed?");
+		ERROR_REPORTER_NOLINE(ASC_USER_ERROR,"Unable to load GraphViz (gvc) on your system. Is it installed?");
 		return 1;
 	}
+#ifdef WITH_CGRAPH
+	res = Asc_DynamicLoad(ASC_CGRAPH_LIBNAME,NULL);
+	if(res){
+		ERROR_REPORTER_NOLINE(ASC_USER_ERROR,"Unable to access GraphViz (cgraph) on your system. Is it installed?");
+		return 1;
+	}
+#endif
 
-//	*(void **) (&Agdire) = Asc_DynamicFunction(ASC_GV_LIBNAME,"Agdirected");
 	*(void **) (&gvConte) = Asc_DynamicFunction(ASC_GV_LIBNAME,"gvContext");
-	*(void **) (&agop) = Asc_DynamicFunction(ASC_GV_LIBNAME,"agopen");
-	*(void **) (&agatt) = Asc_DynamicFunction(ASC_GV_LIBNAME,"agattr");
-	*(void **) (&agno) = Asc_DynamicFunction(ASC_GV_LIBNAME,"agnode");
-	*(void **) (&ags) = Asc_DynamicFunction(ASC_GV_LIBNAME,"agset");
-	*(void **) (&aged) = Asc_DynamicFunction(ASC_GV_LIBNAME,"agedge");
 	*(void **) (&gvLayo) = Asc_DynamicFunction(ASC_GV_LIBNAME,"gvLayout");
 	*(void **) (&gvRend) = Asc_DynamicFunction(ASC_GV_LIBNAME,"gvRender");
+	*(void **) (&agop) = Asc_DynamicFunction(ASC_CGRAPH_LIBNAME,"agopen");
+#ifdef WITH_CGRAPH
+	*(void **) (&agatt) = Asc_DynamicFunction(ASC_CGRAPH_LIBNAME,"agattr");
+#else
+	*(void **) (&agatt) = Asc_DynamicFunction(ASC_GV_LIBNAME,"agnodeattr");
+#endif
 
-	if(!gvConte || !agop || !agatt | !agno | !ags || !aged || !gvLayo || !gvRend /*|| !Agdire */){
-		ERROR_REPORTER_NOLINE(ASC_USER_ERROR,"Unable to access find required functions in dynamically-loaded library '%s'. Do you have the correct version installed?",ASC_GV_LIBNAME);
+	*(void **) (&agno) = Asc_DynamicFunction(ASC_CGRAPH_LIBNAME,"agnode");
+	*(void **) (&ags) = Asc_DynamicFunction(ASC_CGRAPH_LIBNAME,"agset");
+	*(void **) (&aged) = Asc_DynamicFunction(ASC_CGRAPH_LIBNAME,"agedge");
+
+	if(!gvConte || !gvLayo || !gvRend || !agop || !agatt | !agno | !ags || !aged){
+		ERROR_REPORTER_NOLINE(ASC_USER_ERROR,"Unable to access find required "
+			"functions in Graphviz dynamically-loaded library/ies Do you have the "
+			"correct version installed?"
+		);
 		return 1;
 	}
 
-	CONSOLE_DEBUG("Dynamically loaded '" ASC_GV_LIBNAME "' OK");
+	CONSOLE_DEBUG("Dynamically loaded GraphViz OK");
 
 	/* create the graph and its style details */
 	gvc = (*gvConte)();
-	g = (*agop)("g",Agdirected,0);
+#ifdef WITH_CGRAPH
+	g = (*agop)("g",Agdirected,NULL);
 	(*agatt)(g,AGNODE,"shape","ellipse");
 	(*agatt)(g,AGNODE,"label","");
 	(*agatt)(g,AGNODE,"color","");
 	(*agatt)(g,AGNODE,"style","");
+#else
+	g = (*agop)("g",AGDIGRAPH);
+	(*agatt)(g,"shape","ellipse");
+	(*agatt)(g,"label","");
+	(*agatt)(g,"color","");
+	(*agatt)(g,"style","");
+#endif
 
 	/* create notes for the relations */
 	for(i=0; i < id.neqn; ++i){
 		char *relname;
 		relname = rel_make_name(sys,id.rlist[i]);
 		sprintf(temp,"r%d",rel_sindex(id.rlist[i]));
-		n = (*agno)(g,temp,1);
+		n = ASC_AGNODE(g,temp);
 		(*ags)(n,"label",relname);
 		if(rel_satisfied(id.rlist[i])){
 			(*ags)(n,"style","filled");
@@ -134,7 +182,7 @@ int system_write_graph(slv_system_t sys
 		char *varname;
 		varname = var_make_name(sys,id.vlist[j]);
 		sprintf(temp,"v%d",var_sindex(id.vlist[j]));
-		n = (*agno)(g,temp,1);
+		n = ASC_AGNODE(g,temp);
 		(*ags)(n,"label",varname);
 		(*ags)(n, "shape", "box");
 		if(var_fixed(id.vlist[j])){
@@ -164,13 +212,13 @@ int system_write_graph(slv_system_t sys
 			const struct var_variable *v;
 			v = ivars[j];
 			sprintf(temp,"v%d",var_sindex(v));
-			n = (*agno)(g, reltemp,1);
-			m = (*agno)(g, temp,1);
+			n = ASC_AGNODE(g, reltemp);
+			m = ASC_AGNODE(g, temp);
 
 			if(id.v2pc[var_sindex(v)]==id.e2pr[rel_sindex(id.rlist[i])]){
-				(*aged)(g,n,m,NULL,1); /* from rel to var */
+				ASC_AGEDGE(g,n,m); /* from rel to var */
 			}else{
-				(*aged)(g,m,n,NULL,1); /* from var to rel */
+				ASC_AGEDGE(g,m,n); /* from var to rel */
 			}
 			edgecount++;
 		}
