f68e5cc6a9ae6153c62c20e04f62792764da4794
Jacob Shealy <jacob@ascend4.org>
2015-06-05 00:50:34 +0000

    Commit (2) of June 4, with latest version of test file `jacob_mix2'
    Added functions to guess initial densities for mixture components based 
    on what region (super-critical, etc.) the components are in when at the 
    given temperature and pressure.  Also added interrupt so that simulation 
    may be terminated if one or more fluid's density has not been 
    successfully found.
    
    
    git-svn-id: https://svn.ascend4.org/branches/jacob@2945 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/johnpye/fprops/test/jacob_mix2.c
+++ models/johnpye/fprops/test/jacob_mix2.c
@@ -47,7 +47,7 @@
 #include "../fprops.h"
 #include "../refstate.h"
 #include "../sat.h"
-#include "../zeroin.h"
+/* #include "../zeroin.h" */
 
 #include <stdio.h>
 #include <assert.h>
@@ -72,7 +72,10 @@ extern const EosData eos_rpp_methane;
 extern const EosData eos_rpp_water;
 
 /* Function prototypes */
+void mixture_x_props(unsigned nPure, double *xs, double *props);
+double mixture_x_fill_in(unsigned nPure, double *xs);
 void ig_rhos(double *rho_out, unsigned nPure, double T, double P, PureFluid **I, char **Names);
+void initial_rhos(double *rho_out, unsigned nPure, double T, double P, PureFluid **PFs, char **names, FpropsError *err);
 void pressure_rhos(double *rho_out, unsigned nPure, double T, double P, double tol, PureFluid **PF, char **Names, FpropsError *err);
 double mixture_rho(unsigned nPure, double *x, double *rhos);
 double mixture_u(unsigned nPure, double *xs, double *rhos, double T, PureFluid **PFs, FpropsError *err);
@@ -82,27 +85,128 @@ double mixture_cv(unsigned nPure, double *xs, double *rhos, double T, PureFluid 
 double mixture_x_ln_x(unsigned nPure, double *xs, PureFluid **PFs);
 double mixture_M_avg(unsigned nPure, double *xs, PureFluid **PFs);
 double mixture_s(unsigned nPure, double *xs, double *rhos, double T, PureFluid **PFs, FpropsError *err);
+void solve_mixture_conditions(unsigned nPure, double *xs, double *rhos, double T, PureFluid **PFs, char **Names, FpropsError *err);
 
 
 /* 
 	Function Definitions
  */
 
+/*	
+	Calculate mass fractions from an array of numbers, with each mass fraction 
+	sized proportionally to its corresponding number
+ */
+void mixture_x_props(unsigned nPure, double *xs, double *props){
+	unsigned i;
+	double x_total=0.0; /* sum of proportions */
+
+	for(i=0;i<nPure;i++){
+		x_total += props[i]; /* find sum of proportions */
+	}
+	/*	
+		Each mass fraction is its corresponding proportion, divided by the sum 
+		over all proportions.
+	 */
+	for(i=0;i<nPure;i++){
+		xs[i] = props[i] / x_total; 
+	}
+}
+
+/*
+	Calculate last of (n) mass fractions given an array of (n-1) mass 
+	fractions, such that the sum over all mass fractions will equal one.
+ */
+double mixture_x_fill_in(unsigned nPure, double *xs){
+	unsigned i;
+	double x_total;
+
+	for(i=0;i<(nPure-1);i++){ /* sum only for nPure-1 loops */
+		x_total += xs[i];
+	}
+	if(x_total>0){
+		printf(MIX_ERROR "%.6f.", x_total);
+	}
+	return 1-x_total;
+}
+
 /* 
 	Calculate ideal-gas densities `rho_out' from the temperature and pressure 
 	(used as starting densities for other routines)
 
 	Specifically, these densities may be useful if T > T_c (critical 
-	temperature), and P < P_c (critical pressure).
+	temperature), and P < P_c (sub-critical pressure).
  */
 void ig_rhos(double *rho_out, unsigned nPure, double T, double P, PureFluid **I, char **Names){
 	unsigned i; /* counter variable */
 
 	for(i=0;i<nPure;i++){
-		rho_out[i] = P / I[i]->data->R /T;
+		rho_out[i] = P / I[i]->data->R / T;
 		printf("\n\t%s%s is :  %.4f kg/m3", "The ideal-gas mass density of ",
 				Names[i], rho_out[i]);
 	} puts("");
+}
+
+/*
+	Set initial densities for use in `pressure_rhos', given an initial 
+	temperature and pressure.  Considers whether a substance is in critical or 
+	saturation regions.
+ */
+void initial_rhos(double *rho_out, unsigned nPure, double T, double P, PureFluid **PFs, char **Names, FpropsError *err){
+	unsigned i;
+	enum Region_Enum {SUPERCRIT, GASEOUS, LIQUID, VAPOR, SAT_VLE} Region;
+	char *region_names[] = {
+		"super-critical",
+		"gaseous",
+		"liquid",
+		"vapor",
+		"vapor-liquid equilibrium (saturation)"
+	};
+
+#define D PFs[i]->data
+	for(i=0;i<nPure;i++){
+		if(T >= D->T_c){ /* temperature >= critical temperature */
+			if(P >= D->p_c){       /* pressure >= critical pressure */
+				rho_out[i] = D->rho_c;     /* super-critical fluid */
+				Region = SUPERCRIT;
+			}else{                 /* true gas (as opposed to sub-critical vapor) */
+				rho_out[i] = P / D->R / T; /* density from ideal gas */
+				Region = GASEOUS;
+			}
+		}else{
+			if(P >= D->p_c){ /* pressure >= critical pressure -- liquid */
+				rho_out[i] = fprops_rhof_T_rackett(T, D); /* density from saturation liquid */
+				Region = LIQUID;
+			}else{ /* now we're getting into hard conditions */
+				double T_sat,   /* find saturation temperature, densities */
+					   rho_liq,
+					   rho_vap;
+				fprops_sat_p(P, &T_sat, &rho_liq, &rho_vap, PFs[i], err);
+
+				if(T > T_sat){ /* this is the last condition that we can precisely find */
+					rho_out[i] = rho_vap; /* vapor phase */
+					Region = VAPOR;
+				}else{
+					rho_out[i] = (rho_liq + rho_vap) / 2; /* both liquid and vapor */
+					/*	
+						We are in vapor-liquid equilibrium, and the density 
+						cannot be determined without knowing how much of the 
+						substance is in the vapor and how much in the liquid.
+					 */
+					/*printf("\n\tThe substance %s is in vapor-liquid equilibrium; "
+							"it has been assigned a provisional density %.5f kg/m3.",
+							Names[i], rho_out[i]);*/
+					Region = SAT_VLE;
+				}
+			}
+		}
+		printf("\n\tThe substance %s was assigned a density of %.5f kg/m3;\n"
+				"\t  it is in the %s region, since\n"
+				"\t\tCritical temperature T_c=%.2f K \tand current temperature T=%.2f K;\n"
+				"\t\tCritical pressure    P_c=%.0f Pa\tand current pressure    P=%.0f Pa.",
+				Names[i], rho_out[i], region_names[Region], D->T_c, T, D->p_c, P);
+	}
+#undef D
+
 }
 
 /* 
@@ -119,16 +223,12 @@ void pressure_rhos(double *rho_out, unsigned nPure, double T, double P, double t
 		starting points for this search; e.g. if P > critical pressure, start 
 		from critical density, etc.
 
-		I take inspiration from the structure of `zeroin_solve' in how it takes 
-		inputs and outputs
-
 		Compare with:
 			zeroin_solve               in  zeroin.c
 			helmholtz_sat              in  helmholtz.c
 			sat_p_resid, fprops_sat_p  in  sat.c (example of using zeroin_solve)
 			fprops_sat_hf              in  sat.c
 			fprops_solve_ph            in  solve_ph.c (uses fprops_sat_p from sat.c)
-		This is partly for my own reference.
 	 */
 	unsigned i1, i2;   /* counter variables */
 	double p1, p2,     /* pressures */
@@ -145,19 +245,24 @@ void pressure_rhos(double *rho_out, unsigned nPure, double T, double P, double t
 
 			if(fabs(P - p1) < tol){ /* Success! */
 				rho_out[i1] = rho1;
-				printf("\n\tRoot-finding succeeded at rho1 = %.5f kg/m3, p1 = %.0f Pa "
-						"(and P = %.0f Pa), after %d iterations.", 
-						rho1, p1, P, i2);
+				printf("\n\tRoot-finding succeeded for substance %s after %d iterations;\n"
+						"\t  at rho1=%.5f kg/m3, p1=%.0f Pa (and P=%.0f Pa).", 
+						Names[i1], i2, rho1, p1, P);
 				break;
 			}
 			if(p1==p2){
-				printf("\nWith %s, got p1 = p2 = %.6e Pa, but P = %.6e Pa!",
-						Names[i1], p1, P);
+				printf("\n\tRoot-finding FAILED for substance %s after %d iterations;\n"
+						"\t  at rho1=%.5f kg/m3, rho2=%.5f, got p1=p2=%.6e Pa, but P = %.6e Pa!",
+						Names[i1], i2, rho1, rho2, p1, P);
+				break;
 			}
 
 			/*
-				unlike in sat.c, here delta_rho does not need to check bounds, 
-				since we do not need to stay in the saturation region
+				Unlike in sat.c, here delta_rho does not need to check bounds, 
+				since we do not need to stay in the saturation region.
+
+				However, I will eventually need to provide for correct handling 
+				of saturation conditions.
 			 */
 			delta_rho = (P - p1) * (rho1 - rho2) / (p1 - p2);
 			rho2 = rho1;
@@ -358,94 +463,51 @@ double mixture_M_avg(unsigned nPure, double *x_mass, PureFluid **PFs){
 
 	for(i=0;i<nPure;i++){
 		rM_avg += x_mass[i] / PFs[i]->data->M;
+		x_total += x_mass[i];
+	}
+	if(fabs(x_total - 1) > MIX_XTOL){
+		printf(MIX_XSUM_ERROR, x_total);
 	}
 	return 1. / rM_avg;
-}
+} /* end of `mixture_M_avg' */
 
 /*
 	Calculate the overall ideal-solution entropy per unit mass in a mixture of 
 	pure components
  */
 
-
 /*
-	Establish mixing conditions (T,P), find individual densities, and use those 
-	along with the temperature to find first-law properties for individual 
-	components and the whole mixture.
-	
-	As a check, find pressure corresponding to temperature and component density 
-	for each component -- this should equal the overall pressure
+	Calculate and print mixture properties by various means
  */
-int main(){
-	int i; /* loop counter used throughout `main' */
-	enum FluidAbbrevs {N2,NH3,CO2,/*CH4,H2O,*/NFLUIDS}; /* fluids that will be used */
+void solve_mixture_conditions(unsigned nPure, double *xs, double *rhos, double T, PureFluid **PFs, char **Names, FpropsError *err){
+	int i; /* loop counter */
+	double rho_mx1 = mixture_rho(nPure, xs, rhos), /* mixture properties */
+		   u_mx1   = mixture_u(nPure, xs, rhos, T, PFs, err),
+		   h_mx1   = mixture_h(nPure, xs, rhos, T, PFs, err),
+		   cp_mx1  = mixture_cp(nPure, xs, rhos, T, PFs, err),
+		   cv_mx1  = mixture_cv(nPure, xs, rhos, T, PFs, err);
 
-	const EosData *IdealEos[]={
-		&eos_rpp_nitrogen, 
-		&eos_rpp_ammonia, 
-		&eos_rpp_carbon_dioxide, 
-		&eos_rpp_methane, 
-		&eos_rpp_water
-	};
-	char *FluidNames[]={
-		"nitrogen", "ammonia", "carbondioxide", "methane", "water"
-	};
-	PureFluid *Ideals[NFLUIDS];
-	PureFluid *Helms[NFLUIDS];
+	double x_ln_x_mx1 = mixture_x_ln_x(nPure, xs, PFs),
+		   M_avg_mx1  = mixture_M_avg(nPure, xs, PFs);
 
-	ReferenceState ref = {FPROPS_REF_REF0}; /* reference and error */
-	FpropsError err = FPROPS_NO_ERROR;
-
-	/*	
-		Fill the `Ideals' and `Helms' PureFluid arrays with data for ideal and 
-		helmholtz equations of state.  Ideal-gas equation of state is used for 
-		comparison, although eventually I will try to set conditions far from 
-		the ideal-gas case
-	 */
-	for(i=N2;i<NFLUIDS;i++){
-		Ideals[i] = ideal_prepare(IdealEos[i],&ref);
-		Helms[i] = fprops_fluid(FluidNames[i],"helmholtz",NULL);
-	}
-
-	/* Mixing conditions (temperature,pressure), and mass fractions */
-	double T=300; /* K */
-	double P=1e5; /* Pa */
-	double x[NFLUIDS]={0.5, 0.3, 0.2}; /* mass fractions */
-	double rho[NFLUIDS];             /* individual densities */
-
-	/* Find ideal-gas density, to use as a starting point */
-	ig_rhos(rho, NFLUIDS, T, P, Ideals, FluidNames);
-
-	double tol = 1e-9;
-	pressure_rhos(rho, NFLUIDS, T, P, tol, Helms, FluidNames, &err);
-
-	double rho_mx1 = mixture_rho(NFLUIDS, x, rho), /* mixture properties */
-		   u_mx1   = mixture_u(NFLUIDS, x, rho, T, Helms, &err),
-		   h_mx1   = mixture_h(NFLUIDS, x, rho, T, Helms, &err),
-		   cp_mx1  = mixture_cp(NFLUIDS, x, rho, T, Helms, &err),
-		   cv_mx1  = mixture_cv(NFLUIDS, x, rho, T, Helms, &err);
-
-	double x_ln_x_mx1 = mixture_x_ln_x(NFLUIDS, x, Helms),
-		   M_avg_mx1  = mixture_M_avg(NFLUIDS, x, Helms);
-
-	double u_mx2=0.0, /* alternate mixture properties */
+	double u_mx2=0.0,  /* alternate mixture properties */
 		   h_mx2=0.0,
 		   cp_mx2=0.0,
 		   cv_mx2=0.0;
-	double p_i=0.0, /* component properties */
+	double p_i=0.0,    /* component properties */
 		   u_i=0.0,
 		   h_i=0.0,
 		   cp_i=0.0,
 		   cv_i=0.0;
 	FluidState fs_i;
 	/* Find mixture properties in a loop */
-	for(i=N2;i<NFLUIDS;i++){
-		fs_i = (FluidState){T,rho[i],Helms[i]};
-		p_i  = fprops_p(fs_i, &err);
-		u_i  = fprops_u(fs_i, &err);
-		h_i  = fprops_h(fs_i, &err);
-		cp_i = fprops_cp(fs_i, &err);
-		cv_i = fprops_cv(fs_i, &err);
+	for(i=0;i<nPure;i++){
+		fs_i = (FluidState){T,rhos[i],PFs[i]};
+		p_i  = fprops_p(fs_i, err);
+		u_i  = fprops_u(fs_i, err);
+		h_i  = fprops_h(fs_i, err);
+		cp_i = fprops_cp(fs_i, err);
+		cv_i = fprops_cv(fs_i, err);
 		printf("\n\t%s %s"
 				"\n\t\t%s\t\t:  %.3f;"
 				"\n\t\t%s\t\t\t:  %.0f Pa;"
@@ -453,33 +515,29 @@ int main(){
 				"\n\t\t%s\t\t\t:  %g J/kg;"
 				"\n\t\t%s\t:  %g J/kg/K;"
 				"\n\t\t%s\t:  %g J/kg/K.\n",
-				"For the substance", FluidNames[i],
-				"the mass fraction is", x[i],
+				"For the substance", Names[i],
+				"the mass fraction is", xs[i],
 				"the pressure is", p_i,
 				"the internal energy is", u_i,
 				"the enthalpy is", h_i,
 				"the isobaric heat capacity is", cp_i,
 				"the isometric heat capacity is", cv_i);
-		u_mx2  += x[i] * u_i;
-		h_mx2  += x[i] * h_i;
-		cp_mx2 += x[i] * cp_i;
-		cv_mx2 += x[i] * cv_i;
+		u_mx2  += xs[i] * u_i;
+		h_mx2  += xs[i] * h_i;
+		cp_mx2 += xs[i] * cp_i;
+		cv_mx2 += xs[i] * cv_i;
 	}
 	printf("\n  %s\n\t%s\t\t:  %f kg/m3"
-			"\n\t%s\t:  %g J/kg"
-			"\n\t%s\t\t:  %g J/kg"
-			"\n\t%s\t:  %g J/kg/K"
-			"\n\t%s\t:  %g J/kg/K\n",
+			"\n\t%s\t:  %g J/kg \n\t%s\t\t:  %g J/kg"
+			"\n\t%s\t:  %g J/kg/K \n\t%s\t:  %g J/kg/K\n",
 			"For the mixture properties calculated with functions",
 			"The density of the mixture is", rho_mx1,
 			"The internal energy of the mixture is", u_mx1,
 			"The enthalpy of the mixture is", h_mx1,
 			"The constant-pressure heat capacity is", cp_mx1,
 			"The constant-volume heat capacity is", cv_mx1);
-	printf("\n  %s\n\t%s\t:  %g J/kg"
-			"\n\t%s\t\t:  %g J/kg"
-			"\n\t%s\t:  %g J/kg/K"
-			"\n\t%s\t:  %g J/kg/K\n",
+	printf("\n  %s\n\t%s\t:  %g J/kg \n\t%s\t\t:  %g J/kg"
+			"\n\t%s\t:  %g J/kg/K \n\t%s\t:  %g J/kg/K\n",
 			"For the mixture properties calculated directly",
 			"The internal energy of the mixture is", u_mx2,
 			"The enthalpy of the mixture is", h_mx2,
@@ -496,6 +554,64 @@ int main(){
 	printf("\n  %s:\n\t%s\t:  %.5f\n\t%s\t:  %.5f kg/kmol.\n",
 			"The second-law properties of the solution are",
 			"sum of (x ln(x))", x_ln_x_mx1, "average molar mass", M_avg_mx1);
+} /* end of `solve_mixture_conditions' */
+
+
+/*
+	Establish mixing conditions (T,P), find individual densities, and use those 
+	along with the temperature to find first-law properties for individual 
+	components and the whole mixture.
+	
+	As a check, find pressure corresponding to temperature and component density 
+	for each component -- this should equal the overall pressure
+ */
+int main(){
+	enum FluidAbbrevs {N2,NH3,CO2,CH4,/* H2O, */NFLUIDS}; /* fluids that will be used */
+
+	char *FluidNames[]={
+		"nitrogen", "ammonia", "carbondioxide", "methane", "water"
+	};
+	PureFluid *Helms[NFLUIDS];
+	FpropsError err = FPROPS_NO_ERROR;
+
+	/*	
+		Fill the `Helms' PureFluid array with data from the helmholtz equation 
+		of state.
+	 */
+	int i;
+	for(i=N2;i<NFLUIDS;i++){
+		Helms[i] = fprops_fluid(FluidNames[i],"helmholtz",NULL);
+	}
+
+	/* Mixing conditions (temperature,pressure), and mass fractions */
+	double T=250;        /* K */
+	double P=1.5e5;      /* Pa */
+	double rho[NFLUIDS]; /* individual densities */
+	double x[NFLUIDS];   /* mass fractions */
+
+	double props[] = {1, 3, 2, 1.5, 2.5}; /* proportions of mass fractions */
+	mixture_x_props(NFLUIDS, x, props);
+
+	/* Find ideal-gas density, to use as a starting point */
+	initial_rhos(rho, NFLUIDS, T, P, Helms, FluidNames, &err);
+
+	double tol = 1e-9;
+	pressure_rhos(rho, NFLUIDS, T, P, tol, Helms, FluidNames, &err);
+
+	int usr_cont;
+	char temp_str[100];
+	printf("\n  %s\n\t%s\n\t%s",
+			"Continue to calculate and print solution properties?",
+			"0 - No", "1 - Yes");
+	do{
+		printf("\n    %s ", "Choice?");
+		fgets(temp_str, 100, stdin);
+	}while((1 != sscanf(temp_str, "%i", &usr_cont) || (0>usr_cont || 1<usr_cont)) 
+			&& printf("\n  %s", "You must enter either 0 or 1"));
+	if(1==usr_cont){
+		solve_mixture_conditions(NFLUIDS, x, rho, T, Helms, FluidNames, &err);
+	}
 
 	return 0;
-}
+} /* end of `main' */
+
