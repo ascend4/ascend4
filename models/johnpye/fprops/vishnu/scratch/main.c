988fa961754b6b19923e37f8c5c9ae669ae7046c
Vishnu <vishnu@ascend4.org>
2016-06-13 02:22:51 +0000

    added evaluation functions, test results generated, testing unsuccesfull
    git-svn-id: https://svn.ascend4.org/branches/vishnu@3116 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/johnpye/fprops/vishnu/scratch/main.c
+++ models/johnpye/fprops/vishnu/scratch/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <math.h>
 
 #define BUF 1024
 
@@ -30,6 +31,10 @@
 				test_liq->Q.type = (char*)malloc(sizeof("exppolynomial")); \
 				strcpy(test_liq->Q.type,"exppolynomial"); \
 			} \
+			else if(strstr(word,"\"type\": \"exponential\"")) { \
+				test_liq->Q.type = (char*)malloc(sizeof("exponential")); \
+				strcpy(test_liq->Q.type,"exponential"); \
+			} \
 		} \
 		if(strcmp(test_liq->Q.type,"notdefined")) { \
 			fseek(in,pos,SEEK_SET); \
@@ -55,7 +60,6 @@
 			while(numc) { \
 				numc--; \
 				test_liq->Q.coeff[numc] = coefs[numc]; \
-				printf("%e\n",test_liq->Q.coeff[numc]); \
 			} \
 		} 
 
@@ -99,10 +103,58 @@ double to_number(char w[BUF]) {
 
 	return (atof(token));
 
+}
+
+// eveluation function for polynamial type coefficients
+double eval_poly(coefficients c, double T, double x) {
+
+	double res = 0.0;
+	int i;
+
+	for(i=1;i<c.numc;i++)
+		res += c.coeff[i]*pow(T,i-1);
+
+	return res;
+
+}
+
+// eveluation function for exponential-polynomial type coefficients
+double eval_exppoly(coefficients c, double T, double x) {
+
+	return exp(eval_poly(c,T,x));
+
+}
+
+// eveluation function for exponential type coefficients
+double eval_expo(coefficients c, double T, double x) {
+
+	return exp(c.coeff[1]/(T+c.coeff[2])+c.coeff[3]);
+
 }	
+
+// generalized property evaluation functions: macro definition
+#define PROP_EVAL(Q) \
+	double eval_ ## Q (fprops *fluid, double T, double x) { \
+		assert(T>0&&T>=fluid->T_min&&T<=fluid->T_max); \
+		assert(x>=fluid->x_min&&x<=fluid->x_max); \
+		if(!strcmp(fluid->Q.type,"polynomial")) return eval_poly(fluid->Q,T,x); \
+		else if(!strcmp(fluid->Q.type,"exppolynomial")) return eval_exppoly(fluid->Q,T,x); \
+		else if(!strcmp(fluid->Q.type,"exponential")) return eval_expo(fluid->Q,T,x); \
+		else { \
+			printf("\nType not defined.\n"); \
+			ABORT \
+		} \
+	}
+
+// declaration and definition of property evaluation functions for conductivity, density, specific-heat and viscosity
+PROP_EVAL(conductivity)
+PROP_EVAL(density)
+PROP_EVAL(specific_heat)
+PROP_EVAL(viscosity)
 
 int main() {
 
+// parsing from input (json format) and loading data to new fluid data structures
 	FILE* in;
 
 	fprops* test_liq;
@@ -121,20 +173,20 @@ int main() {
 
 	while(!feof(in)) {
 
-		fgets(word,BUF,in);
+		fgets(word,BUF,in); 
 
-		if(strstr(word,"T_freeze")) { READ(T_freeze); } // not required for incompressible EOS; implemented to check input coefficients of "type": "notdefined"
-		else if(strstr(word,"Tbase")) test_liq->T_base = extracted_number(word);
-		else if(strstr(word,"Tmax")) test_liq->T_max = extracted_number(word);
-		else if(strstr(word,"Tmin")) test_liq->T_min = extracted_number(word);
-		else if(strstr(word,"TminPsat")) test_liq->T_minPsat = extracted_number(word);
-		else if(strstr(word,"conductivity")) { READ(conductivity); }
-		else if(strstr(word,"density")) { READ(density); }
-		else if(strstr(word,"specific_heat")) { READ(specific_heat); }
-		else if(strstr(word,"viscosity")) { READ(viscosity); }
-		else if(strstr(word,"xbase")) test_liq->x_base = extracted_number(word);
-		else if(strstr(word,"xmax")) test_liq->x_max = extracted_number(word);
-		else if(strstr(word,"xmin")) test_liq->x_min = extracted_number(word);
+		if(strstr(word,"\"T_freeze\"")) { READ(T_freeze); } // not required for incompressible EOS; implemented to check input coefficients of "type": "notdefined"
+		else if(strstr(word,"\"Tbase\"")) test_liq->T_base = extracted_number(word);
+		else if(strstr(word,"\"Tmax\"")) test_liq->T_max = extracted_number(word); 
+		else if(strstr(word,"\"Tmin\"")) test_liq->T_min = extracted_number(word); 
+		else if(strstr(word,"\"TminPsat\"")) test_liq->T_minPsat = extracted_number(word);
+		else if(strstr(word,"\"conductivity\"")) { READ(conductivity); }
+		else if(strstr(word,"\"density\"")) { READ(density); }
+		else if(strstr(word,"\"specific_heat\"")) { READ(specific_heat); }
+		else if(strstr(word,"\"viscosity\"")) { READ(viscosity); }
+		else if(strstr(word,"\"xbase\"")) test_liq->x_base = extracted_number(word);
+		else if(strstr(word,"\"xmax\"")) test_liq->x_max = extracted_number(word);
+		else if(strstr(word,"\"xmin\"")) test_liq->x_min = extracted_number(word);
 		else if(strstr(word,"\"xid\": \"pure\"")) {
 				test_liq->x_id = (char*)malloc(sizeof("pure")); 
 				strcpy(test_liq->x_id,"pure"); 
@@ -143,6 +195,22 @@ int main() {
 	}
 
 	fclose(in);
+
+// test
+	double T = test_liq->T_min;
+	
+	FILE *out;
+	out = fopen("test_res.dat","w");
+
+	fprintf(out,"VARIABLES = \"Temperature [C]\", \"Conductivity [W/m/K]\", \"Density [kg/m<sup>3</sup>]\", \"Heat Capacity [J/Kg/K]\", \"Viscosity [Pa s]\"\n");
+	do {
+
+		fprintf(out,"%e\t%e\t%e\t%e\t%e\n",T-273.0,eval_conductivity(test_liq, T, 1),eval_density(test_liq, T, 1),eval_specific_heat(test_liq, T, 1),eval_viscosity(test_liq, T, 1));
+		T+=1;
+
+	} while(T<=test_liq->T_max);
+
+	fclose(out);
 
 	return 0;
 
