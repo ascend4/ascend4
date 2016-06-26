e40c0eaca0c1d5b03b0703a9cc6a5835e98ad54b
Vishnu <vishnu@ascend4.org>
2016-06-26 04:58:07 +0000

    working incompressible EOS stand alone code for pure and mixture fluids: ready to be linked to FPROPS
    git-svn-id: https://svn.ascend4.org/branches/vishnu@3120 f15cd148-d149-4907-926e-73cf92b3ee03

--- models/johnpye/fprops/vishnu/scratch/main.c
+++ models/johnpye/fprops/vishnu/scratch/main.c
@@ -9,12 +9,13 @@
 
 #define MAXC 10
 
-#define PATH "incomp_liq_data/Water.dat"
+#define PATH "incomp_liq_data/LiBr.dat"
 
 #define ABORT \
 		printf("\nExiting Program!\n"); \
 		exit(0);
 
+// json parser
 #define READ(Q)	\
 		pos = ftell(in); \
 		while(!strstr(word,"},")) { \
@@ -36,69 +37,46 @@
 				strcpy(test_liq->Q.type,"exponential"); \
 			} \
 		} \
-		if(!strcmp(test_liq->Q.type,"polynomial")||!strcmp(test_liq->Q.type,"exppolynomial")) { \
+		if(strcmp(test_liq->Q.type,"notdefined")) { \
 			fseek(in,pos,SEEK_SET); \
 			fgets(word,BUF,in); \
-			double coefs[MAXC]; \
-			int numc = 0; \
+			double coefs[MAXC][MAXC]; \
+			int numc_row = 0, numc_col = 0; \
 			while(!strstr(word,"},")) { \
-				if(strstr(word,"NRMS")) { \
-					coefs[numc] = extracted_number(word); \
-					numc++; \
-					assert(numc==1); \
+				if((strstr(word,"e+")||strstr(word,"e-"))&&!strstr(word,"NRMS")) { \
+					coefs[numc_row][numc_col] = to_number(word); \
+					numc_col++; \
+					assert(numc_col<MAXC);  \
 				} \
-				else if(!strstr(word,"coeffs")&&strstr(word,"[")) { \
-					fgets(word,BUF,in); \
-					coefs[numc] = to_number(word); \
-					numc++; \
-					assert(numc<MAXC);  \
+				else if(strstr(word,"],")) { \
+					numc_row++; \
+					test_liq->Q.numc_c = numc_col; \
+					numc_col = 0; \
+					assert(numc_row<MAXC);  \
 				} \
 				fgets(word,BUF,in); \
 			} \
-			test_liq->Q.coeff = (double*)malloc(numc*sizeof(double)); \
-			test_liq->Q.numc = numc; \
-			while(numc) { \
-				numc--; \
-				test_liq->Q.coeff[numc] = coefs[numc]; \
-			} \
-		} \
-		else if(!strcmp(test_liq->Q.type,"exponential")) { \
-			fseek(in,pos,SEEK_SET); \
-			fgets(word,BUF,in); \
-			double coefs[MAXC]; \
-			int numc = 0; \
-			while(!strstr(word,"},")) { \
-				if(strstr(word,"NRMS")) { \
-					coefs[numc] = extracted_number(word); \
-					numc++; \
-					assert(numc==1); \
-				} \
-				else if(strstr(word,"e+")||strstr(word,"e-")) { \
-					coefs[numc] = to_number(word); \
-					numc++; \
-					assert(numc<MAXC);  \
-				} \
-				fgets(word,BUF,in); \
-			} \
-			test_liq->Q.coeff = (double*)malloc(numc*sizeof(double)); \
-			test_liq->Q.numc = numc; \
-			while(numc) { \
-				numc--; \
-				test_liq->Q.coeff[numc] = coefs[numc]; \
-			} \
+			test_liq->Q.numc_r = numc_row; \
+			test_liq->Q.coeff = (double**)malloc(test_liq->Q.numc_r*sizeof(double*)); \
+			int i,j; \
+			for(i=0;i<test_liq->Q.numc_r;i++) \
+				test_liq->Q.coeff[i] = (double*)malloc(test_liq->Q.numc_c*sizeof(double)); \
+			for(i=0;i<test_liq->Q.numc_r;i++) \
+				for(j=0;j<test_liq->Q.numc_c;j++) \
+					test_liq->Q.coeff[i][j] = coefs[i][j]; \
 		} 
 
 typedef struct {
 
 	char* type;
-	double* coeff;
-	int numc;	
+	double** coeff;
+	int numc_r, numc_c;	// number of rows and columns in coefficient matrix respectively
 
 } coefficients;
 
 typedef struct {
 
-	coefficients T_freeze, conductivity, density, specific_heat, viscosity;
+	coefficients T_freeze, conductivity, density, specific_heat, viscosity, saturation_pressure;
 	char* x_id;
 	char* description;
 	double T_base, T_max, T_min, T_minPsat, x_base, x_max, x_min; 
@@ -130,30 +108,31 @@ double to_number(char w[BUF]) {
 
 }
 
-// eveluation function for polynamial type coefficients
+// evaluation function for polynamial type coefficients
 double eval_poly(coefficients c, double T, double x) {
 
 	double res = 0.0;
-	int i;
+	int i,j;
 
-	for(i=1;i<c.numc;i++)
-		res += c.coeff[i]*pow(T,i-1);
+	for(i=0;i<c.numc_r;i++)
+		for(j=0;j<c.numc_c;j++)
+			res += pow(x,j)*c.coeff[i][j]*pow(T,i);
 
 	return res;
 
 }
 
-// eveluation function for exponential-polynomial type coefficients
+// evaluation function for exponential-polynomial type coefficients
 double eval_exppoly(coefficients c, double T, double x) {
 
 	return exp(eval_poly(c,T,x));
 
 }
 
-// eveluation function for exponential type coefficients
+// evaluation function for exponential type coefficients
 double eval_expo(coefficients c, double T, double x) {
 
-	return exp(c.coeff[1]/(T+c.coeff[2])-c.coeff[3]);
+	return exp(c.coeff[0][0]/(T+c.coeff[0][1])-c.coeff[0][2]);
 
 }	
 
@@ -164,7 +143,7 @@ double eval_expo(coefficients c, double T, double x) {
 		assert(x>=fluid->x_min&&x<=fluid->x_max); \
 		if(!strcmp(fluid->Q.type,"polynomial")) return eval_poly(fluid->Q,T-fluid->T_base,x-fluid->x_base); \
 		else if(!strcmp(fluid->Q.type,"exppolynomial")) return eval_exppoly(fluid->Q,T-fluid->T_base,x-fluid->x_base); \
-		else if(!strcmp(fluid->Q.type,"exponential")) return eval_expo(fluid->Q,T,0); \
+		else if(!strcmp(fluid->Q.type,"exponential")) return eval_expo(fluid->Q,T,x); \
 		else { \
 			printf("\nType not defined.\n"); \
 			ABORT \
@@ -176,6 +155,7 @@ PROP_EVAL(conductivity)
 PROP_EVAL(density)
 PROP_EVAL(specific_heat)
 PROP_EVAL(viscosity)
+PROP_EVAL(saturation_pressure)
 
 int main() {
 
@@ -209,6 +189,7 @@ int main() {
 		else if(strstr(word,"\"density\"")) { READ(density); }
 		else if(strstr(word,"\"specific_heat\"")) { READ(specific_heat); }
 		else if(strstr(word,"\"viscosity\"")) { READ(viscosity); }
+		else if(strstr(word,"\"saturation_pressure\"")) { READ(saturation_pressure); }
 		else if(strstr(word,"\"xbase\"")) test_liq->x_base = extracted_number(word);
 		else if(strstr(word,"\"xmax\"")) test_liq->x_max = extracted_number(word);
 		else if(strstr(word,"\"xmin\"")) test_liq->x_min = extracted_number(word);
@@ -227,10 +208,10 @@ int main() {
 	FILE *out;
 	out = fopen("test_res.dat","w");
 
-	fprintf(out,"VARIABLES = \"Temperature [C]\", \"Conductivity [W/m/K]\", \"Density [kg/m<sup>3</sup>]\", \"Heat Capacity [J/Kg/K]\", \"Viscosity [Pa s]\"\n");
+	fprintf(out,"VARIABLES = \"Temperature [C]\", \"Conductivity [W/m/K]\", \"Density [kg/m<sup>3</sup>]\", \"Heat Capacity [J/Kg/K]\", \"Viscosity [Pa s]\", \"Saturation Pressure [Pa]\"\n");
 	do {
 
-		fprintf(out,"%e\t%e\t%e\t%e\t%e\n",T-273.0,eval_conductivity(test_liq, T, 1),eval_density(test_liq, T, 1),eval_specific_heat(test_liq, T, 1),eval_viscosity(test_liq, T, 1));
+		fprintf(out,"%e\t%e\t%e\t%e\t%e\t%e\n",T-273.0,eval_conductivity(test_liq, T, 0.39),eval_density(test_liq, T, 0.39),eval_specific_heat(test_liq, T, 0.39),eval_viscosity(test_liq, T, 0.39),eval_saturation_pressure(test_liq, T, 0.39));
 		T+=1;
 
 	} while(T<=test_liq->T_max);
