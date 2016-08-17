c0cdd88b42fb7c1a85088a55aec45c23c4ae12ed
Vishnu <vishnu@ascend4.org>
2016-08-17 06:38:40 +0000

    git-svn-id: https://svn.ascend4.org/branches/vishnu@3169 f15cd148-d149-4907-926e-73cf92b3ee03

---
+++ models/johnpye/fprops/vishnu/incompressible mixture files/fprops.h
@@ -0,0 +1,122 @@
+/*
+ * FPROPS
+ * Copyright (C) 2011 - Carnegie Mellon University
+ *
+ * ASCEND is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ASCEND is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ASCEND; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+#ifndef FPROPS_H
+#define FPROPS_H
+
+#include "rundata.h"
+
+/**
+	State object for FPROPS. This struct allows user-friendly API in a similar
+	way to in freesteam, but supports different fluid types and correlations,
+	and might be extensible to support fluid mixtures.
+
+	TODO if we've got a saturated state then we almost certainly have calculated
+	rhof, rhog, p. They're expensive, so we should save them in this state
+	struct, too, or else find some other way to cache them.
+
+	TODO perhaps eventually we can different different correlations using
+	different independent variables, in which case this state could be modified/
+	expanded/improved.
+*/
+typedef struct FluidState_struct{
+	double T; ///< temperature / K
+	double rho; ///< density / kg/m3
+	double p; ///< Pressure / [Pa]
+	double x;
+	const PureFluid *fluid; ///< pointer to fluid description and associated functions
+} FluidState;
+
+FluidState fprops_set_Trho(double T, double rho, const PureFluid *fluid, FpropsError *err);
+FluidState fprops_set_Tpx(double T, double p, double x, const PureFluid *fluid, FpropsError *err); // added for incompressible
+
+/* TODO we need to add a way to specify what fluid correlation is desired
+and also what reference state, as another option. */
+
+//FpropsError* fprops_get_err_pointer(void);
+
+/*The following take the fluid data and use the function pointers
+  to call the correct function (e.g. fprops_p -> helmholtz_p)*/
+
+double fprops_rho(FluidState state, FpropsError *err); ///< density / kg/m3 // added for incompressible
+double fprops_p(FluidState state, FpropsError *err); ///< Pressure / [Pa]
+double fprops_u(FluidState state, FpropsError *err); ///< Specific internal energy / [J/kg]
+double fprops_h(FluidState state, FpropsError *err); ///< Specific enthalpy / [J/kg]
+double fprops_s(FluidState state, FpropsError *err); ///< Specific entropy / [J/kg/K]
+double fprops_a(FluidState state, FpropsError *err); ///< Specific helmholtz energy / [J/kg]
+double fprops_cv(FluidState state, FpropsError *err);///< Specific isochoric heat capacity / [J/kg/K]
+double fprops_cp(FluidState state, FpropsError *err);///< Specific isobaric heat capacity / [J/kg/K]
+double fprops_w(FluidState state, FpropsError *err); ///< Speed of sound / [m/s]
+double fprops_g(FluidState state, FpropsError *err); ///< Specific Gibbs energy / [J/kg]
+
+double fprops_alphap(FluidState state, FpropsError *err);
+double fprops_betap(FluidState state, FpropsError *err);
+
+double fprops_cp0(FluidState state, FpropsError *err); ///< Specific isobaric heat capacity at zero pressure / [J/kg/K] (ideal gas limit)
+
+/**	\brief Partial derivative of pressure wrt temperature at density constant.
+	\f$\left(\frac{\partial p}{\partial T}\right)_{\rho}\f$
+*/
+double fprops_dpdT_rho(FluidState state, FpropsError *err); 
+
+/// return the fluid quality; 0 if subcooled, 1 if superheated, error if both T>T_c and p>p_c
+double fprops_x(FluidState state, FpropsError *err);
+
+#if 1
+double fprops_dpdrho_T(const FluidState state, FpropsError *err);
+double fprops_d2pdrho2_T(const FluidState state, FpropsError *err);
+
+double fprops_dhdT_rho(const FluidState state, FpropsError *err);
+double fprops_dhdrho_T(const FluidState state, FpropsError *err);
+
+double fprops_dudT_rho(const FluidState state, FpropsError *err);
+double fprops_dudrho_T(const FluidState state, FpropsError *err);
+#endif
+
+
+double fprops_mu(FluidState state, FpropsError *err); ///< Dynamic viscosity / [Pa*s]
+double fprops_lam(FluidState state, FpropsError *err); ///< Thermal conductivity / [W/m/K]
+
+
+// needs to be undefined here?
+double fprops_psat(FluidState state, FpropsError *err); 
+double fprops_Tfreeze(FluidState state, FpropsError *err); 
+
+/**
+	Convert file data E into a PureFluid object, doing any necessary pre-calculation
+	along the way. The PureFluid should implement the named correlation type.
+	@return NULL on failure.
+*/
+PureFluid *fprops_prepare(const EosData *E, const char *corrtype);
+
+/* TODO what about a function to destroy the PureFluid structure? */
+
+/**
+	Check if the file data E is suitable for preparing a PureFluid
+	of the named correlation type.
+	If corrtype is NULL, return the 'best' available correlation in the data.
+	@return 0 if not available, else the corresponding EosType value if available.
+*/
+int fprops_corr_avail(const EosData *E, const char *corrtype);
+
+char *fprops_corr_type(EosType type);
+
+char *fprops_error(FpropsError err);
+#endif
---
+++ models/johnpye/fprops/vishnu/incompressible mixture files/incompressible.c
@@ -0,0 +1,336 @@
+/*	ASCEND modelling environment
+	Copyright (C) 2008-2013 John Pye
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*//** @file
+	Ideal-gas components of helmholtz fundamental functions, calculated using
+	terms in cp0 in a standard power series form. For details see the
+	publications cited in the various fluid *.c files.
+
+	John Pye, Jul 2008.
+*/
+
+#include <math.h>
+
+#include "incompressible.h"
+#include "cp0.h"
+#include "refstate.h"
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SQ(X) ((X)*(X))
+
+#ifndef FPROPS_NEW
+# error "Where is FPROPS_NEW??"
+#endif
+
+#ifndef FPROPS_ARRAY_COPY
+# error "where is FPROPS_ARRAY_COPY??"
+#endif
+
+//static void incompressible_set_reference_std(FluidData *D, const ReferenceStateStd *R);
+
+#define INCOMPRESSIBLE_DEBUG
+#ifdef INCOMPRESSIBLE_DEBUG
+# include "color.h"
+# define MSG FPROPS_MSG
+# define ERRMSG FPROPS_ERRMSG
+#else
+# define MSG(ARGS...) ((void)0)
+# define ERRMSG(ARGS...) ((void)0)
+#endif
+
+
+// next three functions are the typical models to get data from incompressible fit coefficients
+
+// evaluation function for polynamial type coefficients
+double eval_poly(IncompressibleCoeff c, double T, double x) {
+
+	double res = 0.0;
+	int i,j;
+
+	for(i=0;i<c.numc_r;i++)
+		for(j=0;j<c.numc_c;j++)
+			res += pow(x,j)*c.coeff[i][j]*pow(T,i);
+
+	return res;
+
+}
+
+// evaluation function for exponential-polynomial type coefficients
+double eval_exppoly(IncompressibleCoeff c, double T, double x) {
+
+	return exp(eval_poly(c,T,x));
+
+}
+
+// evaluation function for exponential type coefficients
+double eval_expo(IncompressibleCoeff c, double T, double x) {
+
+	return exp(c.coeff[0][0]/(T+c.coeff[0][1])-c.coeff[0][2]);
+
+}	
+
+
+
+/*--------------------------------------------
+  PREPARATION OF INCOMPRESSIBLE RUNDATA from FILEDATA
+*/
+
+PureFluid *incompressible_prepare(const EosData *E, const ReferenceState *ref){
+	PureFluid *P = FPROPS_NEW(PureFluid); 
+	P->data = FPROPS_NEW(FluidData);
+#define D P->data->corr.incomp
+
+	//MSG("...");
+
+	/* metadata */
+	P->name = E->name;
+	P->source = E->source;
+	P->type = FPROPS_INCOMPRESSIBLE;
+
+	switch(E->type){
+	case FPROPS_INCOMPRESSIBLE:
+		MSG("Incompressible");
+		D = FPROPS_NEW(IncompressibleRunData);
+#define IC E->data.incomp
+		D->T_base = IC->T_base;
+		D->T_max = IC->T_max;
+		D->T_min = IC->T_min;
+		D->x_base = IC->x_base;
+		D->x_max = IC->x_max;
+		D->x_min = IC->x_min;
+		D->T_minPsat = IC->T_minPsat;
+		int i,j;
+#define INCOMP_PREP(Q) \
+	D->Q.type = IC->Q.type; \
+	D->Q.numc_r = IC->Q.numc_r; \
+	D->Q.numc_c = IC->Q.numc_c; \
+	D->Q.coeff = (double**)malloc(D->Q.numc_r*sizeof(double*)); \
+	for(i=0;i<D->Q.numc_r;i++) \
+		D->Q.coeff[i] = (double*)malloc(D->Q.numc_c*sizeof(double)); \
+	for(i=0;i<D->Q.numc_r;i++) \
+		for(j=0;j<D->Q.numc_c;j++) \
+			D->Q.coeff[i][j] = IC->Q.coeff[i][j]; 
+		INCOMP_PREP(T_freeze)
+		INCOMP_PREP(conductivity)
+		INCOMP_PREP(density)
+		INCOMP_PREP(specific_heat)
+		INCOMP_PREP(viscosity)
+		INCOMP_PREP(saturation_pressure)
+#undef IC
+#undef INCOMP_PREP
+	break;/*
+	case FPROPS_HELMHOLTZ:
+		MSG("Helmholtz");
+		D->M = E->data.helm->M;
+		if(E->data.helm->R == 0){
+			P->data->R = R_UNIVERSAL / D->M;
+		}else{
+			P->data->R = E->data.helm->R;
+		}
+		D->T_t = 0;
+		D->T_c = E->data.helm->T_c;
+		D->p_c = 0;
+		D->rho_c = E->data.helm->rho_c;
+		D->omega = 0;
+		D->Tstar = 1;
+		D->rhostar = 1;
+		D->cp0 = cp0_prepare(E->data.helm->ideal, D->R, D->Tstar);
+		D->corr.helm = NULL;
+
+		if(ref == NULL){
+			ref = &(E->data.helm->ref);
+		}
+		break;*/
+	default:
+		ERRMSG("Unsupported source data type in incompressible_prepare");
+		FPROPS_FREE(P->data);
+		FPROPS_FREE(P);
+		return NULL;
+	}
+
+	// function pointers... more to come still? 
+#define FN(VAR1,VAR2) P->VAR1##_fn_inc = &incomp_eval_##VAR2
+	FN(rho,density);  FN(cp,specific_heat); FN(cv,specific_heat); 
+	FN(lam,conductivity); FN(mu,viscosity); 
+	FN(T_freeze,T_freeze); FN(p_sat,saturation_pressure);
+#undef FN
+
+// FN(u); FN(h); FN(s); FN(a); FN(g); FN(w); FN(dpdrho_T); FN(sat);// to be added for incompressible
+
+/*	//MSG("Setting reference state...");
+	// set the reference point
+	switch(ref->type){
+	case FPROPS_INCOMPRESSIBLE:
+		MSG("No reference state required in current incompressible implementation.\n");
+		FPROPS_FREE(P->data);
+		FPROPS_FREE(P);
+		return NULL;
+		break;
+	case FPROPS_REF_PHI0:
+		MSG("Applying PHI0 reference data");
+		P->data->cp0->c = ref->data.phi0.c;
+		P->data->cp0->m = ref->data.phi0.m;
+		break;
+	case FPROPS_REF_REF0:
+		//MSG("Applying ref0 reference state");
+		switch(P->data->ref0.type){
+		case FPROPS_REF_TPHG:
+			{
+				//MSG("TPHG");
+				ReferenceState *ref0 = &(P->data->ref0);
+				//MSG("T0 = %f, p0 = %f, h0 = %f, g0 = %f",ref0->data.tphg.T0,ref0->data.tphg.p0,ref0->data.tphg.h0,ref0->data.tphg.g0);
+				FpropsError res = FPROPS_NO_ERROR;
+				double rho0 = ref0->data.tphg.p0 / D->R / ref0->data.tphg.T0;
+				double T0 = ref0->data.tphg.T0;
+				double s0 = (ref0->data.tphg.h0 - ref0->data.tphg.g0) / T0;
+				double h0 = ref0->data.tphg.h0;
+
+				P->data->cp0->c = 0;
+				P->data->cp0->m = 0;
+				//MSG("T0 = %f, rho0 = %f",T0,rho0);
+				//MSG("btw, p = %f", D->R * T0 *rho0); // is OK
+				res = FPROPS_NO_ERROR;
+				double h1 = ideal_h(T0, rho0, P->data, &res);
+				double s1 = ideal_s(T0, rho0, P->data, &res);
+				if(res)ERRMSG("error %d",res);
+				//MSG("h1 = %f",h1);
+				P->data->cp0->c = -(s0 - s1)/D->R;
+				P->data->cp0->m = (h0 - h1)/D->R/P->data->Tstar;
+
+				h0 = ideal_h(T0,rho0, P->data, &res);
+				if(res)ERRMSG("error %d",res);
+				//MSG("new h0(T0,rho0) = %f", h0);
+				//double g0 = ideal_g(T0,rho0, P->data, &res);
+				//if(res)ERRMSG("error %d",res);
+				//MSG("new g0(T0,rho0) = %f", g0);
+				//MSG("DONE");
+			}
+			break;
+		default:
+			ERRMSG("Unsupported type of reference state (ref0) in incompressible_prepare");
+			FPROPS_FREE(P->data); FPROPS_FREE(P);
+			return NULL;
+		}
+		break;
+	default:
+		ERRMSG("Unsupported type of reference state requested in incompressible_prepare.\n");
+		FPROPS_FREE(P->data);
+		FPROPS_FREE(P);
+		return NULL;
+	}*/
+
+	return P;
+}
+
+#undef D
+#define D data->corr.incomp
+
+#define PROP_EVAL(Q) \
+	double incomp_eval_ ## Q (double T, double p, double x, const FluidData *data, FpropsError *err) { \
+		assert(T>0&&T>=D->T_min&&T<=D->T_max); \
+		assert(x>=D->x_min&&x<=D->x_max); \
+		if(!strcmp(D->Q.type,"polynomial")) return eval_poly(D->Q,T-D->T_base,x-D->x_base); \
+		else if(!strcmp(D->Q.type,"exppolynomial")) return eval_exppoly(D->Q,T-D->T_base,x-D->x_base); \
+		else if(!strcmp(D->Q.type,"exponential")) return eval_expo(D->Q,T,x); \
+		MSG("Invalid request"); \
+		*err = FPROPS_INVALID_REQUEST; \
+		return 0; \
+	}
+
+// declaration and definition of property evaluation functions for conductivity, density, specific-heat and viscosity
+PROP_EVAL(T_freeze)
+PROP_EVAL(conductivity)
+PROP_EVAL(density)
+PROP_EVAL(specific_heat)
+PROP_EVAL(viscosity)
+PROP_EVAL(saturation_pressure)
+		
+#undef PROP_EVAL
+#undef D
+
+/*
+#define DEFINE_TAU double tau = data->Tstar / T
+#define DEFINE_TAUDELTA DEFINE_TAU; double delta = rho / data->rhostar
+
+double ideal_p(double T, double rho, const FluidData *data, FpropsError *err){
+	return data->R * T * rho;
+}
+
+double ideal_h(double T, double rho, const FluidData *data, FpropsError *err){
+	DEFINE_TAUDELTA;
+	return data->R * T * (1 + tau * ideal_phi_tau(tau,delta,data->cp0));
+}
+
+double ideal_s(double T, double rho, const FluidData *data, FpropsError *err){
+	DEFINE_TAUDELTA;
+	//double pht = ideal_phi_tau(tau,delta,data->cp0);
+	//double ph = ideal_phi(tau,delta,data->cp0);
+	return data->R * (tau * ideal_phi_tau(tau,delta,data->cp0) - ideal_phi(tau,delta,data->cp0));
+}
+
+double ideal_u(double T, double rho, const FluidData *data, FpropsError *err){
+	return ideal_h(T,rho,data,err) - data->R * T;
+}
+
+double ideal_a(double T, double rho, const FluidData *data, FpropsError *err){
+	return ideal_h(T,rho,data,err) - T * (data->R + ideal_s(T,rho,data,err));
+}
+
+double ideal_g(double T, double rho, const FluidData *data, FpropsError *err){
+	//MSG("g(T=%f,rho=%f)...",T,rho);
+	double h = ideal_h(T,rho,data,err);
+	double s = ideal_s(T,rho,data,err);
+	//MSG("h = %f, T = %f, s = %f, h-T*s = %f",h,T,s,h-T*s);
+	return h - T * s;
+}
+
+
+//	Note that this function is called by ALL fluid types via 'fprops_cp0' which
+//	means that it needs to include the scaling temperature within the structure;
+//	we can't just define Tstar as a constant for ideal fluids.
+
+double ideal_cp(double T, double rho, const FluidData *data, FpropsError *err){
+	DEFINE_TAU;
+	double res = data->R * (1. - SQ(tau) * ideal_phi_tautau(tau,data->cp0));
+	return res;
+}
+
+double ideal_cv(double T, double rho, const FluidData *data, FpropsError *err){
+	DEFINE_TAU;
+	return - data->R * SQ(tau) * ideal_phi_tautau(tau,data->cp0);
+}
+
+double ideal_w(double T, double rho, const FluidData *data, FpropsError *err){
+	DEFINE_TAU;
+	double w2onRT = 1. - 1. / (SQ(tau) * ideal_phi_tautau(tau,data->cp0));
+	return sqrt(data->R * T * w2onRT);
+}
+
+double ideal_dpdrho_T(double T, double rho, const FluidData *data, FpropsError *err){
+	return data->R * T;
+}
+
+double ideal_sat(double T,double *rhof_ret, double *rhog_ret, const FluidData *data, FpropsError *err){
+	MSG("Ideal gas: saturation calculation is not possible");
+	*err = FPROPS_RANGE_ERROR;
+	return 0;
+}
+
+*/
+
+
