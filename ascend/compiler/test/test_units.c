f5f2ff402792e17b7cd7d767fee83b67ff9c2c5c
John Pye <john.pye@anu.edu.au>
2018-04-03 07:00:54 +0000

    add missing test_units.c
    git-svn-id: https://svn.ascend4.org/trunk@3349 f15cd148-d149-4907-926e-73cf92b3ee03

---
+++ ascend/compiler/test/test_units.c
@@ -0,0 +1,58 @@
+/*	ASCEND modelling environment
+	Copyright (C) 2007 Carnegie Mellon University
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*//**
+	@file
+	Unit test functions for compiler.
+*/
+#include <ascend/compiler/units.h>
+#include <ascend/compiler/symtab.h>
+
+#include <ascend/general/env.h>
+#include <ascend/general/list.h>
+#include <ascend/general/platform.h>
+#include <ascend/utilities/ascEnvVar.h>
+#include <ascend/utilities/error.h>
+
+#include <test/common.h>
+
+static void test_test1(void){
+	// test setup and destruction of the global list
+
+	gl_init_pool();
+	gl_init();
+	InitDimenList();
+	InitSymbolTable();
+	InitUnitsTable();
+
+	DumpUnits(stderr);
+
+	DestroyUnitsTable();
+	DestroyStringSpace();
+	DestroySymbolTable();
+	DestroyDimenList();
+	gl_destroy_pool();
+}
+
+/*===========================================================================*/
+/* Registration information */
+
+/* the list of tests */
+
+#define TESTS(T) \
+	T(test1)
+
+REGISTER_TESTS_SIMPLE(compiler_units, TESTS)
+
