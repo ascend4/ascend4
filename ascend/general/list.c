405a02f4bf56aabc2e42295c915cceb148b755dc
John Pye <pye@sent.com>
2026-02-06 13:25:02 +1100

    static decl

--- ascend/general/list.c
+++ ascend/general/list.c
@@ -303,6 +303,7 @@ struct gl_list_t *gl_create(unsigned long int capacity){
   }
 }
 
+static void gl_note_destroyed(struct gl_list_t *list);
 
 void gl_free_and_destroy(struct gl_list_t *list){
   unsigned long c;
