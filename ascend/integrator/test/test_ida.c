ad0372889512645a64f18d52ace7d76e4e0cf942
John Pye <john.pye@anu.edu.auwq>
2026-03-26 19:11:21 +1100

    obs_id

--- ascend/integrator/test/test_ida.c
+++ ascend/integrator/test/test_ida.c
@@ -858,6 +858,12 @@ static void test_example_ideal_rebound(){
 	it = ida_child(root, "t");
 	itlast = ida_child(root, "t_last_event");
 
+	CU_ASSERT_EQUAL(testsys.integ->n_obs, 4);
+	CU_ASSERT_PTR_EQUAL(var_instance(testsys.integ->obs[0]), it);
+	CU_ASSERT_PTR_EQUAL(var_instance(testsys.integ->obs[1]), iy);
+	CU_ASSERT_PTR_EQUAL(var_instance(testsys.integ->obs[2]), iv);
+	CU_ASSERT_PTR_EQUAL(var_instance(testsys.integ->obs[3]), itlast);
+
 	for(i = 0; i < testsys.integ->n_y; ++i){
 		CU_TEST(testsys.integ->y[i] == NULL || var_instance(testsys.integ->y[i]) != itlast);
 	}
@@ -890,6 +896,13 @@ static void test_example_lengthening_sawtooth(){
 	isample = ida_child(root, "sample");
 	iperiod = ida_child(root, "period");
 	itlast = ida_child(root, "t_last_event");
+
+	CU_ASSERT_EQUAL(testsys.integ->n_obs, 5);
+	CU_ASSERT_PTR_EQUAL(var_instance(testsys.integ->obs[0]), it);
+	CU_ASSERT_PTR_EQUAL(var_instance(testsys.integ->obs[1]), iy);
+	CU_ASSERT_PTR_EQUAL(var_instance(testsys.integ->obs[2]), isample);
+	CU_ASSERT_PTR_EQUAL(var_instance(testsys.integ->obs[3]), iperiod);
+	CU_ASSERT_PTR_EQUAL(var_instance(testsys.integ->obs[4]), itlast);
 
 	for(i = 0; i < testsys.integ->n_y; ++i){
 		CU_TEST(testsys.integ->y[i] == NULL || var_instance(testsys.integ->y[i]) != iperiod);
